a39b05ee22870f2b6fe43bcacd93ef761c0793f2
me-suzy <me.suzana@gmail.com>
2025-08-11 00:54:04 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/notes/conf/layout.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/notes/conf/layout.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$layout_info[] = array ("content_var" => "CNT_notes", 
+			"transfer_var" => "body",
+			"home_only"=>0);
+
+$layout_info[] = array ("content_var" => "CNT_notes_block", 
+			"transfer_var" => "left_col_mid",
+			"home_only"=>0);
+?>
