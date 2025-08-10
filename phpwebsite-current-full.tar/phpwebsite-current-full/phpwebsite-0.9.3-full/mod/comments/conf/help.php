86ac33166760e97059c58f26a0edb4b993817379
me-suzy <me.suzana@gmail.com>
2025-08-11 00:49:23 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/comments/conf/help.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/comments/conf/help.php
@@ -0,0 +1,9 @@
+<?php
+
+$main = "Comment Thread Management";
+$main_content = "The Comment Thread Management is mainly for maintaining the comment threads which are in the database.  These threads are usually associated with an annoucement, pagemaster page, poll, or faq question.  From this list you can view and delete entire threads.";
+
+$settings = "Settings";
+$settings_content = "The following are basic thread management settings.  The first set is to give your visitors and new users default options for viewing comment threads.  The second set of options is to control the amount of content being posted under a single thread and to protect your site from and overload by a single user.";
+
+?>
