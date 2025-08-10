cfbd8e5e5577cd5851110fbfd02996f73ff6d600
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:34 +0300

    Add Photopost_PHP/languages/spanish/login.php

---
+++ Photopost_PHP/languages/spanish/login.php
@@ -0,0 +1,5 @@
+<?
+$Globals['pp_lang']['loginok'] = "Identificaci&oacute;n completa!";
+$Globals['pp_lang']['loginbad'] = "Identificaci&oacute;n inv&aacute;lida. Por favor reviza tu nombre de usuario y password, o";
+$Globals['pp_lang']['loginbad2'] = "reg&iacute;strate";
+?>
