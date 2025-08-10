3de95984ad32bcbb95cf2d122bb7e2cd1ab0d465
me-suzy <me.suzana@gmail.com>
2025-08-11 00:46:45 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/announce/conf/config.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/announce/conf/config.php
@@ -0,0 +1,8 @@
+<?php
+$max_image_width = 1500;
+$max_image_height = 1500;
+
+// Uncomment and edit to restrict image types
+//$allowedImageTypes = array("image/jpg", "image/png", "image/gif");
+
+?>
