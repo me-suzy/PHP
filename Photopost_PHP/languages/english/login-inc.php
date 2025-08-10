e5f63294382824743a46fcb9cf334b8d1bb7f0f3
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:18 +0300

    Add Photopost_PHP/languages/english/login-inc.php

---
+++ Photopost_PHP/languages/english/login-inc.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['login'] = "Login";
+$Globals['pp_lang']['enterup'] = "Please enter your username and password.";
+$Globals['pp_lang']['username'] = "Username";
+$Globals['pp_lang']['register'] = "Register";
+$Globals['pp_lang']['password'] = "Password";
+$Globals['pp_lang']['forgot'] = "Forgot password?";
+$Globals['pp_lang']['submit'] = "Submit";
+$Globals['pp_lang']['unreg'] = "Unregistered";
+?>
