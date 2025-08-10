a7535df4bd62fbad494ccfb2465fe672ebde103d
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:19 +0300

    Add Photopost_PHP/languages/english/login.php

---
+++ Photopost_PHP/languages/english/login.php
@@ -0,0 +1,5 @@
+<?
+$Globals['pp_lang']['loginok'] = "Login Successful!";
+$Globals['pp_lang']['loginbad'] = "Invalid login.  Please check your username and password, or";
+$Globals['pp_lang']['loginbad2'] = "register";
+?>
