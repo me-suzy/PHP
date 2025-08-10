2defe8573ef0da1a1ff3a3a802b007b1e155d656
me-suzy <me.suzana@gmail.com>
2025-08-11 00:48:19 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/branch/conf/layout.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/branch/conf/layout.php
@@ -0,0 +1,13 @@
+<?php
+
+$layout_info[] = array (
+			"content_var"=>"CNT_Branch_Main", 
+			"theme_var"=>"body", 
+			);
+
+$layout_info[] = array (
+			"content_var"=>"CNT_Branch_Panel", 
+			"theme_var"=>"top", 
+			);
+
+?>
