946bde59cae14a8cd374148da43be291eb9b6aed
me-suzy <me.suzana@gmail.com>
2025-08-11 00:48:16 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/branch/conf/boost.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/branch/conf/boost.php
@@ -0,0 +1,19 @@
+<?php
+$mod_title = "branch";
+$mod_pname = "Branch Creator";
+$mod_directory = "branch";
+$mod_filename = "index.php";
+$priority = 50;
+$admin_op = "&branch_op=firstPage";
+$allow_view = array ("boost"=>1, "branch"=>1);
+$mod_icon = "branch.gif";
+$user_mod = 0;
+$admin_mod = 1;
+$deity_mod = 1;
+$mod_class_files = array("Branch.php");
+$active = "on";
+$branch_allow = 0;
+$version = 1.5;
+$install_file = "install.php";
+$uninstall_file = "uninstall.php";
+?>
