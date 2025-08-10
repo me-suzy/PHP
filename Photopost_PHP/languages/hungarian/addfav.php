8dd6dd79337ca778a0663c6df1cc9447ab1bea62
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:42 +0300

    Add Photopost_PHP/languages/hungarian/addfav.php

---
+++ Photopost_PHP/languages/hungarian/addfav.php
@@ -0,0 +1,8 @@
+<?
+$Globals['pp_lang']['badcall'] = "Szkript behívása hibás.  Menjen egy adott fotóhoz, majd kattintson a Hozzáadás a kedvencekhez linkre!";
+$Globals['pp_lang']['closed'] = "Nagyon sajnáljuk, de a Képtár technikai okok miatt átmenetileg zárva tart. Kérjük, próbálkozzon késõbb!";
+$Globals['pp_lang']['noreg'] = "Csak regisztrált tagok választhatnak kedvenc fotót..";
+$Globals['pp_lang']['added'] = "A kép bekerült a Kedvencek albumába.";
+$Globals['pp_lang']['malform'] = "Hibás kérés az addfav.php fájl felé";
+$Globals['pp_lang']['removed'] = "A kép már nincs a Kedvencek albumában.";
+?>
