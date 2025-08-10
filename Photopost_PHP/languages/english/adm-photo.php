76e99c8aeb4c96154c695e3667f1fcaaa62991c8
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:08 +0300

    Add Photopost_PHP/languages/english/adm-photo.php

---
+++ Photopost_PHP/languages/english/adm-photo.php
@@ -0,0 +1,18 @@
+<?
+$Globals['pp_lang']['malform'] = "Malformed parameter passed!";
+$Globals['pp_lang']['mogrify'] = "Unsupported call to modify image.";
+$Globals['pp_lang']['erotate'] = "Error rotating image!";
+$Globals['pp_lang']['erotatemed'] = "Error rotating medium image!";
+$Globals['pp_lang']['rotateccw'] = "Image has been rotated counter-clockwise!";
+$Globals['pp_lang']['rotatecw'] = "Image has been rotated clockwise!";
+$Globals['pp_lang']['flipped'] = "Image has been flipped!";
+$Globals['pp_lang']['flopped'] = "Image has been flopped!";    
+$Globals['pp_lang']['nopic'] = "Error: No pic to delete!";
+$Globals['pp_lang']['nophoto'] = "Photo not found in your database!";
+$Globals['pp_lang']['delete'] = "Deleted image";
+$Globals['pp_lang']['errorcode'] = "Error code";
+$Globals['pp_lang']['noperm'] = "You do not have permission for this action!";
+$Globals['pp_lang']['badmove'] = "You cannot move an image to that category, please try again.";
+$Globals['pp_lang']['noneed'] = "No need to move image to same category!";
+$Globals['pp_lang']['noaction'] = "No action specified";
+?>
