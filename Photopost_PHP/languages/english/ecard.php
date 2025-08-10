3dec297ff1fb44555e1eb2cabff42aa5bd4d9826
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:13 +0300

    Add Photopost_PHP/languages/english/ecard.php

---
+++ Photopost_PHP/languages/english/ecard.php
@@ -0,0 +1,23 @@
+<?
+$Globals['pp_lang']['disabled'] = "e-Cards are disabled";
+$Globals['pp_lang']['larger'] = "View Larger Image";
+$Globals['pp_lang']['smaller'] = "View Smaller Image";
+$Globals['pp_lang']['send'] = "Send photo as e-Card";
+$Globals['pp_lang']['view'] = "View your e-Card";
+$Globals['pp_lang']['ecard'] = "eCard";
+$Globals['pp_lang']['home'] = "Home";
+$Globals['pp_lang']['name'] = "Name";
+$Globals['pp_lang']['email'] = "Email";
+$Globals['pp_lang']['info'] = "Recipient Info";
+$Globals['pp_lang']['alter'] = "If you don't know the recipient's email address, enter their member name instead:<br /><br />Member Name";
+$Globals['pp_lang']['subjgreet'] = "e-Card Subject/Greeting";
+$Globals['pp_lang']['subject'] = "Subject";
+$Globals['pp_lang']['yourmess'] = "Your e-card message";
+$Globals['pp_lang']['yourecard'] = "Your e-Card";
+$Globals['pp_lang']['send'] = "Send the e-Card";
+$Globals['pp_lang']['noemail'] = "You didn't enter a recipient email.  Please go back and enter either a recipient email or a member name.";
+$Globals['pp_lang']['blank'] = "Your message was blank.";
+$Globals['pp_lang']['badname'] = "Sorry, member name not recognized.";
+$Globals['pp_lang']['hello'] = "Hello";
+$Globals['pp_lang']['cardsent'] = "Your card has been sent!";
+?>
