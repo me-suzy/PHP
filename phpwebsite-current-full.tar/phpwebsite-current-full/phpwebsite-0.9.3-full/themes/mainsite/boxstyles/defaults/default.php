890278e598154810e25cc632ebeda2ee16050ae1
me-suzy <me.suzana@gmail.com>
2025-08-11 00:59:50 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/themes/mainsite/boxstyles/defaults/default.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/themes/mainsite/boxstyles/defaults/default.php
@@ -0,0 +1,8 @@
+<?php
+// Default box will update the entire module's boxstyle
+// If the content variable is found in the layout table
+// then it will be updated according to the array
+
+$default_box = "default_box.tpl";
+$default_pop = "default_pop.tpl";
+?>
