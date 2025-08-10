a1ec8c007f7ce4e918c282c34206381b66fa63bb
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:15 +0300

    Add Photopost_PHP/languages/english/editphoto.php

---
+++ Photopost_PHP/languages/english/editphoto.php
@@ -0,0 +1,26 @@
+<?
+$Globals['pp_lang']['closed'] = "We're sorry, but our Photo Boards are currently down for maintainence. Please try again later.";
+$Globals['pp_lang']['noperm'] = "Sorry, you don't have permission to post, or if you tried to edit, you might not be the post's author.";
+$Globals['pp_lang']['badcall'] = "Script not called correctly.  Navigate to a specific photo, then click on the edit link.";
+$Globals['pp_lang']['editphoto'] = "Edit Photo";
+$Globals['pp_lang']['editor'] = "Image Editor";
+$Globals['pp_lang']['username'] = "Username";
+$Globals['pp_lang']['thumbnail'] = "Thumbnail";
+$Globals['pp_lang']['rotateccw'] = "Rotate CCW";
+$Globals['pp_lang']['rotatecw'] = "Rotate CW";
+$Globals['pp_lang']['flip'] = "Flip Vertical";
+$Globals['pp_lang']['flop'] = "Flop Horizontal";
+$Globals['pp_lang']['catchange'] = "Change category? (leave blank to leave alone)";
+$Globals['pp_lang']['edittitle'] = "Edit title for the photo";
+$Globals['pp_lang']['editkey'] = "Edit keywords:";
+$Globals['pp_lang']['editdesc'] = "Edit Photo Description";
+$Globals['pp_lang']['deletechk'] = "If you want to <b>delete</b> this image completely, check this box";
+$Globals['pp_lang']['careful'] = "(CAREFUL - once it's gone it's gone)";
+$Globals['pp_lang']['submitchg'] = "Submit changes";
+$Globals['pp_lang']['nophoto'] = "Photo not found in your database!";
+$Globals['pp_lang']['malformed'] = "Malformed parameter passed!";
+$Globals['pp_lang']['deleted'] = "You have successfully deleted your image.";
+$Globals['pp_lang']['noaction'] = "You do not have permission for this action!";
+$Globals['pp_lang']['success'] = "You successfully edited your photo.";
+$Globals['pp_lang']['allowprint'] = "Do you want to allow people to print your photo onto a calendar?";
+?>
