6f7829fabcc593d7ea710163dc85c6faab13a646
me-suzy <me.suzana@gmail.com>
2025-08-11 00:56:34 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/photoalbum/conf/layout.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/photoalbum/conf/layout.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @version $Id: layout.php,v 1.2 2003/03/28 18:00:09 steven Exp $
+ */
+
+$layout_info[] = array ("content_var"=>"CNT_photoalbum", 
+			"transfer_var"=>"body");
+
+?>
