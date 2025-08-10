b030517cc0519484d5e7e9e72580eb66c49e7e49
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:10 +0300

    Add Photopost_PHP/languages/norwegian/reportphoto.php

---
+++ Photopost_PHP/languages/norwegian/reportphoto.php
@@ -0,0 +1,13 @@
+<?
+$Globals['pp_lang']['badcall'] = "Skriptet ble ikke kalt opp korrekt.  Naviger til et spesifikt foto, klikk så rediger linken.";
+$Globals['pp_lang']['closed'] = "Vi beklager, men våre sider er for øyeblikket nede grunnet vedlikehold. Vennligst prøv igjen senere.";
+$Globals['pp_lang']['reportphoto'] = "Rapporter Bilde";
+$Globals['pp_lang']['tool'] = "Rapporteringsverktøy";
+$Globals['pp_lang']['reportreason'] = "Grunnlag for rapportering?";
+$Globals['pp_lang']['inapprop'] = "Upassende materiale";
+$Globals['pp_lang']['copyright'] = "Brudd på Opphavsrett";
+$Globals['pp_lang']['wrongcat'] = "Bilde I Feil Kategori";
+$Globals['pp_lang']['other'] = "Annet";
+$Globals['pp_lang']['moreinfo'] = "Mer informasjon (om nødvendig)?";
+$Globals['pp_lang']['webnotice'] = "Bildet du har rapportert vil bli sjekket av webmaster.";
+?>
