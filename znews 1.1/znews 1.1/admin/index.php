566c69d1d7673f10b653534ce0afbfad32103711
me-suzy <me.suzana@gmail.com>
2025-08-11 01:06:01 +0300

    Add znews 1.1/znews 1.1/admin/index.php

---
+++ znews 1.1/znews 1.1/admin/index.php
@@ -0,0 +1,52 @@
+<html>
+
+<head>
+
+<title>ZNews 1.1</title>
+
+<link rel="stylesheet" href="style.css" type="text/css">
+
+</head>
+
+<body bgcolor="#4682B4">
+
+<form name="form1" method="post" action="admin.php">
+
+  <div align="center">
+
+<p>
+
+<br>
+
+<br>
+
+<br>
+
+<p>
+
+<table border="0">
+
+<tr><td>
+
+    <font face="Verdana" size="2">  Name: </td><td><input type="text" name="user" size="40" style="background-color : #3399FF; border : 2px dotted black">
+
+</td></tr><tr><td>
+
+<font face="Verdana" size="2">Passwort: </td><td><input type="password" name="passw" size="40" style="background-color : #3399FF; border : 2px dotted black">
+
+</td></tr><tr><td>&nbsp;</td><td>    
+
+    <input type="submit" name="Abschicken" value="Login" value="Senden" style="BACKGROUND-COLOR: #3399FF; border : 2px dotted black">
+
+</tr></td>
+
+</table>
+
+  </div>
+
+</form>
+
+</body>
+
+</html>
+
