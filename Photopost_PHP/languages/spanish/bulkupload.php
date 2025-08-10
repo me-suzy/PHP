5583600cd3af0f6ce6a2f734e616127159316ec1
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:24 +0300

    Add Photopost_PHP/languages/spanish/bulkupload.php

---
+++ Photopost_PHP/languages/spanish/bulkupload.php
@@ -0,0 +1,26 @@
+<?
+$Globals['pp_lang']['closed'] = "Lo sentimos, pero nuestra galer&iacute;a se encuentra cerrada. Por favor intenta m&aacute;s tarde.";
+$Globals['pp_lang']['noupload'] = "No se permiten cargas de los usuarios";
+$Globals['pp_lang']['noperm'] = "Lo siento, no tienes permisos para subir im&aacute;genes.";
+$Globals['pp_lang']['noverify'] = "Lo siento, pero no has verificado tu cuenta.<p>Debes hacerlo antes de subir fotos.";
+$Globals['pp_lang']['processing'] = "Procesando lista de im&aacute;genes";
+$Globals['pp_lang']['bulkuploads'] = "Carga M&uacute;ltiple";
+$Globals['pp_lang']['photoadd'] = "Agregar Im&aacute;genes Photopost";
+$Globals['pp_lang']['addselect'] = "Agregar Im&aacute;genes - Seleccionar Im&aacute;genes / Configurar Propiedades";
+$Globals['pp_lang']['image'] = "Imagen";
+$Globals['pp_lang']['add'] = "Agregar";
+$Globals['pp_lang']['category'] = "Categor&iacute;a";
+$Globals['pp_lang']['optional'] = "Texto Opcional";
+$Globals['pp_lang']['title'] = "T&iacute;tulo";
+$Globals['pp_lang']['desc'] = "Descripci&oacute;n";
+$Globals['pp_lang']['nomore'] = "No se encontraron m&aacute;s im&aacute;genes.";
+$Globals['pp_lang']['clickhere'] = "Click aqu&iacute; para volver al men&uacute; principal.";
+$Globals['pp_lang']['show'] = "Mostrar vistas previas?";
+$Globals['pp_lang']['thumbs'] = "N&uacute;mero de vistas previas para procesar?";
+$Globals['pp_lang']['proc'] = "Procesar";
+$Globals['pp_lang']['preppro'] = "Prepar&aacute;ndose para procesar las siguientes";
+$Globals['pp_lang']['images'] = "im&aacute;genes";
+$Globals['pp_lang']['click'] = "click aqu&iacute;";
+$Globals['pp_lang']['process'] = "Procesando Archivos";
+$Globals['pp_lang']['notrefresh'] = "si la p&aacute;gina no refresca.";
+?>
