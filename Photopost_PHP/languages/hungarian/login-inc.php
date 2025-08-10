5c5e204f1903f19edf0394ffe0943969acc730a6
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:55 +0300

    Add Photopost_PHP/languages/hungarian/login-inc.php

---
+++ Photopost_PHP/languages/hungarian/login-inc.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['login'] = "Belépés";
+$Globals['pp_lang']['enterup'] = "Kérjük, adja meg felhasználónevét és jelszavát!";
+$Globals['pp_lang']['username'] = "Felhasználónév";
+$Globals['pp_lang']['register'] = "Regisztráció";
+$Globals['pp_lang']['password'] = "Jelszó";
+$Globals['pp_lang']['forgot'] = "Elfelejtette jelszavát?";
+$Globals['pp_lang']['submit'] = "Küldés";
+$Globals['pp_lang']['unreg'] = "Nem regisztrált";
+?>
