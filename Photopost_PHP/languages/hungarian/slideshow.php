5503e212ba44d31bd2347d8722050a854fb1565c
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:10 +0300

    Add Photopost_PHP/languages/hungarian/slideshow.php

---
+++ Photopost_PHP/languages/hungarian/slideshow.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['closed'] = "Nagyon sajnáljuk, de a Képtár technikai okok miatt átmenetileg zárva tart. Kérjük, próbálkozzon késõbb!";
+$Globals['pp_lang']['slideshow'] = "Vetítés";
+$Globals['pp_lang']['delay'] = "Fotók késleltetése?<br />(lassabb kapcsolatoknál hosszabb a holtidõ)";
+$Globals['pp_lang']['twosec'] = "2 mp";
+$Globals['pp_lang']['foursec'] = "4 mp";
+$Globals['pp_lang']['eightsec'] = "8 mp";
+$Globals['pp_lang']['tensec'] = "10 mp";   
+$Globals['pp_lang']['startslide'] = "Vetítés elkezdése";
+?>
