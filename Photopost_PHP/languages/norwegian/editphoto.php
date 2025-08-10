f89f0466e81e8eae34e91c36f3ffaef2008742fd
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:57 +0300

    Add Photopost_PHP/languages/norwegian/editphoto.php

---
+++ Photopost_PHP/languages/norwegian/editphoto.php
@@ -0,0 +1,26 @@
+<?
+$Globals['pp_lang']['closed'] = "Vi beklager, men våre sider er for øyeblikket nede grunnet vedlikehold. Vennligst prøv igjen senere.";
+$Globals['pp_lang']['noperm'] = "Beklager, du har ikke tillatelse til å poste, eller om du forsøkte å redigere beskjeden, du er sannsynligvis ikke den som postet.";
+$Globals['pp_lang']['badcall'] = "Skript ikke kalt opp korrekt. Gå til et spesifikt bilde, klikk så rediger lenken.";
+$Globals['pp_lang']['editphoto'] = "Rediger bilde";
+$Globals['pp_lang']['editor'] = "Bilde Redigeringsverktøy";
+$Globals['pp_lang']['username'] = "Brukernavn";
+$Globals['pp_lang']['thumbnail'] = "Småbilde";
+$Globals['pp_lang']['rotateccw'] = "Roter CCW";
+$Globals['pp_lang']['rotatecw'] = "Roter CW";
+$Globals['pp_lang']['flip'] = "Flipp Vertikalt";
+$Globals['pp_lang']['flop'] = "Flopp Horisontalt";
+$Globals['pp_lang']['catchange'] = "Forandre kategori? (la være om du ønsker det som det er)";
+$Globals['pp_lang']['edittitle'] = "Forandre tittel på bildet";
+$Globals['pp_lang']['editkey'] = "Forandre nøkkelord:";
+$Globals['pp_lang']['editdesc'] = "Forandre Bildebeskrivelse";
+$Globals['pp_lang']['deletechk'] = "Om du ønsker å <b>slette</b> dette bildet helt, merk av her";
+$Globals['pp_lang']['careful'] = "(PASS PÅ - når du har slettet det, er det borte for godt)";
+$Globals['pp_lang']['submitchg'] = "Fullfør";
+$Globals['pp_lang']['nophoto'] = "Bilde ikke funnet i vår database!";
+$Globals['pp_lang']['malformed'] = "Ukorrekt parameter sendt!";
+$Globals['pp_lang']['deleted'] = "Du har nå slettet ditt bilde.";
+$Globals['pp_lang']['noaction'] = "Du har ikke tillatelse til å gjøre dette!";
+$Globals['pp_lang']['success'] = "Redigering av bilde vellykket.";
+$Globals['pp_lang']['allowprint'] = "Do you want to allow people to print your photo onto a calendar?";
+?>
