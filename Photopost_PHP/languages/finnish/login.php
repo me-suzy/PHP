4b90d480d05a0a9d1f60fa7fbe46f07186864ceb
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:51 +0300

    Add Photopost_PHP/languages/finnish/login.php

---
+++ Photopost_PHP/languages/finnish/login.php
@@ -0,0 +1,5 @@
+<?
+$Globals['pp_lang']['loginok'] = "Kirjautuminen onnistui!";
+$Globals['pp_lang']['loginbad'] = "Väärä kirjautuminen.  Tarkista käyttäjätunnus ja salasana, tai";
+$Globals['pp_lang']['loginbad2'] = "rekisteröidy";
+?>
