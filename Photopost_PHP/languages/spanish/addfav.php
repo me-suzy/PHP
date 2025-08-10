feaa0e202e7e8de297db2c46862655adb3f5df9e
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:20 +0300

    Add Photopost_PHP/languages/spanish/addfav.php

---
+++ Photopost_PHP/languages/spanish/addfav.php
@@ -0,0 +1,8 @@
+<?
+$Globals['pp_lang']['badcall'] = "El programa no se llam&oacute; correctamente. Navegue a una foto espec&iacute;fica, y entonces haga click en el link Agregar a Favoritos.";
+$Globals['pp_lang']['closed'] = "Lo sentimos, pero nuestra galer&iacute;a se encuentra cerrada por el momento. Por favor intente m&aacute;s tarde.";
+$Globals['pp_lang']['noreg'] = "S&oacute;lo usuarios registrados pueden tener im&aacute;genes favoritas";
+$Globals['pp_lang']['added'] = "La imagen ha sido agregada a su album de favoritos";
+$Globals['pp_lang']['malform'] = "Llamada inv&aacute;lida a addfav.php";
+$Globals['pp_lang']['removed'] = "La imagen ha sido removida de su &Aacute;lbum de Favoritos!";
+?>
