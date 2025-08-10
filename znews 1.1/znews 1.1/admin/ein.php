f2a494704747045db719a505abc1d59dea5f0c4a
me-suzy <me.suzana@gmail.com>
2025-08-11 01:05:59 +0300

    Add znews 1.1/znews 1.1/admin/ein.php

---
+++ znews 1.1/znews 1.1/admin/ein.php
@@ -0,0 +1,27 @@
+<body  bgcolor="#4682B4">
+
+<p align="center"><font face="Verdana" size="2"><b>Fragen</b><table border="0"  bgcolor="#4682B4">
+
+<form method="post" action="set.php"><tr><td><font face="Verdana" size="2">
+
+Name:</td><td><font face="Verdana" size="2"><input type="text" name="name" size="40" style="background-color : #3399FF; border : 2px dotted black;"></td></tr>
+
+<tr><td><font face="Verdana" size="2">Mail:</td><td><input type="text" name="mail" size="40" style="background-color : #3399FF; border : 2px dotted black"></td></tr>
+
+<tr><td><font face="Verdana" size="2">Datum:</td><td><input type="text" name="date" size="40" style="background-color : #3399FF; border : 2px dotted black"></td></tr>
+
+<tr><td><font face="Verdana" size="2">Betreff:</td><td><input type="text" name="betr" size="40" style="background-color : #3399FF; border : 2px dotted black"></td></tr>
+
+<tr><td><font face="Verdana" size="2">Text:</td><td><TEXTAREA name="text" rows="5" cols="30" wrap="virtual" style="background-color : #3399FF; border : 2px dotted black; scrollbar-base-color : #3399FF"></TEXTAREA></td></tr>
+
+<tr><td><font face="Verdana" size="2">Weitere Angaben:</td><td><TEXTAREA name="text2" rows="5" cols="30" wrap="virtual" style="background-color : #3399FF; border : 2px dotted black; scrollbar-base-color : #3399FF"></TEXTAREA></td></tr>
+
+<tr><td></td><td><div align="right"><input type="submit" value="Senden" style="BACKGROUND-COLOR: #3399FF; border : 2px dotted black"></td></tr>
+
+<tr>&nbsp;<td></td><td>&nbsp;</td></tr>
+
+<tr>&nbsp;<td></td><td>&nbsp;</td></tr>
+
+<tr><td></td><td><div align="center"></div></tr>
+
+</table>
