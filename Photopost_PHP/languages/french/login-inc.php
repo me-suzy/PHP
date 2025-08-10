bcc45ffaf1664a56c3433836ef84441b1c16343d
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:21 +0300

    Add Photopost_PHP/languages/french/login-inc.php

---
+++ Photopost_PHP/languages/french/login-inc.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['login'] = "Se connecter";
+$Globals['pp_lang']['enterup'] = "Veuillez entrer votre nom d'utilisateur et votre mot de passe.";
+$Globals['pp_lang']['username'] = "Nom d'utilisateur";
+$Globals['pp_lang']['register'] = "S'inscrire";
+$Globals['pp_lang']['password'] = "Mot de passe";
+$Globals['pp_lang']['forgot'] = "Mot de passe oublié?";
+$Globals['pp_lang']['submit'] = "Envoyer";
+$Globals['pp_lang']['unreg'] = "Non inscrit";
+?>
