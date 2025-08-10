57939f61eb2428d542c0fface6b8a6e5931d82ba
me-suzy <me.suzana@gmail.com>
2025-08-11 00:09:07 +0300

    Add andys_chat_3.0_english/chat/kick_haupt.php

---
+++ andys_chat_3.0_english/chat/kick_haupt.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+<title>KICKING</title>
+</head>
+
+<body bgcolor="white" text="black" link="blue" vlink="purple" alink="red">
+<p><font color="#CC0000" face="Verdana"><span style="font-size:22pt;">Please enter the Nickname and for check the Identity your Admin-Password!</span></font></p>
+<form name="form1" action="killen.php?killen=1" method="post">
+    <ul>
+    <p align="left"><font face="Verdana">Username: &nbsp;&nbsp;&nbsp;<input type="text" name="username" maxlength="15" size="56"></font></p>
+        <p align="left"><font face="Verdana">Admin-Password: &nbsp;&nbsp;&nbsp;<input type="text" name="adminpasswort" size="20"></font></p>
+    <p><font face="Verdana"><input type="submit" name="formbutton1" value="KICK!"></font></p>
+    </ul>
+</form>
+<p><font face="Verdana">&nbsp;</font></p>
+<p><font face="Verdana"><a href="admin/admin.php?action=go" target="_parent">&lt;&lt;</a>Back to the Administration</font></p>
+<p><font face="Verdana"><a href="login.php" target="_parent">&lt;&lt;</a> Back to Login</font></p>
+</body>
+
+</html>
