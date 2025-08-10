065f900afe1d2f27d18e80e5bb0b0bef469030d5
me-suzy <me.suzana@gmail.com>
2025-08-11 01:06:10 +0300

    Add znews 1.1/znews 1.1/eint/00330206354072952084.php

---
+++ znews 1.1/znews 1.1/eint/00330206354072952084.php
@@ -0,0 +1,8 @@
+<? $betr="Forum - Partner";
+?><? $date="04.10.2002";
+?><? $name="Neo";
+?><? $mail="webmaster@nicis-page.de";
+?><? $text="Wenn einer interresse hta, mein Partner zu werden, dann soll er in das Forum Partner/Werbung schreiben.";
+?><? $text2="Wenn einer werbung auf meiner seite machen will, OHNE emeinen banner in seine seite einbinden zu müssen. Ich werde die Seite dann überprüfen! Vielleicht wird man auch einen kleinen betrag zahlen müssen!";
+?><? $mname="00330206354072952084";
+?>
