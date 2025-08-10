0a134bb8299e378cf7578d88601009b2f61609e9
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:20 +0300

    Add Photopost_PHP/languages/french/index.php

---
+++ Photopost_PHP/languages/french/index.php
@@ -0,0 +1,24 @@
+<?
+$Globals['pp_lang']['board_closed'] = "Nous sommes désolés mais les albums photos sont actuellement indisponibles pour cause de maintenance. Veuillez revenir plus tard. Merci.";
+$Globals['pp_lang']['home'] = "Index";
+$Globals['pp_lang']['welcomeuser'] = "Bienvenue,";
+$Globals['pp_lang']['welcome'] = "Bienvenue sur ";
+$Globals['pp_lang']['tablehead'] = "Index principal";
+$Globals['pp_lang']['recent'] = "Messages récents";
+$Globals['pp_lang']['lastday'] = "dernier jour";
+$Globals['pp_lang']['last7'] = "7 derniers jours";
+$Globals['pp_lang']['last14'] = "14 derniers jours";
+$Globals['pp_lang']['allimages'] = "toutes les images";
+$Globals['pp_lang']['category'] = "Catégorie";
+$Globals['pp_lang']['photos'] = "Photos";
+$Globals['pp_lang']['comments'] = "Commentaires";
+$Globals['pp_lang']['lastcomment'] = "Dernier commentaire";
+$Globals['pp_lang']['lastphoto'] = "Dernière photo téléchargée";
+$Globals['pp_lang']['toposter'] = "Top Posters:";
+$Globals['pp_lang']['regusers'] = "utilisateurs enregistrés,";
+$Globals['pp_lang']['posted'] = "images téléchargées et";
+$Globals['pp_lang']['postednoc'] = "posted photos";
+$Globals['pp_lang']['posts'] = "commentaires";
+$Globals['pp_lang']['views'] = "photos vues et";
+$Globals['pp_lang']['diskspace'] = "de l'espace disque.";
+?>
