ade5ee41c008c951f5d3cfd2547ad9727ea6a6f4
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:37 +0300

    Add Photopost_PHP/languages/italian/reportphoto.php

---
+++ Photopost_PHP/languages/italian/reportphoto.php
@@ -0,0 +1,13 @@
+<?
+$Globals['pp_lang']['badcall'] = "Errore nello Script.  Vai ad una immagine specifica, quindi clicca sul collegamento Modifica.";
+$Globals['pp_lang']['closed'] = "Ci spiace, ma il servizio è temporaneamente sospeso per aggiornamenti. Riprova più tardi.";
+$Globals['pp_lang']['reportphoto'] = "Segnalazione Immagine";
+$Globals['pp_lang']['tool'] = "Servizio di segnalazione";
+$Globals['pp_lang']['reportreason'] = "Motivo della segnalazione?";
+$Globals['pp_lang']['inapprop'] = "Materiale inappropriato";
+$Globals['pp_lang']['copyright'] = "Infrazione di Copyright";
+$Globals['pp_lang']['wrongcat'] = "Immagine in Categoria errata";
+$Globals['pp_lang']['other'] = "Altro";
+$Globals['pp_lang']['moreinfo'] = "Maggiori informazioni (se necessarie)?";
+$Globals['pp_lang']['webnotice'] = "La tua segnalazione è stata inviata al webmaster.";
+?>
