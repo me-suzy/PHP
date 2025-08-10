8de1d78fd9b7aa3eec09e79d9258e7d5105917f7
me-suzy <me.suzana@gmail.com>
2025-08-11 01:06:02 +0300

    Add znews 1.1/znews 1.1/admin/loeschen.php

---
+++ znews 1.1/znews 1.1/admin/loeschen.php
@@ -0,0 +1,16 @@
+<body  bgcolor="#4682B4" alink="#000000" link="#000000" vlink="#00000">
+
+<?
+
+unlink("../eint/$id.php");
+
+?>
+
+<font face="Verdana" size="2">
+
+<div align="center"><br>
+
+Eintrag gelöscht gelöscht!<br>
+
+
+
