554aaa8e43a25661074105ae847238e69784e287
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:25 +0300

    Add Photopost_PHP/languages/italian/editphoto.php

---
+++ Photopost_PHP/languages/italian/editphoto.php
@@ -0,0 +1,26 @@
+<?
+$Globals['pp_lang']['closed'] = "Ci spiace, ma il servizio è temporaneamente sospeso per aggiornamenti. Riprova più tardi.";
+$Globals['pp_lang']['noperm'] = "Spiacente, non sei autorizzato a caricare immagini, o, se hai cercato di modificare,non sei l'autore di questo documento.";
+$Globals['pp_lang']['badcall'] = "Chiamata allo script non corretta.  Vai ad una immagine specifica, quindi clicca sul collegamento di Modifica.";
+$Globals['pp_lang']['editphoto'] = "Elabora Immagine";
+$Globals['pp_lang']['editor'] = "Modifica Immagine";
+$Globals['pp_lang']['username'] = "Username";
+$Globals['pp_lang']['thumbnail'] = "Miniatura";
+$Globals['pp_lang']['rotateccw'] = "Ruota in senso anti-orario";
+$Globals['pp_lang']['rotatecw'] = "Ruota in senso orario";
+$Globals['pp_lang']['flip'] = "Rovescia Verticalmente";
+$Globals['pp_lang']['flop'] = "Rovescia orizzontalmente";
+$Globals['pp_lang']['catchange'] = "Cambi categoria? (lascia bianco per lasciare da sola)";
+$Globals['pp_lang']['edittitle'] = "Inserisci il titolo dell'immagine";
+$Globals['pp_lang']['editkey'] = "Inserisci parola-chiave:";
+$Globals['pp_lang']['editdesc'] = "Inserisci descrizione dell'immagine";
+$Globals['pp_lang']['deletechk'] = "Se vuoi <b>cancellare</b> definitivamente questa immagine, seleziona questa casella";
+$Globals['pp_lang']['careful'] = "(ATTENZIONE - una volta eseguito è irreversibile)";
+$Globals['pp_lang']['submitchg'] = "Esegui modifiche";
+$Globals['pp_lang']['nophoto'] = "Immagine non trovata nel tuo database!";
+$Globals['pp_lang']['malformed'] = "Inviato parametro non corretto!";
+$Globals['pp_lang']['deleted'] = "Immagine eliminata con successo.";
+$Globals['pp_lang']['noaction'] = "Non sei autorizzato ad eseguire questo comando!";
+$Globals['pp_lang']['success'] = "Immagine correttamente modificata.";
+$Globals['pp_lang']['allowprint'] = "Dai il consenso agli utenti di stampare questa immagine su un calendario?";
+?>
