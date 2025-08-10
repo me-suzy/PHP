68f026a669000ce73298a6ab4e518f74125667a3
me-suzy <me.suzana@gmail.com>
2025-08-11 00:56:31 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/photoalbum/conf/config.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/photoalbum/conf/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define("PHOTOALBUM_DIR", PHPWS_HOME_DIR . "images/photoalbum/");
+
+//NO SPACES in image types list
+define("PHOTOALBUM_IMAGE_TYPES", "image/jpeg,image/jpg,image/pjpeg,image/png,image/x-png");
+
+define("PHOTOALBUM_TN_WIDTH", "150");
+define("PHOTOALBUM_TN_HEIGHT", "150");
+
+define("PHPWS_PHOTOALBUM_DEBUG", 0);
+
+?>
