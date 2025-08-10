5cbb622c4faea4f4ce75d0619d04170caafa32a3
me-suzy <me.suzana@gmail.com>
2025-08-11 00:48:48 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/calendar/conf/help.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/calendar/conf/help.php
@@ -0,0 +1,12 @@
+<?php
+
+$eventtype = "Event Type";
+$eventtype_content = "There are four types of time frames you can assign to an event.
+<ul>
+<li><b>Interval</b> - when an event has a definite start and end time, choose this option.</li>
+<li><b>Starts At</b> - when an event has a definite start time but an unknown end time, choose this one.</li>
+<li><b>Deadline</b> - when the end of an event is more important than when it starts, use this one.</li>
+<li><b>All Day</b> - when an event is defined more by the day it is on than when it starts or ends (e.g. holidays)</li>
+</ul>";
+
+?>
