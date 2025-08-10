8538354548479ccffeda5f30a209afc93358d19f
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:37 +0300

    Add Photopost_PHP/languages/french/slideshow.php

---
+++ Photopost_PHP/languages/french/slideshow.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['closed'] = "Nous sommes désolés mais les albums photos sont actuellement indisponibles pour cause de maintenance. Veuillez revenir plus tard. Merci.";
+$Globals['pp_lang']['slideshow'] = "Présentation";
+$Globals['pp_lang']['delay'] = "Délai entre l'enchainement des photos?<br />(ceux qui ont une connexion lente voudront un temps d'arrêt sur l'image plus long)";
+$Globals['pp_lang']['twosec'] = "2 secondes";
+$Globals['pp_lang']['foursec'] = "4 secondes";
+$Globals['pp_lang']['eightsec'] = "8 secondes";
+$Globals['pp_lang']['tensec'] = "10 secondes";   
+$Globals['pp_lang']['startslide'] = "Démarrer la présentation";
+?>
