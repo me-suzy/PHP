d14e1a6dd1fcabb6928e2acc6dc47bdb50490c6d
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:12 +0300

    Add Photopost_PHP/languages/french/adm-photo.php

---
+++ Photopost_PHP/languages/french/adm-photo.php
@@ -0,0 +1,18 @@
+<?
+$Globals['pp_lang']['malform'] = "Paramètre mal formé passé!";
+$Globals['pp_lang']['mogrify'] = "Appel non soutenu pour modifier l'image.";
+$Globals['pp_lang']['erotate'] = "Erreur de rotation d'image!";
+$Globals['pp_lang']['erotatemed'] = "Erreur de rotation de l'image moyenne!";
+$Globals['pp_lang']['rotateccw'] = "L'image a été tournée dans le sens contraire des aiguilles d'une montre!";
+$Globals['pp_lang']['rotatecw'] = "L'image a été tournée dans le sens des aiguilles d'une montre!";
+$Globals['pp_lang']['flipped'] = "L'image a été renversée";
+$Globals['pp_lang']['flopped'] = "L'image a été supprimée!";    
+$Globals['pp_lang']['nopic'] = "Erreur: pas de photo à supprimer!";
+$Globals['pp_lang']['nophoto'] = "Photo introuvable dans la base de données!";
+$Globals['pp_lang']['delete'] = "Image supprimée";
+$Globals['pp_lang']['errorcode'] = "Erreur de code";
+$Globals['pp_lang']['noperm'] = "Vous n'avez pas la permission pour effectuer cette action!";
+$Globals['pp_lang']['badmove'] = "Vous ne pouvez pas déplacer d'images dans cette catégorie, veuillez réessayer.";
+$Globals['pp_lang']['noneed'] = "Il n'est pas utile de déplacer une image dans la même catégorie!";
+$Globals['pp_lang']['noaction'] = "Pas d'action spécifiée";
+?>
