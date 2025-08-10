cfb81d60fd34646ac555b6c8d7cf0b021215f99e
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:29 +0300

    Add Photopost_PHP/languages/spanish/editphoto.php

---
+++ Photopost_PHP/languages/spanish/editphoto.php
@@ -0,0 +1,26 @@
+<?
+$Globals['pp_lang']['closed'] = "Lo sentimos, pero nuestra galer&iacute;a se encuentra cerrada. Por favor intenta m&aacute;s tarde.";
+$Globals['pp_lang']['noperm'] = "Lo siento, no tienes permisos para colocar mensajes.<p>Si intentaste editar, puede que no seas el autor del mensaje."; 
+$Globals['pp_lang']['badcall'] = "El programa no se llam&oacute; correctamente. Navegue a una imagen espec&iacute;fica, y haz click en el bot&oacute;n editar";
+$Globals['pp_lang']['editphoto'] = "Editar Imagen";
+$Globals['pp_lang']['editor'] = "Editor de imagen";
+$Globals['pp_lang']['username'] = "Nombre de usuario";
+$Globals['pp_lang']['thumbnail'] = "Vista previa";
+$Globals['pp_lang']['rotateccw'] = "Rotar Inversamente";
+$Globals['pp_lang']['rotatecw'] = "Rotar";
+$Globals['pp_lang']['flip'] = "Voltear Vertical";
+$Globals['pp_lang']['flop'] = "Voltear Horizontal";
+$Globals['pp_lang']['catchange'] = "Cambiar categor&iacute;a? (dejar en blanco para dejarla sola)";
+$Globals['pp_lang']['edittitle'] = "Editar el t&iacute;tulo de la foto?";
+$Globals['pp_lang']['editkey'] = "Editar palabras claves:";
+$Globals['pp_lang']['editdesc'] = "Editar descripci&oacute;n";
+$Globals['pp_lang']['deletechk'] = "Si quieres <b>borrar<?b> esta imagen completamente, marque esta opci&oacute;n";
+$Globals['pp_lang']['careful'] = "(CUIDADO - una vez borrada, no se puede recuperar)";
+$Globals['pp_lang']['submitchg'] = "Enviar cambios";
+$Globals['pp_lang']['nophoto'] = "La imagen no se encontr&oacute; en la base de datos!";
+$Globals['pp_lang']['malformed'] = "Par&aacute;metros err&oacute;neos pasados!";
+$Globals['pp_lang']['deleted'] = "Has borrado la imagen exitosamente.";
+$Globals['pp_lang']['noaction'] = "No tienes permiso para realizar esta acci&oacute;n!";
+$Globals['pp_lang']['success'] = "Has editado la imagen exitosamente.";
+$Globals['pp_lang']['allowprint'] = "Quieres permitir que las personas impriman tu imagen en un calendario?";
+?>
