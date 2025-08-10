993b787247c91f215ccbcc5ba071e125c11e4ff3
me-suzy <me.suzana@gmail.com>
2025-08-11 01:06:18 +0300

    Add znews 1.1/znews 1.1/eint/73205778360073194839.php

---
+++ znews 1.1/znews 1.1/eint/73205778360073194839.php
@@ -0,0 +1,8 @@
+<? $betr="Neues Forum";
+?><? $date="29.09.2002";
+?><? $name="Neo";
+?><? $mail="webmaster@nicis-page.de";
+?><? $text="Jetzt ist das Forum online!";
+?><? $text2="Jetzt könnt ihr Fragen an mich stellen, oder sachen unter euch austauschen! ";
+?><? $mname="73205778360073194839";
+?>
