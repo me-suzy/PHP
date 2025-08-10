0c77fc3cbd0bc78f07e181703b50acb028f343a8
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:31 +0300

    Add Photopost_PHP/languages/french/reportphoto.php

---
+++ Photopost_PHP/languages/french/reportphoto.php
@@ -0,0 +1,13 @@
+<?
+$Globals['pp_lang']['badcall'] = "Script not called correctly.  Choisissez une photo et cliquez sur le lien éditer.";
+$Globals['pp_lang']['closed'] = "Nous sommes désolés mais les albums photos sont actuellement indisponibles pour cause de maintenance. Veuillez revenir plus tard. Merci.";
+$Globals['pp_lang']['reportphoto'] = "Rapport sur les photos";
+$Globals['pp_lang']['tool'] = "Outil de rapport";
+$Globals['pp_lang']['reportreason'] = "Raisons du rapport sur cette photo?";
+$Globals['pp_lang']['inapprop'] = "Photo inappropriée";
+$Globals['pp_lang']['copyright'] = "Violation des droit d'auteur";
+$Globals['pp_lang']['wrongcat'] = "Image placée dans une mauvaise catégorie";
+$Globals['pp_lang']['other'] = "Autre";
+$Globals['pp_lang']['moreinfo'] = "Plus d'information (si nécessaire)?";
+$Globals['pp_lang']['webnotice'] = "Votre rapport sur cette photo a été envoyé au webmestre.";
+?>
