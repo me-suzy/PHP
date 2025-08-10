28840485632422920a8378c0d59fc8f689f195e6
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:59 +0300

    Add Photopost_PHP/languages/finnish/reportphoto.php

---
+++ Photopost_PHP/languages/finnish/reportphoto.php
@@ -0,0 +1,13 @@
+<?
+$Globals['pp_lang']['badcall'] = "Scriptiä ei kutsuttu oikein.  Liiku oikean kuvan ylle, ja muokkaa linkkiä klikkaamalla kuvaa.";
+$Globals['pp_lang']['closed'] = "Olemme pahoillamme, mutta Photo Boards on tällä hetkellä huollettavana eikä kuvia ole saatavilla. Yrittäkää myöhemmin uudelleen.";
+$Globals['pp_lang']['reportphoto'] = "Lähetä palaute";
+$Globals['pp_lang']['tool'] = "palaute";
+$Globals['pp_lang']['reportreason'] = "Palautteen syy";
+$Globals['pp_lang']['inapprop'] = "Sopimatonta materiaalia";
+$Globals['pp_lang']['copyright'] = "Kopiointisuojauksen loukkaaminen";
+$Globals['pp_lang']['wrongcat'] = "Kuva on väärässä kategoriassa";
+$Globals['pp_lang']['other'] = "Muu, mikä?";
+$Globals['pp_lang']['moreinfo'] = "Tarkempi kuvaus palautteesta (ei pakollinen)";
+$Globals['pp_lang']['webnotice'] = "Kuvasta josta olet raportoinnut, on lähetetty viesti ylläpidolle.";
+?>
