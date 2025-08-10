3ee4df935b6c43d6fd7215e848c33fec14d50803
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:55 +0300

    Add Photopost_PHP/languages/norwegian/ecard.php

---
+++ Photopost_PHP/languages/norwegian/ecard.php
@@ -0,0 +1,23 @@
+<?
+$Globals['pp_lang']['disabled'] = "e-Cards er deaktivert";
+$Globals['pp_lang']['larger'] = "Vis Større Bilde";
+$Globals['pp_lang']['smaller'] = "Vis Mindre Bilde";
+$Globals['pp_lang']['send'] = "Send bilde som e-Card";
+$Globals['pp_lang']['view'] = "Vis ditt e-Card";
+$Globals['pp_lang']['ecard'] = "eCard";
+$Globals['pp_lang']['home'] = "Hjem";
+$Globals['pp_lang']['name'] = "Navn";
+$Globals['pp_lang']['email'] = "Email";
+$Globals['pp_lang']['info'] = "Mottakers Info";
+$Globals['pp_lang']['alter'] = "Om du ikke vet mottaker´s email adresse, bruk deres navn istedet:<br /><br />Medlemsnavn";
+$Globals['pp_lang']['subjgreet'] = "e-Card Tema/Hilsen";
+$Globals['pp_lang']['subject'] = "Tema";
+$Globals['pp_lang']['yourmess'] = "Din e-card beskjed";
+$Globals['pp_lang']['yourecard'] = "Ditt e-Card";
+$Globals['pp_lang']['send'] = "Send ditt e-Card";
+$Globals['pp_lang']['noemail'] = "Du fylte ikke inn mottaker´s email. Vennligst gå tilbake og fyll ut med enten mottakers email adresse eller medlemsnavn.";
+$Globals['pp_lang']['blank'] = "Din beskjed var blank.";
+$Globals['pp_lang']['badname'] = "Beklager, medlemsnavn ble ikke gjenkjent.";
+$Globals['pp_lang']['hello'] = "Heisann";
+$Globals['pp_lang']['cardsent'] = "Ditt kort har blitt sendt!";
+?>
