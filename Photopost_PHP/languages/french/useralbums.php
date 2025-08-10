77577b7f555eb2c5e5d65d9f1e0575e85cdccc27
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:40 +0300

    Add Photopost_PHP/languages/french/useralbums.php

---
+++ Photopost_PHP/languages/french/useralbums.php
@@ -0,0 +1,27 @@
+<?
+$Globals['pp_lang']['closed'] = "Nous sommes désolés mais les albums photos sont actuellement indisponibles pour cause de maintenance. Veuillez revenir plus tard. Merci.";
+$Globals['pp_lang']['noperm'] = "Désolé mais vous n'avez pas la permission de créer des albums.";
+$Globals['pp_lang']['noverify'] = "Désolé mais vous n'avez pas encore activé votre compte.<p>Vous devez l'activez avant de pouvoir gérer votre album.";
+$Globals['pp_lang']['nopa'] = "Désolé, les albums personnels ne sont pas activés.";
+$Globals['pp_lang']['useralbums'] = "Albums de l'utilisateur";
+$Globals['pp_lang']['editor'] = "L'éditeur d'album de Photopost.";
+$Globals['pp_lang']['addalbum'] = "Ajouter un album";
+$Globals['pp_lang']['errordir'] = "Erreur en créant le répertoire $newdir. Veuillez notifier l'administrateur système.";
+$Globals['pp_lang']['complete'] = "Traitement terminé!";
+$Globals['pp_lang']['adduser'] = "Ajouter l'album de l'utilisateur";
+$Globals['pp_lang']['addaalbum'] = "PhotoPost Ajouter un album";
+$Globals['pp_lang']['albumname'] = "Nom de l'album";
+$Globals['pp_lang']['albumdesc'] = "Description de l'album";
+$Globals['pp_lang']['isprivate'] = "Est-ce que l'album est privé?";
+$Globals['pp_lang']['newalbum'] = "Nouvel album.";
+$Globals['pp_lang']['complete'] = "Traitement terminé!";
+$Globals['pp_lang']['editalbum'] = "Editer l'album de l'utilisateur";
+$Globals['pp_lang']['savechanges'] = "Sauver les modifications.";
+$Globals['pp_lang']['malformed'] = "Malformed parameter passed!";
+$Globals['pp_lang']['invalidid'] = "Invalid album ID";
+$Globals['pp_lang']['deleteuser'] = "Supprimer l'album de l'utilisateur";
+$Globals['pp_lang']['delete1'] = "Vous êtes sur le point de supprimer l'album";
+$Globals['pp_lang']['delete2'] = "et <b>TOUTES LES PHOTOS ET COMMENTAIRES</b> de cette album. <p>";
+$Globals['pp_lang']['delete3'] = "Etes-vous certain de faire cela?";
+$Globals['pp_lang']['imsure'] = "J'en suis certain, supprimez l'album.";
+?>
