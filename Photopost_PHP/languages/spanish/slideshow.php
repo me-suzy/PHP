f44ade7caee6386df48095b2bf8b2f4394274c26
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:48 +0300

    Add Photopost_PHP/languages/spanish/slideshow.php

---
+++ Photopost_PHP/languages/spanish/slideshow.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['closed'] = "Lo sentimos, pero nuestra galer&iacute;a se encuentra cerrada. Por favor intenta m&aacute;s tarde.";
+ $Globals['pp_lang']['slideshow'] = "Recorrido";
+$Globals['pp_lang']['delay'] = "Retrazo entre im&aacute;genes?<br />(conexiones lentas quieren m&aacute;s retrazo)";
+$Globals['pp_lang']['twosec'] = "2 segundos";
+$Globals['pp_lang']['foursec'] = "4 segundos";
+$Globals['pp_lang']['eightsec'] = "8 segundos";
+$Globals['pp_lang']['tensec'] = "10 segundos";   
+$Globals['pp_lang']['startslide'] = "Comenzar recorrido";
+?>
