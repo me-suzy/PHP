ab4e2cf176df93dd89c75bc6f9acb569be032331
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:28 +0300

    Add Photopost_PHP/languages/spanish/ecard.php

---
+++ Photopost_PHP/languages/spanish/ecard.php
@@ -0,0 +1,23 @@
+<?
+$Globals['pp_lang']['disabled'] = "Las e-Cards est&aacute;n deshabilitadas";
+$Globals['pp_lang']['larger'] = "Ver Imagen Grande";
+$Globals['pp_lang']['smaller'] = "Ver Imagen Peque&ntilde;a";
+$Globals['pp_lang']['send'] = "Enviar imagen como una e-Card";
+$Globals['pp_lang']['view'] = "Ver su e-Card";
+$Globals['pp_lang']['ecard'] = "eCard";
+$Globals['pp_lang']['home'] = "Inicio";
+$Globals['pp_lang']['name'] = "Nombre";
+$Globals['pp_lang']['email'] = "Email";
+$Globals['pp_lang']['info'] = "Informaci&oacute;n de Recipiente";
+$Globals['pp_lang']['alter'] = "Si no conoces la direcci&oacute;n de email del recipiente, entra su nombre de usuario:<br /><br />Nombre de usuario";
+$Globals['pp_lang']['subjgreet'] = "Saludo en el e-Card";
+$Globals['pp_lang']['subject'] = "T&iacute;tulo";
+$Globals['pp_lang']['yourmess'] = "Tu mensaje en el e-Card";
+$Globals['pp_lang']['yourecard'] = "Su e-Card";
+$Globals['pp_lang']['send'] = "Enviar el e-Card";
+$Globals['pp_lang']['noemail'] = "No entraste un email como recipiente.  Por favor devu&eacute;lvete y escribe un recipiente o un nombre de usuario.";
+$Globals['pp_lang']['blank'] = "Su mensaje est&aacute; en blanco.";
+$Globals['pp_lang']['badname'] = "Lo sentimos, el nombre de usuario no pudo ser encontrado.";
+$Globals['pp_lang']['hello'] = "Hola";
+$Globals['pp_lang']['cardsent'] = "Su e-Card fue enviada!!";
+?>
