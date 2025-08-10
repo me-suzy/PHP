6eb2d81b9dd5a3c72301578ac4bf48ad7dd11c44
me-suzy <me.suzana@gmail.com>
2025-08-11 00:54:33 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/pagemaster/conf/config.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/pagemaster/conf/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This is the config file for the pagemaster module.  Do not
+ * change these values unless you are sure you know what you
+ * are doing.
+ *
+ * @version $Id: config.php,v 1.3 2002/10/30 18:21:56 adam Exp $
+ * @author  Adam Morton <adam@NOSPAM.tux.appstate.edu>
+ */
+$image_directory = "images/pagemaster/";
+$template_directory = "mod/pagemaster/templates/";
+
+?>
