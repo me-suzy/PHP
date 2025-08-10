76600f9046973abc7ccea27ba10e764837c2d479
me-suzy <me.suzana@gmail.com>
2025-08-11 00:51:29 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/help/conf/help_config.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/help/conf/help_config.php
@@ -0,0 +1,6 @@
+<?php
+
+// This is the graphic that appers for any new window help items.
+$help_graphic = "<img src=\"http://". PHPWS_SOURCE_HTTP ."mod/help/img/help_icon.gif\" border=\"0\" alt=\"Get Help\" />";
+
+?>
