961a0388f391cfa1e132d3d504568454019f76e7
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:05 +0300

    Add Photopost_PHP/languages/hungarian/reportphoto.php

---
+++ Photopost_PHP/languages/hungarian/reportphoto.php
@@ -0,0 +1,13 @@
+<?
+$Globals['pp_lang']['badcall'] = "Szkript behívása hibás.  Menjen egy adott fotóhoz, majd kattintson a szerkesztés linkre!";
+$Globals['pp_lang']['closed'] = "Nagyon sajnáljuk, de a Képtár technikai okok miatt átmenetileg zárva tart. Kérjük, próbálkozzon késõbb!";
+$Globals['pp_lang']['reportphoto'] = "Jelentés a fotóról";
+$Globals['pp_lang']['tool'] = "Jelentés szerkesztõ";
+$Globals['pp_lang']['reportreason'] = "Jelentés oka?";
+$Globals['pp_lang']['inapprop'] = "Nem megfelelõ anyag";
+$Globals['pp_lang']['copyright'] = "Jogtalan sokszorosítás";
+$Globals['pp_lang']['wrongcat'] = "Kép rossz kategóriában";
+$Globals['pp_lang']['other'] = "Egyéb";
+$Globals['pp_lang']['moreinfo'] = "Több információ (ha szükséges)?";
+$Globals['pp_lang']['webnotice'] = "Az Ön által készített jelentésrõl az adminisztrátor értesítést fog kapni.";
+?>
