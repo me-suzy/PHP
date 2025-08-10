270a373501e57c5bd41607b81e2eae90f5c774b9
me-suzy <me.suzana@gmail.com>
2025-08-11 00:59:44 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/themes/Default/theme.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/themes/Default/theme.php
@@ -0,0 +1,11 @@
+<?php
+
+// This is a sample theme include file. Just insert the functions you want to appear
+// in your theme and place them into the $THEME['LABEL_NAME'] array. Then just create
+// a {EXAMPLE} tag in your theme.tpl file.
+// This file will NOT be included if layout's config.php file prohibits it.
+
+// $THEME["EXAMPLE"] = "This is a sample of including php code into your theme. <br />
+//                      Uncomment this line then look at the bottom of your Default theme page.";
+
+?>
