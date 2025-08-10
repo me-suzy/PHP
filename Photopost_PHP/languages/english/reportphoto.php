b5a6dff3c0ba2577894e4e6e7dfa52f531a6676d
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:27 +0300

    Add Photopost_PHP/languages/english/reportphoto.php

---
+++ Photopost_PHP/languages/english/reportphoto.php
@@ -0,0 +1,13 @@
+<?
+$Globals['pp_lang']['badcall'] = "Script not called correctly.  Navigate to a specific photo, then click on the edit link.";
+$Globals['pp_lang']['closed'] = "We're sorry, but our Photo Boards are currently down for maintainence. Please try again later.";
+$Globals['pp_lang']['reportphoto'] = "Report Photo";
+$Globals['pp_lang']['tool'] = "Reporting Tool";
+$Globals['pp_lang']['reportreason'] = "Reason for reporting?";
+$Globals['pp_lang']['inapprop'] = "Inappropriate material";
+$Globals['pp_lang']['copyright'] = "Copyright Infringement";
+$Globals['pp_lang']['wrongcat'] = "Image in wrong Category";
+$Globals['pp_lang']['other'] = "Other";
+$Globals['pp_lang']['moreinfo'] = "More information (if needed)?";
+$Globals['pp_lang']['webnotice'] = "The photo you reported has generated a notice to the webmaster.";
+?>
