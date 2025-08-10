bd9ad61130fbd55b811c2dea81e1c48fde174b64
me-suzy <me.suzana@gmail.com>
2025-08-11 00:55:44 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/phatform/conf/convert.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/phatform/conf/convert.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Setup database information for location of old phatform installation */
+define("OLD_DBHOST", "localhost");
+define("OLD_DBNAME", "dbname");
+define("OLD_DBUSER", "dbuser");
+define("OLD_DBPASS", "dbpass");
+define("OLD_PREFIX", "");
+
+/* Setup database information for location of new phatform installation */
+define("NEW_DBHOST", "localhost");
+define("NEW_DBNAME", "dbname");
+define("NEW_DBUSER", "dbname");
+define("NEW_DBPASS", "dbpass");
+define("NEW_PREFIX", "");
+
+?>
