7f82900652842c1ef1e2865993197c07c28a6c8d
me-suzy <me.suzana@gmail.com>
2025-08-11 00:53:18 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/menuman/conf/help.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/menuman/conf/help.php
@@ -0,0 +1,21 @@
+<?php
+
+$menu_spacer = "Menu Spacer";
+$menu_spacer_content = "The menu spacer value represents the number of spaces that will be added to each nested level of the menu.";
+
+$menu_indent = "Menu Indent";
+$menu_indent_content = "Menu indent can either be a character or an image.  This item will be placed before each link in the menu.  The {ITEM} tag must appear in the template for it to appear.";
+
+$menu_char = "Character Options";
+$menu_char_content = "These options allow you to choose the character that will show up before each link in the menu.  You can also choose an activity color.  The character will turn that color when the menu link is active";
+
+$menu_image = "Image Options";
+$menu_image_content = "Three different images can be used for the menu.  An active image for the active link.  An open image for the parent link that is expanded.  And a default for all other links.";
+
+$menu_template = "Menu Templating";
+$menu_template_content = "There are two templating choices, vertical and horizontal.  Two of templates that come with menuman give some example of what can be done.  The default template is the vertical example and the tabular template is the horizontal example.";
+
+$allow_view = "Allow View";
+$allow_view_content = "The allow view option allows you to choose when the menu you are creating will show up on the page. Choose which modules need to be active in order for the menu to appear.";
+
+?>
