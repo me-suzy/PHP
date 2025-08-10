9eece7e31f931f02cf362913691c26271fc1f415
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:02 +0300

    Add Photopost_PHP/languages/norwegian/login.php

---
+++ Photopost_PHP/languages/norwegian/login.php
@@ -0,0 +1,5 @@
+<?
+$Globals['pp_lang']['loginok'] = "Innlogging vellykket!";
+$Globals['pp_lang']['loginbad'] = "Mislykket innlogging.  Vennligst sjekk ditt brukernavn og passord, eller";
+$Globals['pp_lang']['loginbad2'] = "registrer";
+?>
