2fd790e2cc4572fbb95c4caf80b2edbb0c67fca9
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:21 +0300

    Add Photopost_PHP/languages/spanish/adm-cinc.php

---
+++ Photopost_PHP/languages/spanish/adm-cinc.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['delete'] = "Borrar";
+$Globals['pp_lang']['edit'] = "Editar";
+$Globals['pp_lang']['addsubcat'] = "Agregar Subcategor&iacute;a";
+$Globals['pp_lang']['empty'] = "Vaciar";
+$Globals['pp_lang']['parenter1'] = "Intentaste asociar un foro con uno de sus hijos.";
+$Globals['pp_lang']['parenter2'] = "Intentaste asociar un foro con s&iacute; mismo.";
+$Globals['pp_lang']['prlink'] = "Link privado para acceder a &aacute;lbum";
+$Globals['pp_lang']['order'] = "Ordenar";
+?>
