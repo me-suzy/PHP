b3d3fb07afd53d28f066e84d9db314028463b41e
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:37 +0300

    Add Photopost_PHP/languages/finnish/addfav.php

---
+++ Photopost_PHP/languages/finnish/addfav.php
@@ -0,0 +1,8 @@
+<?
+$Globals['pp_lang']['badcall'] = "Scriptiä kutsuttiin väärin. Mene kyseiseen kuvaan, valitse Lisää Suosikkeihin linkki.";
+$Globals['pp_lang']['closed'] = "Olemme pahoillamme, mutta Photo Boards on tällä hetkellä huollettavana eikä kuvia ole saatavilla. Yrittäkää myöhemmin uudelleen.";
+$Globals['pp_lang']['noreg'] = "Vain rekisteröityneillä käyttäjäjillä voi olla suosikkikuvia.";
+$Globals['pp_lang']['added'] = "Kuva on lisätty suosikkialbumiisi.";
+$Globals['pp_lang']['malform'] = "Väärä kutsu addfav.php scriptiin";
+$Globals['pp_lang']['removed'] = "Kuva on poistettu suosikkialbumistasi!";
+?>
