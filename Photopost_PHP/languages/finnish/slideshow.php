da940867d88f6553f4c9a0e4de7f6c80408f41a5
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:04 +0300

    Add Photopost_PHP/languages/finnish/slideshow.php

---
+++ Photopost_PHP/languages/finnish/slideshow.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['closed'] = "Olemme pahoillamme, mutta Photo Boards on tällä hetkellä huollettavana eikä kuvia ole saatavilla. Yrittäkää myöhemmin uudelleen.";
+$Globals['pp_lang']['slideshow'] = "Slide Show";
+$Globals['pp_lang']['delay'] = "Aikaviive kuville?<br />(Hitaampi yhteys vaatii pidemmän aikaviiveen)";
+$Globals['pp_lang']['twosec'] = "2 sekuntia";
+$Globals['pp_lang']['foursec'] = "4 sekuntia";
+$Globals['pp_lang']['eightsec'] = "8 sekuntia";
+$Globals['pp_lang']['tensec'] = "10 sekuntia";   
+$Globals['pp_lang']['startslide'] = "Aloita Slideshow";
+?>
