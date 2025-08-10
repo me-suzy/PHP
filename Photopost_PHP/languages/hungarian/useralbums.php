cdf3eecc8022a90cbd9157d8c1cca877c46aff44
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:13 +0300

    Add Photopost_PHP/languages/hungarian/useralbums.php

---
+++ Photopost_PHP/languages/hungarian/useralbums.php
@@ -0,0 +1,27 @@
+<?
+$Globals['pp_lang']['closed'] = "Nagyon sajnáljuk, de a Képtár technikai okok miatt átmenetileg zárva tart. Kérjük, próbálkozzon késõbb!";
+$Globals['pp_lang']['noperm'] = "Sajnáljuk, Önnek nincs jogosultsága albumok létrehozására.";
+$Globals['pp_lang']['noverify'] = "Sajnáljuk, de nem még nem erõsítette meg regisztrációját.<p>Ennek meg kell történnie, hogy gondozhassa albumát.";
+$Globals['pp_lang']['nopa'] = "Sajnáljuk, a személyes albumok nincsenek engedélyezve.";
+$Globals['pp_lang']['useralbums'] = "Felhasználók albumai";
+$Globals['pp_lang']['editor'] = "Képtár Albumszerkesztõ";
+$Globals['pp_lang']['addalbum'] = "Album létrehozása";
+$Globals['pp_lang']['errordir'] = "Hiba új könyvtár létrehozásakor $newdir. Kérjük, értesítse az adminisztrátort.";
+$Globals['pp_lang']['complete'] = "Feldolgozás kész!";
+$Globals['pp_lang']['adduser'] = "Felhasználói album létrehozása";
+$Globals['pp_lang']['addaalbum'] = "Képtár Album hozzáadása";
+$Globals['pp_lang']['albumname'] = "Album Neve";
+$Globals['pp_lang']['albumdesc'] = "Album Leírása";
+$Globals['pp_lang']['isprivate'] = "Személyes Album";
+$Globals['pp_lang']['newalbum'] = "Új Album";
+$Globals['pp_lang']['complete'] = "Feldolgozás kész!";
+$Globals['pp_lang']['editalbum'] = "Felhasználó Albumának Szerkesztése";
+$Globals['pp_lang']['savechanges'] = "Változások mentése";
+$Globals['pp_lang']['malformed'] = "Hibás paraméter elhagyva.";
+$Globals['pp_lang']['invalidid'] = "Érvénytelen album azonosító";
+$Globals['pp_lang']['deleteuser'] = "Felhasználó Albumának Törlése ";
+$Globals['pp_lang']['delete1'] = "Ön a következõ nevû album törléséhez készül";
+$Globals['pp_lang']['delete2'] = "és<b>MINDEN FOTÓ ÉS VÉLEMÉNY</b> törlõdik az albumban.<p>";
+$Globals['pp_lang']['delete3'] = "Biztos, hogy ezt akarja?";
+$Globals['pp_lang']['imsure'] = "Biztos, töröld az albumot.";
+?>
