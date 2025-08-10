e753181c9e6140c994bbb495398238775a9f0271
me-suzy <me.suzana@gmail.com>
2025-08-11 00:58:12 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/skeleton/conf/layout.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/skeleton/conf/layout.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This is a skeleton layout configuration file.  Edit it to be
+ * used with your module.
+ *
+ * $Id: layout.php,v 1.2 2003/07/02 18:18:05 adam Exp $
+ */
+
+/* Adding a content variable for display in the body */
+$layout_info[] = array ("content_var"=>"CNT_skeleton",
+			"transfer_var"=>"body");
+
+?>
