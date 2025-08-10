2011f01e2631ffc5bb7841affde0239f60934a0c
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:05 +0300

    Add Photopost_PHP/languages/english/addfav.php

---
+++ Photopost_PHP/languages/english/addfav.php
@@ -0,0 +1,8 @@
+<?
+$Globals['pp_lang']['badcall'] = "Script not called correctly.  Navigate to a specific photo, then click on the Add to Favorites link.";
+$Globals['pp_lang']['closed'] = "We're sorry, but our Photo Boards are currently down for maintainence. Please try again later.";
+$Globals['pp_lang']['noreg'] = "Only registered users can have favorite photos.";
+$Globals['pp_lang']['added'] = "The image has been added to your Favorites album";
+$Globals['pp_lang']['malform'] = "Malformed call to addfav.php";
+$Globals['pp_lang']['removed'] = "The image has been removed from your Favorites album!";
+?>
