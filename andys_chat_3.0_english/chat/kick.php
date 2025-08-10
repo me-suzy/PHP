a740ee765cdcf53d55ad928450de5c442e2e886a
me-suzy <me.suzana@gmail.com>
2025-08-11 00:09:06 +0300

    Add andys_chat_3.0_english/chat/kick.php

---
+++ andys_chat_3.0_english/chat/kick.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>KICKING</title>
+</head>
+<frameset rows="1*" cols="21%, 79%">
+<frame name="left" scrolling="auto" marginwidth="10" marginheight="14" src="aktuell.php">
+<frame name="right" scrolling="auto" marginwidth="10" marginheight="14" src="kick_haupt.php">
+<noframes>
+<body bgcolor="#FFFFFF" text="#000000" link="#0000FF" vlink="#800080" alink="#FF0000">
+
+<p>Diese Seite enthält Frames. Sie benötigen einen Browser, der Frames unterstützt, um diese Seite anzeigen zu können.</p>
+</body>
+</noframes>
+</frameset>
+</html>
