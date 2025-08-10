471362a1890c6322608965cc40b82707ba67146c
me-suzy <me.suzana@gmail.com>
2025-08-11 00:55:22 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/phatform/boost/uninstall.sql

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/phatform/boost/uninstall.sql
@@ -0,0 +1,8 @@
+DROP TABLE mod_phatform_forms;
+DROP TABLE mod_phatform_options;
+DROP TABLE mod_phatform_textfield;
+DROP TABLE mod_phatform_textarea;
+DROP TABLE mod_phatform_dropbox;
+DROP TABLE mod_phatform_multiselect;
+DROP TABLE mod_phatform_radiobutton;
+DROP TABLE mod_phatform_checkbox;
