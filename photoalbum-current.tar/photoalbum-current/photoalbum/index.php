0185e3f2a4a46c503b58a3fb4cde4141f052d718
me-suzy <me.suzana@gmail.com>
2025-08-11 00:16:49 +0300

    Add photoalbum-current.tar/photoalbum-current/photoalbum/index.php

---
+++ photoalbum-current.tar/photoalbum-current/photoalbum/index.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @version $Id: index.php,v 1.9 2003/07/23 20:57:48 steven Exp $
+ * @author  Steven Levin <steven at NOSPAM tux dot appstate dot edu>
+ */
+
+if(!isset($GLOBALS['core'])) {
+  header("Location: ../..");
+  exit();
+}
+
+$CNT_photoalbum['content'] = NULL;
+
+if(!isset($_SESSION['PHPWS_AlbumManager']) && (isset($_REQUEST['module']) && $_REQUEST['module'] == "photoalbum")) {
+  $_SESSION['PHPWS_AlbumManager'] = new PHPWS_AlbumManager;
+}
+
+if(isset($_REQUEST['PHPWS_AlbumManager_op'])) {
+  $_SESSION['PHPWS_AlbumManager']->managerAction();
+  $_SESSION['PHPWS_AlbumManager']->action();
+}
+
+if(isset($_REQUEST['PHPWS_Album_op']) && isset($_SESSION['PHPWS_AlbumManager']->album)) {
+  $_SESSION['PHPWS_AlbumManager']->album->action();
+}
+
+if(isset($_REQUEST['PHPWS_Photo_op']) && isset($_SESSION['PHPWS_AlbumManager']->album->photo)) {
+  $_SESSION['PHPWS_AlbumManager']->album->photo->action();
+}
+
+if(isset($_REQUEST['module']) && ($_REQUEST['module'] != "photoalbum")) {
+  $_SESSION['PHPWS_AlbumManager'] = NULL;
+  unset($_SESSION['PHPWS_AlbumManager']);
+}
+
+?>
