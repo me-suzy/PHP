03bd5774ebd9709392535a77671bed130e2098d4
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:41 +0300

    Add Photopost_PHP/languages/finnish/bulkupload.php

---
+++ Photopost_PHP/languages/finnish/bulkupload.php
@@ -0,0 +1,25 @@
+<?
+$Globals['pp_lang']['closed'] = "Olemme pahoillamme, mutta Photo Boards on tällä hetkellä huollettavana eikä kuvia ole saatavilla. Yrittäkää myöhemmin uudelleen.";
+$Globals['pp_lang']['noupload'] = "Käyttäjän tallennukset eivät ole sallittuja";
+$Globals['pp_lang']['noperm'] = "Olemme pahoillamme, mutta sinulla ei ole oikeutta tallentaa kuvia.";
+$Globals['pp_lang']['noverify'] = "Olemme pahoillamme, mutta sinä et ole vahvistanut käyttäjätunnustasi.<p>Vahvista käyttäjätunnuksesi ennen jatkamista.";
+$Globals['pp_lang']['processing'] = "Käsittelee kuvalistaa";
+$Globals['pp_lang']['bulkuploads'] = "Massatallennus";
+$Globals['pp_lang']['photoadd'] = "Lisää kuvia";
+$Globals['pp_lang']['addselect'] = "Lisää Kuvia - Valitse Kuvat / Määrittele Ominsaisuudet";
+$Globals['pp_lang']['image'] = "Kuva";
+$Globals['pp_lang']['add'] = "Lisää";
+$Globals['pp_lang']['category'] = "Kategoria";
+$Globals['pp_lang']['optional'] = "Lisäteksti";
+$Globals['pp_lang']['title'] = "Aihe";
+$Globals['pp_lang']['desc'] = "Kuvaus";
+$Globals['pp_lang']['nomore'] = "Kuvia ei löytynyt lisää.";
+$Globals['pp_lang']['clickhere'] = "Paina tästä palataksesi päävalikkoon.";
+$Globals['pp_lang']['show'] = "Näytä pikkukuvat?";
+$Globals['pp_lang']['thumbs'] = "Pikkukuvien määrä?";
+$Globals['pp_lang']['proc'] = "Tallenna";
+$Globals['pp_lang']['preppro'] = "Valmistelee seuraavan kuvan suorittamista";
+$Globals['pp_lang']['images'] = "kuvat";
+$Globals['pp_lang']['click'] = "paina tästä";
+$Globals['pp_lang']['notrefresh'] = "jos kuva ei lataudu.";
+?>
