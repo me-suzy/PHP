3e30c75739e44f143097bac899418623997c4aad
me-suzy <me.suzana@gmail.com>
2025-08-11 00:50:28 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/faq/conf/controlpanel.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/faq/conf/controlpanel.php
@@ -0,0 +1,14 @@
+<?php
+$image['name'] = "faq.png";
+$image['alt'] = "Author: Darren Greene";
+
+$link[0] = array ("label"=>"Frequently Asked Questions",
+		  "module"=>"faq",
+		  "url"=>"index.php?module=faq&amp;FAQ_op=menu",
+		  "image"=>$image,
+		  "admin"=>TRUE,
+		  "description"=>"Provides an interface to manage a list of frequently asked questions.",
+		  "tab"=>"content");
+
+
+?>
