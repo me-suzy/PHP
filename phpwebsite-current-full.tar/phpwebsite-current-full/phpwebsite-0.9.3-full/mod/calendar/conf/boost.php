73b36d28e62c14a1dea738116c0d374ba245d1ce
me-suzy <me.suzana@gmail.com>
2025-08-11 00:48:43 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/calendar/conf/boost.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/calendar/conf/boost.php
@@ -0,0 +1,17 @@
+<?php
+$mod_title = "calendar";
+$mod_pname = "Calendar";
+$mod_directory = "calendar";
+$mod_filename = "index.php";
+$priority = 50;
+$admin_op = "&calendar[admin]=admin_menu";
+$allow_view = "all";
+$user_mod = 0;
+$admin_mod = 1;
+$deity_mod = 0;
+$mod_icon = "calendar.gif";
+$mod_class_files = array("Calendar.php");
+$active = "on";
+$version = "3.4.2";
+$branch_allow = 1;
+?>
