efa5aa440168c5fbd14f64e61ea0ff702a625121
me-suzy <me.suzana@gmail.com>
2025-08-11 00:53:20 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/menuman/conf/layout.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/menuman/conf/layout.php
@@ -0,0 +1,17 @@
+<?php
+
+$layout_info[] = array (
+			"content_var"=>"CNT_menuman_options", 
+			"transfer_var"=>"body", 
+			);
+
+$layout_info[] = array (
+			"content_var"=>"CNT_menuman_add", 
+			"transfer_var"=>"bottom", 
+			);
+
+$layout_info[] = array (
+			"content_var"=>"CNT_menuman_1", 
+			"transfer_var"=>"left_col_top", 
+			);
+?>
