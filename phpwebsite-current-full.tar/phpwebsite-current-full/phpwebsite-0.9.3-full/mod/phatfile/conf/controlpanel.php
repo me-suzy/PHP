da55705f1e1ed6e66aacbb4d58f6a0bd5270218b
me-suzy <me.suzana@gmail.com>
2025-08-11 00:55:04 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/phatfile/conf/controlpanel.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/phatfile/conf/controlpanel.php
@@ -0,0 +1,14 @@
+<?php
+
+$image["name"] = "phatfile.png";
+$image["alt"] = "Author: Adam Morton";
+
+$link[] = array ("label"=>"Document Manager",
+		 "module"=>"phatfile",
+		 "url"=>"index.php?module=phatfile&FILE_MAN_OP=Main",
+		 "image"=>$image,
+		 "admin"=>TRUE,
+		 "description"=>"Go here to post files and documents that your site visitors might be interested in.",
+		 "tab"=>"content");
+
+?>
