80e4d7d8ab05521852befa4c1cd4e1a815756577
me-suzy <me.suzana@gmail.com>
2025-08-11 01:06:14 +0300

    Add znews 1.1/znews 1.1/eint/29982803010426450834.php

---
+++ znews 1.1/znews 1.1/eint/29982803010426450834.php
@@ -0,0 +1,8 @@
+<? $betr="Poll/Design";
+?><? $date="03.10.2002";
+?><? $name="Neo";
+?><? $mail="webmaster@nicis-page.de";
+?><? $text="Ich habe einen Poll installiert! Und da schon so viele gegen das neue design gesprochen habe, wollte ich willsen, warum! Bitte schreibt doch ins Fpum, was euch nicht gefällt!!";
+?><? $text2="BITTE!";
+?><? $mname="29982803010426450834";
+?>
