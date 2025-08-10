b2b1ff2bc1fad1e2766926b9220a6d6b69e2b784
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:32 +0300

    Add Photopost_PHP/languages/spanish/login-inc.php

---
+++ Photopost_PHP/languages/spanish/login-inc.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['login'] = "Identificar";
+$Globals['pp_lang']['enterup'] = "Por favor entra tu nombre de usuario y password.";
+$Globals['pp_lang']['username'] = "Nombre de usuario";
+$Globals['pp_lang']['register'] = "Registrar";
+$Globals['pp_lang']['password'] = "Password";
+$Globals['pp_lang']['forgot'] = "Olvidaste el password?";
+$Globals['pp_lang']['submit'] = "Enviar";
+$Globals['pp_lang']['unreg'] = "No registrado";
+?>
