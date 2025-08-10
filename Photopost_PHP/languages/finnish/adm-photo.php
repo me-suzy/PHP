cd5905708a9f51fe2d6016c0dfc2529d4fd4a448
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:40 +0300

    Add Photopost_PHP/languages/finnish/adm-photo.php

---
+++ Photopost_PHP/languages/finnish/adm-photo.php
@@ -0,0 +1,18 @@
+<?
+$Globals['pp_lang']['malform'] = "Suoritettu väärä parametri!";
+$Globals['pp_lang']['mogrify'] = "Väärä kutsu kuvan muuttamiseen.";
+$Globals['pp_lang']['erotate'] = "Virhe käännettäessä kuvaa!";
+$Globals['pp_lang']['erotatemed'] = "Virhe käännettäessä keskikokoista kuvaa!";
+$Globals['pp_lang']['rotateccw'] = "Kuva on käännetty myötäpäivään!";
+$Globals['pp_lang']['rotatecw'] = "Kuva on käännetty vastapäivään!";
+$Globals['pp_lang']['flipped'] = "Kuva on käännetty pystysuunnassa!";
+$Globals['pp_lang']['flopped'] = "Kuva on käännetty vaakasuunnassa!";    
+$Globals['pp_lang']['nopic'] = "Virhe: Ei poistettavaa kuvaa!";
+$Globals['pp_lang']['nophoto'] = "Kuvaa ei löytynyt tietokannastasi!";
+$Globals['pp_lang']['delete'] = "Kuva poistettu";
+$Globals['pp_lang']['errorcode'] = "Virhe koodi";
+$Globals['pp_lang']['noperm'] = "Sinulla ei ole oikeutta suorittaa tätä toimintoa!";
+$Globals['pp_lang']['badmove'] = "Et voi siirtää kuvaa tähän kategoriaan, yritä uudelleen.";
+$Globals['pp_lang']['noneed'] = "Ei tarvitse siirtää kuvaa samaan kategoriaan!";
+$Globals['pp_lang']['noaction'] = "Ei määriteltyä toimintoa";
+?>
