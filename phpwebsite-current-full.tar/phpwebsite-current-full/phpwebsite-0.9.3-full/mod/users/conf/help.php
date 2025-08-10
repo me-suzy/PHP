1f714cbabd501c7a5e65360b0ca8c434dd4d46f2
me-suzy <me.suzana@gmail.com>
2025-08-11 00:58:46 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/users/conf/help.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/users/conf/help.php
@@ -0,0 +1,26 @@
+<?php
+
+$contact = "Contact Information";
+$contact_content = "This section lets you control the information in a user's authentication email.
+
+<b>User Email Contact:</b> The reply email address for a user.
+
+<b>Subject Line:</b> Appears in the subject portion of the user's email.
+
+<b>Greeting:</b> The body of the email message. Make sure to include a link to your site.";
+
+$signup = "Allow New User Signup";
+$signup_content = "This controls whether users can sign up for a new account.
+
+<b>None:</b> New users are not allowed to sign up for an account.
+
+<b>All users can apply:</b> New users can sign up for an account and it is instantly created.
+
+<b>Only approved users can apply:</b> New users can sign up for an account, but they must be approved before they can log in to the system.";
+
+$authentication = "Authentication Method";
+$authentication_content = "Normally you should not change this from the Local Database option. If however you have an external method of authorizing a user, you may pick the file you wish to use here.";
+
+$login = "Show Login Box";
+$login_content = "If unchecked, the login box will not appear on the site. To login normally, just go to your admin/ directory.";
+?>
