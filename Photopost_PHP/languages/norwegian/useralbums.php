7584169bb2e19469612ab00d6d661f5efa1e9644
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:18 +0300

    Add Photopost_PHP/languages/norwegian/useralbums.php

---
+++ Photopost_PHP/languages/norwegian/useralbums.php
@@ -0,0 +1,27 @@
+<?
+$Globals['pp_lang']['closed'] = "Vi beklager, men våre sider er for øyeblikket nede grunnet vedlikehold. Vennligst prøv igjen senere.";
+$Globals['pp_lang']['noperm'] = "Beklager, men du har ikke tillatelse til å opprette album.";
+$Globals['pp_lang']['noverify'] = "Beklager, men du har ikke aktivert din konto ennå.<p>Du må gjøre det før du kan bruke dine album.";
+$Globals['pp_lang']['nopa'] = "Beklager, Personlige Album er ikke aktivert.";
+$Globals['pp_lang']['useralbums'] = "Bruker Album";
+$Globals['pp_lang']['editor'] = "PhotoPost Album Verktøy";
+$Globals['pp_lang']['addalbum'] = "Legg Til Album";
+$Globals['pp_lang']['errordir'] = "Feil ved oppretting av katalog $newdir. Vennligst kontakt System Administrator.";
+$Globals['pp_lang']['complete'] = "Behandling Fullført!";
+$Globals['pp_lang']['adduser'] = "Legg Til Brukeralbum";
+$Globals['pp_lang']['addaalbum'] = "PhotoPost Legg til et album";
+$Globals['pp_lang']['albumname'] = "Album Navn";
+$Globals['pp_lang']['albumdesc'] = "Album Beskrivelse";
+$Globals['pp_lang']['isprivate'] = "Er Album Privat";
+$Globals['pp_lang']['newalbum'] = "Nytt Album";
+$Globals['pp_lang']['complete'] = "Behandling fullført!";
+$Globals['pp_lang']['editalbum'] = "Forandre Brukeralbum";
+$Globals['pp_lang']['savechanges'] = "Lagre Forandringer";
+$Globals['pp_lang']['malformed'] = "Ugyldig parameter sendt!";
+$Globals['pp_lang']['invalidid'] = "Ugyldig album ID";
+$Globals['pp_lang']['deleteuser'] = "Slett Brukeralbum ";
+$Globals['pp_lang']['delete1'] = "Du er i ferd med å slette albumet kalt";
+$Globals['pp_lang']['delete2'] = "og <b>ALLE BILDER OG KOMMENTARER</b> i albumet.<p>";
+$Globals['pp_lang']['delete3'] = "Er du sikker på at du ønsker å gjøre dette?";
+$Globals['pp_lang']['imsure'] = "Jeg er sikker, slett albumet.";
+?>
