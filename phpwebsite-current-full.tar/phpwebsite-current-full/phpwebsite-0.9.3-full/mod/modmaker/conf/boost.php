efc233608dd4e5368a9100321395a8de317b483a
me-suzy <me.suzana@gmail.com>
2025-08-11 00:53:39 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/modmaker/conf/boost.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/modmaker/conf/boost.php
@@ -0,0 +1,20 @@
+<?php
+
+$mod_title = "modmaker";
+$mod_pname = "Module Maker";
+$mod_directory = "modmaker";
+$mod_filename = "modmaker.php";
+$admin_op = "&mod_op=mod_admin";
+$allow_view = array("modmaker"=>1);
+$priority = 50;
+$mod_icon = "modmaker.gif";
+$admin_mod = 1;
+$deity_mod = 1;
+$mod_class_files = array("mmclass.php");
+$init_object = array("OBJ_modmaker"=>"CLS_modmaker");
+$active = "on";
+$version = 1.2;
+$install_file = "install.php";
+$update_link = "phpwebsite.appstate.edu/fallout/updates/modmaker/VERSION";
+$branch_allow = 1;
+?>
