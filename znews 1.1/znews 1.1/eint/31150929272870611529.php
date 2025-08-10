e6d1ceb4295d68dc860d6b5c76f9307458cb10c3
me-suzy <me.suzana@gmail.com>
2025-08-11 01:06:15 +0300

    Add znews 1.1/znews 1.1/eint/31150929272870611529.php

---
+++ znews 1.1/znews 1.1/eint/31150929272870611529.php
@@ -0,0 +1,10 @@
+<? $betr="News";
+?><? $date="01.10.2002";
+?><? $name="Neo";
+?><? $mail="webmaster@nicis-page.de";
+?><? $text="Hi,
+<br>
+ich habe in der rechten Spalte die Rubrik News weggemacht.";
+?><? $text2="Ich hatte kein Bock, alle News dort einzeln einzutragen. Leider muss ich das noch von hand machen und dass war mir zu viel arbeit!";
+?><? $mname="31150929272870611529";
+?>
