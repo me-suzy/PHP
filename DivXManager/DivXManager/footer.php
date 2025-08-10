6740b7df9b75c0021222b67b54786e20bab1679a
me-suzy <me.suzana@gmail.com>
2025-08-11 00:11:46 +0300

    Add DivXManager/DivXManager/footer.php

---
+++ DivXManager/DivXManager/footer.php
@@ -0,0 +1,30 @@
+</td>
+    <td width="50" background="images/mr.gif">&nbsp;&nbsp;</td>
+</tr>
+<tr>
+    <td><img src="images/lo.gif" width="50" height="50" hspace="0" vspace="0" border="0" alt=""></td>
+    <td height="50" background="images/o.gif" width="100%">
+<table>
+<tr>
+	<td valign="bottom">
+	<a href="index.php" class="a"><img src="images/home.gif" width="30" height="30" border="0" alt="<?echo $mainpage;?>"></a>&nbsp;
+	<a href="overview.php" class="a"><img src="images/overview.gif" width="30" height="30" border="0" alt="<?echo $overview;?>"></a>&nbsp;
+	<a href="search.php" class="a"><img src="images/search.gif" width="30" height="30" border="0" alt="<?echo $searchdivx;?>"></a>&nbsp;
+	<a href="addrecord.php" class="a"><img src="images/add.gif" width="30" height="30" border="0" alt="<?echo $adddivx;?>"></a>&nbsp;
+	<a href="editrecord.php" class="a"><img src="images/edit.gif" width="30" height="30" border="0" alt="<?echo $editdivx;?>"></a>&nbsp;
+	<a href="contact.php" class="a"><img src="images/contact.gif" width="30" height="30" border="0" alt="<?echo $contact;?>"></a>&nbsp;
+	<a href="help.php" class="a"><img src="images/help.gif" width="30" height="30" border="0" alt="<?echo $help;?>"></a>		
+	</td>
+	<td width="100%" valign="top" align="right">
+	<a href="about.php" class="a"><font color="#828282" size="-2"><br>&copy; Lightman.nl</font></a>
+	</td>
+</tr>
+</table>
+	</td>
+    <td><img src="images/ro.gif" width="50" height="50" hspace="0" vspace="0" border="0" alt=""></td>
+</tr>
+</table>
+
+
+</body>
+</html>
