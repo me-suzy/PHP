b115f4d6eb08d81233ea708c71189c6c2b11fa7f
me-suzy <me.suzana@gmail.com>
2025-08-11 01:06:04 +0300

    Add znews 1.1/znews 1.1/admin/menu.php

---
+++ znews 1.1/znews 1.1/admin/menu.php
@@ -0,0 +1,44 @@
+<head>
+
+<style type="text/css">
+
+table {
+
+font-family : verdana; 
+
+font-size : 12px; 
+
+font-weight : bold;
+
+text-align : left;
+
+border : 1px solid #000000;}
+
+
+
+input {
+
+border : 1px solid #000000; 
+
+background-color : #3399FF;
+
+color : #3399FF; 
+
+font-size : 12px; 
+
+color : #3399FF;}
+
+</style>
+
+<body  bgcolor="#4682B4" link="#000000" alink="#000000" vlink="#000000"><font face="Verdana" size="2">
+
+<p align="center"><br>
+
+<a href="admin2.php" target="main">Home</a>&nbsp|&nbsp<a href="ein.php" target="main">Neue News</a>
+
+</center>
+
+</body>
+
+</html>
+
