02c4e531f3c0ade6e54da88171b6f1f6ad24cc19
me-suzy <me.suzana@gmail.com>
2025-08-11 00:49:10 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/comments/boost/uninstall.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/comments/boost/uninstall.php
@@ -0,0 +1,19 @@
+<?php
+if (!$_SESSION["OBJ_user"]->isDeity()){
+  header("location:index.php");
+  exit();
+}
+
+if ($GLOBALS['core']->sqlImport($GLOBALS['core']->source_dir."mod/comments/boost/uninstall.sql", 1, 1)){
+  $content .= "All Comment Manager tables successfully removed.<br />";
+  $status = 1;
+
+  if(isset($_SESSION['OBJ_search'])) {
+    $GLOBALS['core']->sqlDelete("mod_search_register", "module", "comments");
+  }
+} else {
+  $content .= "There was a problem accessing the database.<br />";
+  $status = 0;
+}
+
+?>
