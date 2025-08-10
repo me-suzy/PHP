d2272298d85ed3bccc722b74932995c7e6389eb1
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:46 +0300

    Add Photopost_PHP/languages/finnish/editphoto.php

---
+++ Photopost_PHP/languages/finnish/editphoto.php
@@ -0,0 +1,26 @@
+<?
+$Globals['pp_lang']['closed'] = "Olemme pahoillamme, mutta Photo Boards on tällä hetkellä huollettavana eikä kuvia ole saatavilla. Yrittäkää myöhemmin uudelleen.";
+$Globals['pp_lang']['noperm'] = "Valitettavasti sinulla ei ole oikeutta lähettää/muuttaa.<p>Jos yritit muuttaa, sinä et ole alkuperäisen lähetyksen tekijä tai muutosoikeus on mahdollisesti<br /> poistettu käyttäjäryhmältäsi.";
+$Globals['pp_lang']['badcall'] = "Scriptiä kutsuttiin väärin.  Valitse kyseinen kuva, paina muokkaa linkkiä.";
+$Globals['pp_lang']['editphoto'] = "Muokkaa kuvaa";
+$Globals['pp_lang']['editor'] = "Kuvan muutos";
+$Globals['pp_lang']['username'] = "Käyttäjätunnus";
+$Globals['pp_lang']['thumbnail'] = "Pikkukuva";
+$Globals['pp_lang']['rotateccw'] = "Käännä vastapäivään";
+$Globals['pp_lang']['rotatecw'] = "Käännä myötäpäivään";
+$Globals['pp_lang']['flip'] = "Käännä pystysuunnassa";
+$Globals['pp_lang']['flop'] = "Käännä vaakasuunnassa";
+$Globals['pp_lang']['catchange'] = "Muuta kategoria? (jätä tyhjäksi jos et halua muuttaa)";
+$Globals['pp_lang']['edittitle'] = "Muuta kuvan aihetta";
+$Globals['pp_lang']['editkey'] = "Muuta hakusanoja";
+$Globals['pp_lang']['editdesc'] = "Muuta kuvan kuvausta";
+$Globals['pp_lang']['deletechk'] = "Jos haluat <b>poistaa</b> kuvan kokonaan, rastita tämä valinta";
+$Globals['pp_lang']['careful'] = "(HUOMAA - kerran poistettu on ikuisesti poistettu)";
+$Globals['pp_lang']['submitchg'] = "Tallenna muutokset";
+$Globals['pp_lang']['nophoto'] = "Kuvaa ei löytynyt tietokannastasi!";
+$Globals['pp_lang']['malformed'] = "Väärä parametri suoritettu!";
+$Globals['pp_lang']['deleted'] = "Kuvasi on poistettu onnistuneesti.";
+$Globals['pp_lang']['noaction'] = "Sinulla ei ole oikeutta tähän toimintoon!";
+$Globals['pp_lang']['success'] = "Kuvan muutos onnistui.";
+$Globals['pp_lang']['allowprint'] = "Do you want to allow people to print your photo onto a calendar?";
+?>
