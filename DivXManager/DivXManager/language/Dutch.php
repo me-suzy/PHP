df3950280539b203bd117184efa1d76b523b08e7
me-suzy <me.suzana@gmail.com>
2025-08-11 00:12:06 +0300

    Add DivXManager/DivXManager/language/Dutch.php

---
+++ DivXManager/DivXManager/language/Dutch.php
@@ -0,0 +1,71 @@
+<?php
+/*
+DivX Manager languagefile
+Language = Dutch
+
+If you like this DivX manager please translate the variables below in your own
+language and send them in a mail to lightman@lightman.nl --- THANX!
+*/
+
+// Mainpage
+$mainpage = "Hoofdpagina";
+$overview = "Overzicht";
+$searchdivx = "DivX zoeken";
+$adddivx = "DivX toevoegen";
+$editdivx = "DivX aanpassen / verwijderen";
+$contact = "Neem contact op";
+$help = "Help";
+
+// Searchpage
+$searchheader = "Tik hieronder de DivX titel in welke je zoekt (eerste letters / woord) <br> om te kijken of deze in het systeem staat.";
+$searchbutton = " DivX zoeken! ";
+$clearbutton = " Wis alles ";
+
+// Add
+$addingdivxfailed = "DivX invoeren mislukt...";
+$addingdivxgood = "De DivX is toegevoegd aan de database";
+$divxtitel = "Titel";
+$divxquality = "Kwaliteit";
+$excelent = "Uitstekend";
+$good = "Goed";
+$normal = "Voldoende";
+$acceptable = "Redelijk";
+$bad = "Slecht";
+$unknown = "Onbekend";
+$divxstatus = "Status";
+$home = "Thuis";
+$loaned = "Uitgeleend";
+$lost = "Kwijt";
+$dammaged = "Beschadigd";
+$incomplete = "Onvolledig";
+$adddivxbutton = " DivX invoeren! ";
+
+// Remove
+$recorddelete = "Verwijderen";
+$removedivxfailed = "Verwijderen DivX mislukt...";
+$divxremoved = "De DivX is verwijderd uit de database";
+
+// Update
+$recordedit = "Aanpassen";
+$noidgiven = "Geen ID gedefinieerd.";
+$updatebutton = " Update! ";
+$updatefailed = "Update mislukt...";
+$divxupdated = "De DivX is aangepast...";
+
+// Help
+$helppage = "Sorry, nog geen help in deze versie :)";
+
+// Contact
+$contactname = "Naam";
+$contactemail = "E-mail";
+$contactmessage = "Bericht";
+$contactsend = "Het bericht is verstuurd!";
+$contacttitel = "heeft een bericht gestuurd";
+
+// Install
+$addingtablesfailed = "Tabellen aanmaken mislukt...";
+$tablesadded = "De tabel is succesvol aangemaakt!";
+$deletefile = "Verwijder dit bestand onmiddelijk i.v.m. de beveiliging";
+$checkdata = "Controlleer onderstaande gegevens eerst. Indien alles klopt kunt u op \"Installeren\" klikken. De tabellen worden dan aangemaakt. Als u iets veranderd dient u ook het config.php bestand aan te passen.";
+$install = " Installeren! ";
+?>
