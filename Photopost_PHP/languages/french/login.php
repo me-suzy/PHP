bcab8f85548ef76413dc417d6b4a6b03112e0f31
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:23 +0300

    Add Photopost_PHP/languages/french/login.php

---
+++ Photopost_PHP/languages/french/login.php
@@ -0,0 +1,5 @@
+<?
+$Globals['pp_lang']['loginok'] = "Connexion réussie!";
+$Globals['pp_lang']['loginbad'] = "Connexion non valide. Veuillez vérifier votre nom d'utilisateur et votre mot de passe, ou";
+$Globals['pp_lang']['loginbad2'] = "s'inscrire";
+?>
