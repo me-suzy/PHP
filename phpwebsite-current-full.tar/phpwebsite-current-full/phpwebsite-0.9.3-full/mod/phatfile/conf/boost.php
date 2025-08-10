a1fac6a1a25c292efe3fab2ba2f38acb77483ad0
me-suzy <me.suzana@gmail.com>
2025-08-11 00:55:02 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/phatfile/conf/boost.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/phatfile/conf/boost.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @version $Id: boost.php,v 1.5 2003/06/25 13:31:46 adam Exp $
+ */
+$mod_title = "phatfile";
+$mod_pname = "Document Manager";
+$mod_directory = "phatfile";
+$mod_filename = "index.php";
+$allow_view = "all";
+$priority = 50;
+$active = "on";
+$version = "0.5";
+$admin_mod = 1;
+
+$mod_class_files = array("File.php", "FileManager.php");
+
+?>
