2f4423caccc670d9a47c960f745910a39125345e
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:45 +0300

    Add Photopost_PHP/languages/finnish/ecard.php

---
+++ Photopost_PHP/languages/finnish/ecard.php
@@ -0,0 +1,23 @@
+<?
+$Globals['pp_lang']['disabled'] = "eKortit on poissa käytöstä!";
+$Globals['pp_lang']['larger'] = "Näytä suurempi kuva";
+$Globals['pp_lang']['smaller'] = "Näytä pienempi kuva";
+$Globals['pp_lang']['send'] = "Lähetä kuva eKorttina";
+$Globals['pp_lang']['view'] = "Näytä eKorttisi";
+$Globals['pp_lang']['ecard'] = "eKortti";
+$Globals['pp_lang']['home'] = "Pääsivu";
+$Globals['pp_lang']['name'] = "Nimi";
+$Globals['pp_lang']['email'] = "Email";
+$Globals['pp_lang']['info'] = "Vastaanottajan tiedot";
+$Globals['pp_lang']['alter'] = "Jos et tiedä vastaanottajan sähköpostiosoitetta, syötä vastaanottajien nimet pilkulla eroiteltuna: <br /><br />Käyttäjän nimi";
+$Globals['pp_lang']['subjgreet'] = "eKortin Aihe";
+$Globals['pp_lang']['subject'] = "Aihe";
+$Globals['pp_lang']['yourmess'] = "Viestisi eKorttiin";
+$Globals['pp_lang']['yourecard'] = "eKorttisi";
+$Globals['pp_lang']['send'] = "Lähetä eKortti";
+$Globals['pp_lang']['noemail'] = "Et antanut vastaanottajan sähköpostiosoitetta. Palaa takaisin ja annan joko täydellinen sähköpostiosoite tai käyttäjän nimi.";
+$Globals['pp_lang']['blank'] = "Viestisi oli tyhjä.";
+$Globals['pp_lang']['badname'] = "Valitettavasti käyttäjän nimeä ei tunnistettu";
+$Globals['pp_lang']['hello'] = "Hei";
+$Globals['pp_lang']['cardsent'] = "Viestisi on lähetetty!";
+?>
