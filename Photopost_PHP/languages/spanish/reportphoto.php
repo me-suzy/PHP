7d6910ed834d2ab2d4170c17d5804f00fd078989
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:42 +0300

    Add Photopost_PHP/languages/spanish/reportphoto.php

---
+++ Photopost_PHP/languages/spanish/reportphoto.php
@@ -0,0 +1,13 @@
+<?
+$Globals['pp_lang']['badcall'] = "El programa no se llam&oacute; correctamente. Navegue a una imagen espec&iacute;fica, y haga click en el bot&oacute;n editar";
+$Globals['pp_lang']['closed'] = "Lo sentimos, pero nuestra galer&iacute;a se encuentra cerrada. Por favor intenta m&aacute;s tarde."; 
+$Globals['pp_lang']['reportphoto'] = "Reportar imagen";
+$Globals['pp_lang']['tool'] = "Herramienta para Reporte";
+$Globals['pp_lang']['reportreason'] = "Raz&oacute;n por reportar?";
+$Globals['pp_lang']['inapprop'] = "Material inapropiado";
+$Globals['pp_lang']['copyright'] = "Violaci&oacute;n a los derechos de autor";
+$Globals['pp_lang']['wrongcat'] = "imagen en categor&iacute;a err&oacute;nea";
+$Globals['pp_lang']['other'] = "Otro";
+$Globals['pp_lang']['moreinfo'] = "M&aacute;s informaci&oacute;n (si se necesita)?";
+$Globals['pp_lang']['webnotice'] = "La foto que has reportado ha generado una alerta al webmaster.";
+?>
