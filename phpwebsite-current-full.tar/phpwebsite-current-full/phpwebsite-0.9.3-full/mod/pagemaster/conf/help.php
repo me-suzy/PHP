3bb1be5cf38496b22be0e9c034a28066df94fd15
me-suzy <me.suzana@gmail.com>
2025-08-11 00:54:36 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/pagemaster/conf/help.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/pagemaster/conf/help.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * This is the PageMaster help info file.  It contains some basic help
+ * items to start with. These items can be changed or removed via the
+ * help module included with phpWebSite.
+ *
+ * @version $Id: help.php,v 1.2 2003/04/16 16:40:28 matt Exp $
+ * @author Adam Morton <adam@NOSPAM.tux.appstate.edu>
+ */
+
+$unsaved_title = "Unsaved Page Title";
+$unsaved_title_content = "This is the title given to the page. To edit the title, click the \"Finish\" button and then click the \"Edit\" button near the title.";
+
+$unsaved_created = "Created Date";
+$unsaved_created_content = "This is the date and user that started the creation of this page.";
+
+$unsaved_action = "Unsaved Action";
+$unsaved_action_content = "These are actions you can perform on unsaved pages.  The current actions are:<br /><br />
+<b>Finish</b> - Select this to continue editing a page where you left off.<br />
+<b>Delete</b> - Select this to delete a page. You will be asked to confirm the deletion with a \"Yes\" or \"No\" answer.";
+
+$current_title = "Current Title";
+$current_title_content = "This is the current title given to a page.";
+
+$current_mainpage = "Current Mainpage";
+$current_mainpage_content = "This column will designate which page is currently set to be shown on your web site mainpage.  The current mainpage is marked with a <font color=\"lime\"><b><i>CURRENT</i></b></font> flag.";
+
+$current_updated = "Updated Date";
+$current_updated_content = "This is the date and user who last updated the page.";
+
+$current_action = "Current Page Action";
+$current_action_content = "These are actions you can perform on unsaved pages. The current actions are:<br /><br />
+<b>Hide/Show</b> - This button allows you to remove pages from view without actually deleting them.  Note: A new page will remain hidden until activated.<br />
+<b>Edit</b> - This allows you to edit am existing page.<br />
+<b>Delete</b> - This allows you to remove a page from your database.  You will be asked to confirm the deletion with a \"Yes\" or \"No\" answer.";
+
+$new_title = "Set Title";
+$new_title_content = "Enter a title in this field and select the \"Set Title\" button to save it.";
+
+$edit_title = "Edit Page";
+$edit_title_content = "Click this button to change the page template, category, or page title.";
+
+$section_title = "Section Subtitle";
+$section_title_content = "This is the subtitle given to this section. HTML is allowed and encouraged.";
+
+$section_text = "Section Text";
+$section_text_content = "This is the body text for this section. HTML is allowed and encouraged.";
+
+$section_image = "Section Image";
+$section_image_content = "This is the image that will be associated with this section.  if you choose to upload an image, you must also add a \"Short Image Description\".";
+
+$section_image_desc = "Short Image Description";
+$section_image_desc_content = "This is a short description of the image associated with this section.  It is recommended that the description is no more than 5-6 words.";
+
+$section_template = "Section Template";
+$section_template_content = "This is the template that will be applied to this section when it is viewed. Most templates have names that are fairly intuitive. If you wish to create custom templates, please see the TEMPLATE_README included with PageMaster.";
+
+$section_save = "Section Save";
+$section_save_content = "This will save a new section or save changes made to an existing section.";
+
+$section_remove = "Section Remove";
+$section_remove_content = "This option will remove a section from this page entirely.  You will be asked to confirm the action.";
+
+$section_edit = "Section Edit";
+$section_edit_content = "Select the \"Edit\" button to make changes to this section.";
+
+$page_save = "Page Save";
+$page_save_content = "This button will save a new page to the database and stamp it as saved.  For existing pages it is used as a finishing step to make sure all changes are commited.";
+
+$page_info = "Page Information";
+$page_info_content = "This is some extra information about this page.  Included here is the created date/user and updated date/user.  More information may be added here in later versions.";
+
+?>
