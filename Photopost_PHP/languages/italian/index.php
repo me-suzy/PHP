084208ae7c332f449c38d2312fdd795f0f73149e
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:26 +0300

    Add Photopost_PHP/languages/italian/index.php

---
+++ Photopost_PHP/languages/italian/index.php
@@ -0,0 +1,24 @@
+<?
+$Globals['pp_lang']['board_closed'] = "Ci spiace, ma il servizio è temporaneamente sospeso per aggiornamenti. Riprova più tardi.";
+$Globals['pp_lang']['home'] = "Home";
+$Globals['pp_lang']['welcomeuser'] = "Benvenuto,";
+$Globals['pp_lang']['welcome'] = "Benvenuto su ";
+$Globals['pp_lang']['tablehead'] = "Indice Principale";
+$Globals['pp_lang']['recent'] = "Contributi recenti";
+$Globals['pp_lang']['lastday'] = "ultimo giorno";
+$Globals['pp_lang']['last7'] = "ultima settimana";
+$Globals['pp_lang']['last14'] = "ultime 2 settimane";
+$Globals['pp_lang']['allimages'] = "tutte le immagini";
+$Globals['pp_lang']['category'] = "Categoria";
+$Globals['pp_lang']['photos'] = "Immagini";
+$Globals['pp_lang']['comments'] = "Commenti";
+$Globals['pp_lang']['lastcomment'] = "Ultimo Commento";
+$Globals['pp_lang']['lastphoto'] = "Ultima immagine caricata";
+$Globals['pp_lang']['toposter'] = "Miglior Utente:";
+$Globals['pp_lang']['regusers'] = "Utenti registrati,";
+$Globals['pp_lang']['posted'] = "hanno caricato";
+$Globals['pp_lang']['postednoc'] = "posted photos";
+$Globals['pp_lang']['posts'] = "immagini e commenti";
+$Globals['pp_lang']['views'] = "immagini visualizzate e";
+$Globals['pp_lang']['diskspace'] = "spazio su disco.";
+?>
