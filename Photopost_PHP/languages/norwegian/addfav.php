099e9a7ff3e57a91bc0c94de156f0770d55d6c2a
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:47 +0300

    Add Photopost_PHP/languages/norwegian/addfav.php

---
+++ Photopost_PHP/languages/norwegian/addfav.php
@@ -0,0 +1,8 @@
+<?
+$Globals['pp_lang']['badcall'] = "Skriptet ble ikke kalt opp korrekt.  Gå til et spesifikt fotografi, klikk så på Legg til Favoritter lenken.";
+$Globals['pp_lang']['closed'] = "Vi beklager, men våre sider er for øyeblikket nede grunnet vedlikehold. Vennligst prøv igjen senere.";
+$Globals['pp_lang']['noreg'] = "Kun registrerte brukere kan ha favoritt-fotos.";
+$Globals['pp_lang']['added'] = "Bildet har blitt lagt til ditt Favoritter album";
+$Globals['pp_lang']['malform'] = "Ukorrekt kall til addfav.php";
+$Globals['pp_lang']['removed'] = "Bildet har blitt fjernet fra ditt Favoritter album!";
+?>
