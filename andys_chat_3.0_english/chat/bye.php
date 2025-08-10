787dc86a63543c0654de3b8265bba372c6d72e01
me-suzy <me.suzana@gmail.com>
2025-08-11 00:08:54 +0300

    Add andys_chat_3.0_english/chat/bye.php

---
+++ andys_chat_3.0_english/chat/bye.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+<title>ANDYS CHAT 3.0 | http://www.php-scripts.ch.vu</title>
+</head>
+
+<body bgcolor="white" text="black" link="blue" vlink="purple" alink="red">
+<?php require ("einstellungen.php"); ?>
+<p align="center"><span style="font-size:24pt;"><font face="Tahoma" color="#CC0000">&nbsp;</font></span></p>
+<p align="center"><span style="font-size:28pt;"><font face="Tahoma" color="#3300FF"><b>Thank you very much for visiting!</b></font></span></p>
+<p align="center"><span style="font-size:24pt;"><font face="Tahoma" color="#3300FF"><b><i>&nbsp;</i></b></font></span></p>
+<p align="center"><span style="font-size:24pt;"><font face="Tahoma" color="#3300FF"><b><i>Back to homepage!</i></b></font><b><i><font face="Tahoma" color="#3300FF"><? echo "<a href=\"$to_hp\">&gt;&gt;</a>"; ?></font></i></b></a></span></p>
+<p align="center"><span style="font-size:28pt;"><font face="Tahoma" color="#CC0000"><b>----------------------------------</b></font></span></p>
+<p align="center"><span style="font-size:36pt;"><font face="Tahoma" color="#CC0000"><b>Andys
+Chat 3.0 <br>&nbsp;</b></font><a href="http://www.andys-chat.ch.vu"><b><font face="Tahoma" color="#CC0000">http://www.andys-chat.ch.vu</font></b></a></span></p>
+</body>
+
+</html>
