2b8c9bdd6ea3c42e5923b0005821a9fbb01375d9
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:30 +0300

    Add Photopost_PHP/languages/italian/login.php

---
+++ Photopost_PHP/languages/italian/login.php
@@ -0,0 +1,5 @@
+<?
+$Globals['pp_lang']['loginok'] = "Login Corretto!";
+$Globals['pp_lang']['loginbad'] = "Login errato.  Verifica il tuo Username e Password, o";
+$Globals['pp_lang']['loginbad2'] = "Registrati";
+?>
