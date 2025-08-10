02dc2dc3cdc2c4eb2ff8adc7302d33de0357ca7b
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:20 +0300

    Add Photopost_PHP/languages/italian/bulkupload.php

---
+++ Photopost_PHP/languages/italian/bulkupload.php
@@ -0,0 +1,26 @@
+<?
+$Globals['pp_lang']['closed'] = "Ci spiace, ma il servizio è temporaneamente sospeso per aggiornamenti. Riprova più tardi.";
+$Globals['pp_lang']['noupload'] = "L'utente non è autorizzato a caricare immagini";
+$Globals['pp_lang']['noperm'] = "Spiacente, non sei autorizzato a caricare immagini.";
+$Globals['pp_lang']['noverify'] = "Non hai ancora abilitato il tuo account.<p>Il tuo account deve essere abilitato per poter caricare immagini.";
+$Globals['pp_lang']['processing'] = "Sto analizzando la lista delle immagini";
+$Globals['pp_lang']['bulkuploads'] = "Caricamento di massa";
+$Globals['pp_lang']['photoadd'] = "PhotoPost Aggiungi Immagini";
+$Globals['pp_lang']['addselect'] = "Aggiungi immagini - Seleziona immagini / Configura Proprietà";
+$Globals['pp_lang']['image'] = "Immagine";
+$Globals['pp_lang']['add'] = "Aggiungi";
+$Globals['pp_lang']['category'] = "Categoria";
+$Globals['pp_lang']['optional'] = "Testo opzionale";
+$Globals['pp_lang']['title'] = "Titolo";
+$Globals['pp_lang']['desc'] = "Descrizione";
+$Globals['pp_lang']['nomore'] = "Non ci sono altre immagini.";
+$Globals['pp_lang']['clickhere'] = "Clicca qui per tornare al menù principale.";
+$Globals['pp_lang']['show'] = "mostra miniature?";
+$Globals['pp_lang']['thumbs'] = "Numero di miniature successive?";
+$Globals['pp_lang']['proc'] = "Avvia elaborazione";
+$Globals['pp_lang']['preppro'] = "Preparazione elaborazione successiva ";
+$Globals['pp_lang']['images'] = "immagini";
+$Globals['pp_lang']['click'] = "clicca qui";
+$Globals['pp_lang']['process'] = "Elaborazione Files";
+$Globals['pp_lang']['notrefresh'] = "se la pagina non si auto-aggiorna.";
+?>
