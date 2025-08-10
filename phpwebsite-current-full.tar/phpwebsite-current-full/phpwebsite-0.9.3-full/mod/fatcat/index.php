dca403f5f3880ff6e7fdfbd97fdd59b8f4ff1e89
me-suzy <me.suzana@gmail.com>
2025-08-11 00:50:44 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/fatcat/index.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/fatcat/index.php
@@ -0,0 +1,102 @@
+<?php
+if (!isset($GLOBALS['core'])){
+  header("location:../../");
+  exit();
+}
+
+$CNT_fatcat["content"] = $CNT_fatcat["title"] = NULL;
+if (isset($_REQUEST["fatcat"])){
+  list($section, $command) = each ($_REQUEST["fatcat"]);
+  
+  if ($section == "admin" && $_SESSION["OBJ_user"]->allow_access("fatcat")){
+    switch ($command){
+    case "menu":
+      $_SESSION["OBJ_fatcat"]->admin_menu();
+    break;
+
+    case "createCategoryAction":
+      if ($_SESSION["createCat"]){
+	if($_SESSION["createCat"]->categoryFormAction()){
+	  $content = $_SESSION["translate"]->it("Category Created")."!<br />";
+	  $core->killSession("createCat");
+	  $_SESSION["OBJ_fatcat"]->admin_menu();
+	} else
+	  $_SESSION["createCat"]->createCategoryForm();
+      } else 
+	$_SESSION["OBJ_fatcat"]->admin_menu();
+    break;
+
+    case "updateCategoryAction":
+      if ($_SESSION["updateCat"]){
+	if ($_SESSION["updateCat"]->categoryFormAction()){
+	  $content = $_SESSION["translate"]->it("Category Updated")."!<br />";
+	  $core->killSession("updateCat");
+	  $_SESSION["OBJ_fatcat"]->admin_menu();
+	} else
+	  $_SESSION["updateCat"]->updateCategoryForm();
+      } else
+	$_SESSION["OBJ_fatcat"]->admin_menu();
+    break;
+
+    case "categoryForm":
+      if (isset($_POST["createCategory"])) {
+	$_SESSION["createCat"] = new PHPWS_Fatcat;
+	$_SESSION["createCat"]->parent = $_POST["fatSelect"]["fatcat"];
+	$_SESSION["createCat"]->createCategoryForm();
+      } elseif (isset($_POST["updateCategory"])) {
+	if ($cat_id = $_POST["fatSelect"]["fatcat"]){
+	  $_SESSION["updateCat"] = new PHPWS_Fatcat();
+	  $_SESSION["updateCat"]->initCategory($cat_id);
+	  $_SESSION["updateCat"]->loadCategories();
+	  $_SESSION["updateCat"]->parent = $_SESSION["updateCat"]->categories[$cat_id]->parent;
+	  $_SESSION["updateCat"]->updateCategoryForm();
+	} else
+	  $_SESSION["OBJ_fatcat"]->admin_menu();
+      } elseif (isset($_POST["defIcon"])){
+	if ($_SESSION["OBJ_fatcat"]->saveDefaultIcon()){
+	  $CNT_fatcat["content"] .= $_SESSION["translate"]->it("Default icon set") . ".<hr />";
+	  $_SESSION["OBJ_fatcat"] = new PHPWS_Fatcat;
+	}
+	else
+	  $CNT_fatcat["content"] .= $_SESSION["OBJ_fatcat"]->printError();
+
+	$_SESSION["OBJ_fatcat"]->admin_menu();
+      } elseif (isset($_POST["set_limit"])){
+	$_SESSION["OBJ_fatcat"]->setLimit($_POST["relatedLimit"]);
+	$CNT_fatcat["content"] .= $_SESSION["translate"]->it("Limit Set")."<hr />";
+	$_SESSION["OBJ_fatcat"]->admin_menu();
+      } elseif (isset($_POST['deleteCategory'])){
+	$catId = $_POST['fatSelect']['fatcat'];
+	if (empty($catId) || !is_numeric($catId) || $catId < 1)
+	  $_SESSION["OBJ_fatcat"]->admin_menu();
+	else
+	  phpws_fatcat_forms::deleteCategoryForm($catId);
+      }
+    break;
+
+    case "deleteCategoryAction":
+      phpws_fatcat_category::deleteCategory($_REQUEST['cat_id']);
+      $_SESSION["OBJ_fatcat"]->categories = NULL;
+      $_SESSION["OBJ_fatcat"]->admin_menu();
+      break;
+    }// END switch for command (admin)
+
+  }// END admin section
+
+  if ($section == "user"){
+    switch($command){
+    case "viewCategory":
+      if (isset($_REQUEST["module_title"]))
+	$moduleTitle = $_REQUEST["module_title"];
+      else
+	$moduleTitle = NULL;
+      $CNT_fatcat["title"] = $_SESSION["OBJ_fatcat"]->categories[$_REQUEST["fatcat_id"]]->title;
+      $CNT_fatcat["content"] = $_SESSION["OBJ_fatcat"]->viewCategory($_REQUEST["fatcat_id"], $moduleTitle);
+      break;
+
+    }
+  }// END user section
+
+}// END fatRequest check
+
+?>
