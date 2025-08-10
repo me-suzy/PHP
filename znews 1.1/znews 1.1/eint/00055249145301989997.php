7b3696a18ae084388fabfdb2746407291e4dcbe9
me-suzy <me.suzana@gmail.com>
2025-08-11 01:06:09 +0300

    Add znews 1.1/znews 1.1/eint/00055249145301989997.php

---
+++ znews 1.1/znews 1.1/eint/00055249145301989997.php
@@ -0,0 +1,8 @@
+<? $betr="Forum";
+?><? $date="30.09.2002";
+?><? $name="Neo";
+?><? $mail="webmaster@nicis-page.de";
+?><? $text="Im Forum gibt es jetzt neue Foren.";
+?><? $text2="Leider habe ich keine Zeit mehr und kann desshalb nicht fertigwerden! also wundert euch nicht, dass 2 x scripte und ngb dasteht! Schreibt auch ma was rein!";
+?><? $mname="00055249145301989997";
+?>
