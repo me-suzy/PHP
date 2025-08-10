f2108347a6a49ff46fffd0f87fb0d4c403f997be
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:07 +0300

    Add Photopost_PHP/languages/finnish/useralbums.php

---
+++ Photopost_PHP/languages/finnish/useralbums.php
@@ -0,0 +1,27 @@
+<?
+$Globals['pp_lang']['closed'] = "Olemme pahoillamme, mutta Photo Boards on tällä hetkellä huollettavana eikä kuvia ole saatavilla. Yrittäkää myöhemmin uudelleen.";
+$Globals['pp_lang']['noperm'] = "Valitettavasti sinulla ei ole oikeuksia luoda Albumia";
+$Globals['pp_lang']['noverify'] = "Olemme pahoillamme, mutta sinä et ole vahvistanut käyttäjätunnustasi.<p>Vahvista käyttäjätunnuksesi ennen jatkamista.";
+$Globals['pp_lang']['nopa'] = "Valitettavasti Henkilökohtainen Albumi ei ole käytettävissä.";
+$Globals['pp_lang']['useralbums'] = "Käyttäjän albumit";
+$Globals['pp_lang']['editor'] = "Albumien muuttaminen";
+$Globals['pp_lang']['addalbum'] = "Lisää albumi";
+$Globals['pp_lang']['errordir'] = "Virhe luodessa kansiota $newdir. Ilmoita asiasta järjestelmänvalvojalle.";
+$Globals['pp_lang']['complete'] = "Suoritus Valmis!";
+$Globals['pp_lang']['adduser'] = "Lisää käyttäjän albumi";
+$Globals['pp_lang']['addaalbum'] = "Lisää uusi albumi";
+$Globals['pp_lang']['albumname'] = "Albumin nimi";
+$Globals['pp_lang']['albumdesc'] = "Albumin kuvaus";
+$Globals['pp_lang']['isprivate'] = "Onko albumi yksityinen";
+$Globals['pp_lang']['newalbum'] = "Uusi albumi";
+$Globals['pp_lang']['complete'] = "Suoritus valmis!";
+$Globals['pp_lang']['editalbum'] = "Muuta käyttäjän albumia";
+$Globals['pp_lang']['savechanges'] = "Tallenna muutokset";
+$Globals['pp_lang']['malformed'] = "Väärä parametri suoritettu!";
+$Globals['pp_lang']['invalidid'] = "Väärä albumin ID";
+$Globals['pp_lang']['deleteuser'] = "Poista käyttäjän albumi";
+$Globals['pp_lang']['delete1'] = "Olet suorittamassa poiston albumiin nimeltä";
+$Globals['pp_lang']['delete2'] = "ja <b>KAIKKI KUVAT JA KOMMENTIT</b> albumissa.<p>";
+$Globals['pp_lang']['delete3'] = "Haluatko todella tehdä toiminnon.";
+$Globals['pp_lang']['imsure'] = "Olen varma. Poista albumi";
+?>
