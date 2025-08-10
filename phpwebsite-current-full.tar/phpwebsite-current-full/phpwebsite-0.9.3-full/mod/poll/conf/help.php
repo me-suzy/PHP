39355693664428f54cc4153fcf96113347d47a62
me-suzy <me.suzana@gmail.com>
2025-08-11 00:57:04 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/poll/conf/help.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/poll/conf/help.php
@@ -0,0 +1,18 @@
+<?php
+
+$registered_users = "Registered Users";
+$registered_users_content = "
+This option determines whether or not users who are not logged in are able to vote for this poll.  Select the option for 'YES' if you only want registered users to vote.  By selecting 'NO' all users are able to vote, even if not logged in.";
+
+$active = "Active";
+$active_content = "
+Determines whether to make this poll viewable by users.  There can only be one poll that is active at a time.";
+
+$comments = "Comments";
+$comments_content = "
+The user comments will display at the bottom of the poll results page.  If you would like to allow users to post comments about this poll select 'YES'.
+";
+
+
+
+?>
