f3762b80ad1752f99d607446ebc17eb368732e5a
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:58 +0300

    Add Photopost_PHP/languages/norwegian/index.php

---
+++ Photopost_PHP/languages/norwegian/index.php
@@ -0,0 +1,24 @@
+<?
+$Globals['pp_lang']['board_closed'] = "Vi beklager, men våre sider er for øyeblikket nede grunnet vedlikehold. Vennligst prøv igjen senere.";
+$Globals['pp_lang']['home'] = "Hjem";
+$Globals['pp_lang']['welcomeuser'] = "Velkommen,";
+$Globals['pp_lang']['welcome'] = "Velkommen til ";
+$Globals['pp_lang']['tablehead'] = "Hovedside";
+$Globals['pp_lang']['recent'] = "Nylige bidrag";
+$Globals['pp_lang']['lastday'] = "siste dag";
+$Globals['pp_lang']['last7'] = "siste 7 dager";
+$Globals['pp_lang']['last14'] = "siste 14 dager";
+$Globals['pp_lang']['allimages'] = "alle bilder";
+$Globals['pp_lang']['category'] = "kategori";
+$Globals['pp_lang']['photos'] = "Fotografier";
+$Globals['pp_lang']['comments'] = "Kommentarer";
+$Globals['pp_lang']['lastcomment'] = "Siste Kommentar";
+$Globals['pp_lang']['lastphoto'] = "Siste Bilde Lastet Opp";
+$Globals['pp_lang']['toposter'] = "Topp Postere:";
+$Globals['pp_lang']['regusers'] = "registrerte brukere,";
+$Globals['pp_lang']['posted'] = "postede bilder og";
+$Globals['pp_lang']['postednoc'] = "posted photos";
+$Globals['pp_lang']['posts'] = "kommentarer";
+$Globals['pp_lang']['views'] = "bildevisninger og";
+$Globals['pp_lang']['diskspace'] = "av disk plass.";
+?>
