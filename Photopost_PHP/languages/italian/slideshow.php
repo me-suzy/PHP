c62f2a5cb8a73bc48fe81ba1e2aa5a5e343be018
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:42 +0300

    Add Photopost_PHP/languages/italian/slideshow.php

---
+++ Photopost_PHP/languages/italian/slideshow.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['closed'] = "Ci spiace, ma il servizio è temporaneamente sospeso per aggiornamenti. Riprova più tardi.";
+$Globals['pp_lang']['slideshow'] = "Presentazione automatica";
+$Globals['pp_lang']['delay'] = "Intervallo immagini?<br />(adeguala alla tua connessione internet)";
+$Globals['pp_lang']['twosec'] = "2 secondi";
+$Globals['pp_lang']['foursec'] = "4 secondi";
+$Globals['pp_lang']['eightsec'] = "8 secondi";
+$Globals['pp_lang']['tensec'] = "10 secondi";
+$Globals['pp_lang']['startslide'] = "Avvia Presentazione";
+?>
