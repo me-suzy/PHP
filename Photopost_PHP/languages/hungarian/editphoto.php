bc3135e6221887713b79118b2a93da26e5dc3e63
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:52 +0300

    Add Photopost_PHP/languages/hungarian/editphoto.php

---
+++ Photopost_PHP/languages/hungarian/editphoto.php
@@ -0,0 +1,26 @@
+<?
+$Globals['pp_lang']['closed'] = "Nagyon sajnáljuk, de a Képtár technikai okok miatt átmenetileg zárva tart. Kérjük, próbálkozzon késõbb!";
+$Globals['pp_lang']['noperm'] = "Sajnáljuk, de Önnek nincs jogosultsága vélemény írására, vagy, ha szerkeszteni próbált, valószínûleg nem Ön írta az adott véleményt.";
+$Globals['pp_lang']['badcall'] = "Szkript behívása hibás.  Menjen egy adott fotóhoz, aztán kattintson a szerkesztés linkre!";
+$Globals['pp_lang']['editphoto'] = "Fotó szerkesztése";
+$Globals['pp_lang']['editor'] = "Fotószerkesztõ";
+$Globals['pp_lang']['username'] = "Felhasználónév";
+$Globals['pp_lang']['thumbnail'] = "Nézõkép";
+$Globals['pp_lang']['rotateccw'] = "Forgatás balra";
+$Globals['pp_lang']['rotatecw'] = "Forgtás jobbra";
+$Globals['pp_lang']['flip'] = "Függõleges fordítás";
+$Globals['pp_lang']['flop'] = "Vízszintes fordítás";
+$Globals['pp_lang']['catchange'] = "Kategória megváltoztatása? (hagyja üresen, hogy ne változzon)";
+$Globals['pp_lang']['edittitle'] = "Fotó címének szerkesztése";
+$Globals['pp_lang']['editkey'] = "Kulcsszavak szerkesztése:";
+$Globals['pp_lang']['editdesc'] = "Fotó leírásának szerkesztése";
+$Globals['pp_lang']['deletechk'] = "Ha véglegesen ki akarja <b>törölni</b> ezt a fotót, jelölje meg itt";
+$Globals['pp_lang']['careful'] = "(VIGYÁZAT - ami tapad, az ragad)";
+$Globals['pp_lang']['submitchg'] = "Változások feldolgozása";
+$Globals['pp_lang']['nophoto'] = "Nincs ilyen fotó az adatbázisban.";
+$Globals['pp_lang']['malformed'] = "Hibás paraméter elhagyva!";
+$Globals['pp_lang']['deleted'] = "Sikeresen törölte fotóját.";
+$Globals['pp_lang']['noaction'] = "Nincs jogosultsága ehhez a mûvelethez!";
+$Globals['pp_lang']['success'] = "Sikeresen szerkesztette fotóját.";
+$Globals['pp_lang']['allowprint'] = "Do you want to allow people to print your photo onto a calendar?";
+?>
