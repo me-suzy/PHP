bfe4e0aba1a074bd5e3d9fa1b15a665f709a47aa
me-suzy <me.suzana@gmail.com>
2025-08-11 00:17:04 +0300

    Add photoalbum-current.tar/photoalbum-current/photoalbum/conf/boost.php

---
+++ photoalbum-current.tar/photoalbum-current/photoalbum/conf/boost.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @version $Id: boost.php,v 1.17 2003/08/13 18:17:39 steven Exp $
+ */
+
+$mod_title = "photoalbum";
+$mod_pname = "Photo Albums";
+$mod_directory = "photoalbum";
+$mod_filename = "index.php";
+$mod_class_files = array("AlbumManager.php");
+$allow_view = "all";
+$admin_mod = 1;
+$priority = 50;
+$active = "on";
+$version = "0.77";
+
+?>
