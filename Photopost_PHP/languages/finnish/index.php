384034fa5a185d35d98f457051a574040ad83e00
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:48 +0300

    Add Photopost_PHP/languages/finnish/index.php

---
+++ Photopost_PHP/languages/finnish/index.php
@@ -0,0 +1,24 @@
+<?
+$Globals['pp_lang']['board_closed'] = "Olemme pahoillamme, mutta Photo Boards on tällä hetkellä huollettavana eikä kuvia ole saatavilla. Yrittäkää myöhemmin uudelleen.";
+$Globals['pp_lang']['home'] = "Pääsivu";
+$Globals['pp_lang']['welcomeuser'] = "Tervetuloa,";
+$Globals['pp_lang']['welcome'] = "Tervetuloa  ";
+$Globals['pp_lang']['tablehead'] = "Päähakemisto";
+$Globals['pp_lang']['recent'] = "Viimeisimmät tallennukset";
+$Globals['pp_lang']['lastday'] = "vuorokauden sisällä";
+$Globals['pp_lang']['last7'] = "7 päivän sisällä";
+$Globals['pp_lang']['last14'] = "14 päivän sisällä";
+$Globals['pp_lang']['allimages'] = "kaikki kuvat";
+$Globals['pp_lang']['category'] = "Kategoria";
+$Globals['pp_lang']['photos'] = "Kuvat";
+$Globals['pp_lang']['comments'] = "Kommentit";
+$Globals['pp_lang']['lastcomment'] = "Viimeisin kommentti";
+$Globals['pp_lang']['lastphoto'] = "Viimeisin kuva";
+$Globals['pp_lang']['toposter'] = "Eniten tallentaneet:";
+$Globals['pp_lang']['regusers'] = "rekisteröityneet käyttäjät,";
+$Globals['pp_lang']['posted'] = "tallennetut kuvat ja kommentit";
+$Globals['pp_lang']['postednoc'] = "posted photos";
+$Globals['pp_lang']['posts'] = "kommentit";
+$Globals['pp_lang']['views'] = "kuvanäkymät ja";
+$Globals['pp_lang']['diskspace'] = "levytilasta.";
+?>
