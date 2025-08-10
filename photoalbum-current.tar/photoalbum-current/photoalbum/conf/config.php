268d73b2ef85d17e14bf3413e6a5444d6459a188
me-suzy <me.suzana@gmail.com>
2025-08-11 00:17:06 +0300

    Add photoalbum-current.tar/photoalbum-current/photoalbum/conf/config.php

---
+++ photoalbum-current.tar/photoalbum-current/photoalbum/conf/config.php
@@ -0,0 +1,20 @@
+<?php
+
+define("PHOTOALBUM_DIR", PHPWS_HOME_DIR . "images/photoalbum/");
+
+/* NO SPACES in image types list */
+define("PHOTOALBUM_IMAGE_TYPES", "image/jpeg,image/jpg,image/pjpeg,image/png,image/x-png");
+
+define("PHOTOALBUM_TN_WIDTH", 150);
+define("PHOTOALBUM_TN_HEIGHT", 150);
+
+define("PHPWS_PHOTOALBUM_DEBUG", 0);
+
+define("PHOTOALBUM_MAX_UPLOADS", 10);
+
+define("PHOTOALBUM_DEFAULT_SORT", 0);
+
+define("PHOTOALBUM_MAX_WIDTH", 800);
+define("PHOTOALBUM_MAX_HEIGHT", 800);
+
+?>
