908357624a18eeab15fd73c7051aaccab9f9600e
me-suzy <me.suzana@gmail.com>
2025-08-11 00:50:03 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/debug/conf/layout.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/debug/conf/layout.php
@@ -0,0 +1,13 @@
+<?php
+
+$layout_info[] = array (
+			"content_var"=>"CNT_debug", 
+			"transfer_var"=>"body", 
+			);
+
+$layout_info[] = array (
+			"content_var"=>"CNT_debug_block", 
+			"transfer_var"=>"left_col_bottom", 
+			);
+
+?>
