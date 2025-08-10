8d1b94928b722daf346b9d003edf8c447efcf34e
me-suzy <me.suzana@gmail.com>
2025-08-11 01:01:36 +0300

    Add ShoutProBeta1Build2/help.php

---
+++ ShoutProBeta1Build2/help.php
@@ -0,0 +1,53 @@
+<html><head><title>ShoutPro&#8482; Help</title>
+<style type="text/css">
+body {font-family: Verdana, Arial, Helvetica, sans-serif;}
+</style>
+</head>
+
+<body bgcolor='#333333' text='#DDDDCC' leftmargin=1 rightmargin=1>
+
+<table width=250 style="font-size: 8pt">
+<tr><td colspan=2><b><center style="font-size: 8pt">
+        ShoutPro&#8482; Help 
+      </center></b></td></tr>
+
+<tr>
+    <td colspan=2 style="font-size: 8pt">HTML is prohibited in this ShoutBox . 
+      However, you can use ShoutCode to format your text. You use ShoutCode just 
+      like you use HTML, except instead of using greater-than and less-than signs, 
+      you use open-brackets and close-brackets. Here are the ShoutCode tags:</td>
+  </tr>
+
+<tr bgcolor=#666666><td>[B]Text[/B]</td><td><b>Text</b></td></tr>
+<tr bgcolor=#333333><td>[I]Text[/I]</td><td><i>Text</i></td></tr>
+<tr bgcolor=#666666><td>[U]Text[/U]</td><td><u>Text</u></td></tr>
+<tr bgcolor=#333333><td>[url]http://www.yahoo.com[/url]</td><td>[<a href=http://www.yahoo.com target=_blank>www</a>]</td></tr>
+<tr bgcolor=#666666><td>[mail]joe@yahoo.com[/mail]</td><td>[<a href=mailto:joe@yahoo.com>@</a>]</tr>
+<tr><td colspan=2></td></tr>
+<tr bgcolor=#333333><td colspan=2>There are also several smilies you can put into your posts.  A list is below.</td></tr>
+
+
+<tr bgcolor=#666666><td>:)</td><td><img src='smilies/smile.gif' width="15" height="15"></td></tr>
+<tr bgcolor=#333333><td>:(</td><td><img src='smilies/frown.gif'></td></tr>
+<tr bgcolor=#666666><td>:D</td><td><img src='smilies/biggrin.gif'></td></tr>
+<tr bgcolor=#333333><td>:mad:</td><td><img src='smilies/mad.gif'></td></tr>
+<tr bgcolor=#666666><td>:confused:</td><td><img src='smilies/confused.gif'></td></tr>
+<tr bgcolor=#333333><td>:rolleyes:</td><td><img src='smilies/rolleyes.gif'></td></tr>
+<tr bgcolor=#666666><td>:wink:</td><td><img src='smilies/wink.gif'></td></tr>
+<tr bgcolor=#333333><td>:sigh:</td><td><img src='smilies/sigh.gif'></td></tr>
+<tr bgcolor=#666666><td>:eek:</td><td><img src='smilies/eek.gif'></td></tr>
+<tr bgcolor=#333333><td>:hammer:</td><td><img src='smilies/hammer.gif'></td></tr>
+<tr bgcolor=#666666><td>:wow:</td><td><img src='smilies/wow.gif'></td></tr>
+<tr bgcolor=#333333><td>:hrmm:</td><td><img src='smilies/hrmm.gif'></td></tr>
+<tr bgcolor=#666666><td>:puke:</td><td><img src='smilies/puke.gif'></td></tr>
+<tr bgcolor=#333333><td>:evil:</td><td><img src='smilies/evil.gif'></td></tr>
+<tr bgcolor=#666666><td>:evil2:</td><td><img src='smilies/evil2.gif'></td></tr>
+<tr bgcolor=#333333><td>:bunny:</td><td><img src='smilies/bunny.gif'></td></tr>
+<tr bgcolor=#666666><td>:banana:</td><td><img src='smilies/banana.gif'></td></tr>
+<tr bgcolor=#333333><td>:withstupid:</td><td><img src='smilies/withstupid.gif'></td></tr>
+<tr bgcolor=#333333><td colspan="2"></td></tr>
+<tr bgcolor=#333333><td colspan="2" style="font-size: 7pt"><div align="center">This ShoutBox is 
+        Powered by ShoutPro&#8482;.</div></td></tr>
+</table>
+
+
