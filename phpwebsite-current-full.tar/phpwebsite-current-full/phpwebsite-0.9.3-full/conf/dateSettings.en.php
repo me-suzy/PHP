3528a69ecaf850321abeb7310b73e7aec1e56a31
me-suzy <me.suzana@gmail.com>
2025-08-11 00:25:07 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/conf/dateSettings.en.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/conf/dateSettings.en.php
@@ -0,0 +1,17 @@
+<?php
+if (!defined("PHPWS_DATE_FORMAT"))
+     define("PHPWS_DATE_FORMAT", "m/d/Y");
+
+if (!defined("PHPWS_TIME_FORMAT"))
+     define("PHPWS_TIME_FORMAT", "H:i:s");
+
+$date_month  = "m";
+$date_day    = "d";
+$date_year   = "Y";
+$day_mode    = "l";
+$day_start   = 0; // 0 = Sunday, 1 = Monday
+$date_order  = "m/d/y";
+$time_format = "g:ia";
+$time_dif    = -5;
+
+?>
