d784f0acc97920eff6b9db37ccc81ced6d0f4f64
me-suzy <me.suzana@gmail.com>
2025-08-11 00:11:49 +0300

    Add DivXManager/DivXManager/header.php

---
+++ DivXManager/DivXManager/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title>DivX Manager</title>
+</head>
+<body bgcolor="#000000" text="#000000" link="#000000" vlink="#000000" alink="#000000">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<table cellspacing="0" cellpadding="0" border="0">
+<tr>
+    <td><img src="images/lb.gif" width="50" height="50" hspace="0" vspace="0" border="0" alt=""></td>
+    <td height="50" background="images/b.gif" width="100%" align="right"></td>
+    <td><img src="images/rb.gif" width="50" height="50" hspace="0" vspace="0" border="0" alt=""></td>
+</tr>
+<tr>
+    <td width="50" background="images/ml.gif">&nbsp;&nbsp;</td>
+    <td width="100%" height="100%" background="images/m.gif"><div align="center"><img src="images/logo.gif" width="189" height="143" border="0" alt="DivX Manager"></div>
