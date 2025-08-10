45698fc37eefeeae285f6ed57fb07f07c5356a94
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:18 +0300

    Add Photopost_PHP/languages/french/editphoto.php

---
+++ Photopost_PHP/languages/french/editphoto.php
@@ -0,0 +1,26 @@
+<?
+$Globals['pp_lang']['closed'] = "Nous sommes désolés mais les albums photos sont actuellement indisponibles pour cause de maintenance. Veuillez revenir plus tard. Merci.";
+$Globals['pp_lang']['noperm'] = "Désolé mais vous n'avez pas la permission de poster. Si vous avez essayé d'éditer, il est probable que vous n'êtes pas l'auteur du message";
+$Globals['pp_lang']['badcall'] = "Script not called correctly.  Choisissez une photo et cliquer le lien Editer.";
+$Globals['pp_lang']['editphoto'] = "Editer la photo";
+$Globals['pp_lang']['editor'] = "Êditeur d'image";
+$Globals['pp_lang']['username'] = "Nom d'utilisateur";
+$Globals['pp_lang']['thumbnail'] = "Vignette";
+$Globals['pp_lang']['rotateccw'] = "Tourner dans le sens contraire des aiguilles d'une montre";
+$Globals['pp_lang']['rotatecw'] = "Tourner dans le sens des aiguilles d'une montre";
+$Globals['pp_lang']['flip'] = "Renverser verticalement";
+$Globals['pp_lang']['flop'] = "Renverser horizontalement";
+$Globals['pp_lang']['catchange'] = "Changer de catégorie? (laisser vide si ce n'est pas le cas)";
+$Globals['pp_lang']['edittitle'] = "Editer le titre de la photo";
+$Globals['pp_lang']['editkey'] = "Editer les mots clés:";
+$Globals['pp_lang']['editdesc'] = "Editer la description de la photo";
+$Globals['pp_lang']['deletechk'] = "Si vous souhaitez <b>supprimer</b> cette image complètement, cocher cette case";
+$Globals['pp_lang']['careful'] = "(ATTENTION - quand une image est supprimée, on ne peut plus la récupérer)";
+$Globals['pp_lang']['submitchg'] = "Procéder aux modifications";
+$Globals['pp_lang']['nophoto'] = "Photo introuvable dans la base de données!";
+$Globals['pp_lang']['malformed'] = "Malformed parameter passed!";
+$Globals['pp_lang']['deleted'] = "Vous avez supprimé l'image avec succès.";
+$Globals['pp_lang']['noaction'] = "Vous n'avez pas la permission pour effectuer cette action!";
+$Globals['pp_lang']['success'] = "Vous avez édité la photo avec succès.";
+$Globals['pp_lang']['allowprint'] = "Do you want to allow people to print your photo onto a calendar?";
+?>
