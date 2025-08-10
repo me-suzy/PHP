dd379c4a2c8fba2885f5486e6c7115c420abb25c
me-suzy <me.suzana@gmail.com>
2025-08-11 00:55:42 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/phatform/conf/controlpanel.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/phatform/conf/controlpanel.php
@@ -0,0 +1,14 @@
+<?php
+
+$image["name"] = "phatform.png";
+$image["alt"] = "Author: Steven Levin and Adam Morton";
+
+$link[] = array ("label"=>"Form Generator",
+		 "module"=>"phatform",
+		 "url"=>"index.php?module=phatform&PHAT_MAN_OP=List",
+		 "image"=>$image,
+		 "admin"=>TRUE,
+		 "description"=>"Go here to create online forms that can be viewed and filled out by your site visitors. Reports can be made on the data submitted.",
+		 "tab"=>"content");
+
+?>
