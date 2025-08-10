6172d21263ec6867aa14ccb1b0df3bd9e24c11c9
me-suzy <me.suzana@gmail.com>
2025-08-11 00:52:49 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/linkman/conf/help.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/linkman/conf/help.php
@@ -0,0 +1,9 @@
+<?php
+
+$main = "Link Manager Help";
+$main_content = "The Link Manager is a very basic database of web urls.  These links can be categorized so that they are related to other content within your site like pages, announcements, or frequently asked questions.";
+
+$add = "Add A Web Link";
+$add_content = "In order to add a web link to the database all that is necessary is a title and a url.  But the real benifit comes from categorizing the link.  When in a category with other content the link will show up as related info.  Make sure to test your url after adding the link.";
+
+?>
