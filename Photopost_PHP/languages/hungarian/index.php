60d99f5e3c4d8e555a99468046fe3f97d9bcd9b2
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:53 +0300

    Add Photopost_PHP/languages/hungarian/index.php

---
+++ Photopost_PHP/languages/hungarian/index.php
@@ -0,0 +1,24 @@
+<?
+$Globals['pp_lang']['board_closed'] = "Nagyon sajnáljuk, de a Képtár technikai okok miatt átmenetileg zárva tart. Kérjük, próbálkozzon késõbb!";
+$Globals['pp_lang']['home'] = "Fõoldal";
+$Globals['pp_lang']['welcomeuser'] = "Üdvözöljük,";
+$Globals['pp_lang']['welcome'] = "Üdvözöljük a ";
+$Globals['pp_lang']['tablehead'] = "Fõmenü";
+$Globals['pp_lang']['recent'] = "Eddigi vélemények";
+$Globals['pp_lang']['lastday'] = "utolsó nap";
+$Globals['pp_lang']['last7'] = "utolsó 7 nap";
+$Globals['pp_lang']['last14'] = "utolsó 14 nap";
+$Globals['pp_lang']['allimages'] = "minden fotó";
+$Globals['pp_lang']['category'] = "Kategória";
+$Globals['pp_lang']['photos'] = "Fotók";
+$Globals['pp_lang']['comments'] = "Vélemények";
+$Globals['pp_lang']['lastcomment'] = "Legújabb vélemények";
+$Globals['pp_lang']['lastphoto'] = "Legújabb feltöltött fotó";
+$Globals['pp_lang']['toposter'] = "Legaktívabbak:";
+$Globals['pp_lang']['regusers'] = "regisztrált felhasználók,";
+$Globals['pp_lang']['posted'] = "feltöltött fotók és";
+$Globals['pp_lang']['postednoc'] = "posted photos";
+$Globals['pp_lang']['posts'] = "vélemények";
+$Globals['pp_lang']['views'] = "meglátogatott fotók";
+$Globals['pp_lang']['diskspace'] = "a tárhelnyek.";
+?>
