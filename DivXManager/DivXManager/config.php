43b4018250718f1fd88fcd67fa1be2ec28b999b0
me-suzy <me.suzana@gmail.com>
2025-08-11 00:11:38 +0300

    Add DivXManager/DivXManager/config.php

---
+++ DivXManager/DivXManager/config.php
@@ -0,0 +1,44 @@
+<?php
+######################
+#    Your e-mail     #
+######################
+
+$jouwemail = "your@email.com";
+
+######################
+# Prefered language  #
+######################
+
+$lang = "English.php";
+
+######################
+#  The Databasename  #
+######################
+
+$db = "thedatabase";
+
+######################
+#      The Host      #
+######################
+
+$sqlhost = "localhost";
+
+######################
+#      The User      #
+######################
+
+$sqluser = "username";
+
+######################
+#      Password      #
+######################
+
+$sqlpass = "password";
+
+#################################
+# DON'T EDIT BELOW THIS BLOCK ! #
+#################################
+
+$db2 = $db;
+include("language/$lang");
+?>
