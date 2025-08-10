7b8cca63a5849921587b8ade802ed957b2a04f6b
me-suzy <me.suzana@gmail.com>
2025-08-11 00:46:50 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/announce/conf/help.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/announce/conf/help.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @version $Id: help.php,v 1.1 2003/06/02 12:36:14 adam Exp $
+ */
+
+$admin_menu = "Administration Menu";
+$admin_menu_content = "Select \"New Announcement\" to post a new announcement to your home page.<br /><br />Select \"List Announcements\" to list all your announcements.<br /><br />Select \"Settings\" to edit the global settings for your announcements and how they are displayed.";
+
+?>
