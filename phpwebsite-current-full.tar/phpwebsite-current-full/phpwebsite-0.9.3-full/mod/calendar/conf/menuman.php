1787a60e6a999d39b47ae1b7a87b15339da64f23
me-suzy <me.suzana@gmail.com>
2025-08-11 00:48:51 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/calendar/conf/menuman.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/calendar/conf/menuman.php
@@ -0,0 +1,4 @@
+<?php
+$link[$_SESSION['translate']->it("Submit Event")] = "index.php?module=calendar&amp;calendar[user]=userEvent";
+$link[$_SESSION['translate']->it("Month View")] = "index.php?module=calendar&amp;calendar[view]=month";
+?>
