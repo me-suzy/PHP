688bb4b0501b6e39694d7875a1a2d159718a306f
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:13 +0300

    Add Photopost_PHP/languages/french/bulkupload.php

---
+++ Photopost_PHP/languages/french/bulkupload.php
@@ -0,0 +1,25 @@
+<?
+$Globals['pp_lang']['closed'] = "Nous sommes désolés mais les albums photos sont actuellement indisponibles pour cause de maintenance. Veuillez revenir plus tard. Merci.";
+$Globals['pp_lang']['noupload'] = "Les téléchargements par l'utilisateur ne sont pas autorisés.";
+$Globals['pp_lang']['noperm'] = "Désolé mais vous n'avez pas la permission de télécharger des photos.";
+$Globals['pp_lang']['noverify'] = "Désolé mais vous n'avez pas encore activé votre compte.<p>Vous devez d'abord l'activer pour pouvoir télécharger.";
+$Globals['pp_lang']['processing'] = "Traitement de la liste d'image";
+$Globals['pp_lang']['bulkuploads'] = "Téléchargement en masse.";
+$Globals['pp_lang']['photoadd'] = "PhotoPost Ajouter des photos";
+$Globals['pp_lang']['addselect'] = "Ajouter des photos - Sélectionner des photos / Configurer les propriétés";
+$Globals['pp_lang']['image'] = "Image";
+$Globals['pp_lang']['add'] = "Ajouter";
+$Globals['pp_lang']['category'] = "Catégorie";
+$Globals['pp_lang']['optional'] = "Texte facultatif";
+$Globals['pp_lang']['title'] = "Titre";
+$Globals['pp_lang']['desc'] = "Description";
+$Globals['pp_lang']['nomore'] = "Il n'y a plus d'images.";
+$Globals['pp_lang']['clickhere'] = "Cliquer ici pour revenir au menu principal.";
+$Globals['pp_lang']['show'] = "Montrer les vignettes?";
+$Globals['pp_lang']['thumbs'] = "Nombre de vignettes à traiter?";
+$Globals['pp_lang']['proc'] = "Procéder au traitement";
+$Globals['pp_lang']['preppro'] = "Préparation au traitement suivant";
+$Globals['pp_lang']['images'] = "images";
+$Globals['pp_lang']['click'] = "cliquer ici";
+$Globals['pp_lang']['notrefresh'] = "si la page de se rafraichit pas.";
+?>
