f1c47ae938fb7776fa86952c12e23e9258e02e4f
me-suzy <me.suzana@gmail.com>
2025-08-11 01:02:07 +0300

    Add tagboard/config.php

---
+++ tagboard/config.php
@@ -0,0 +1,24 @@
+<?
+$display = "display.php";	// the display file
+$datfile = "tag.txt";		// the data file
+$MAX_LENGTH = 200;		// the maximum length of posts
+$NUM_COMMENTS = 25;  	// how many comments will be displayed on your tag board
+$print_how_many = 1;  		// set to "1" displays number of tags on the tag board, "0" turns feature off
+$stats = 1;				// set to "1" displays stats when hover over square image ("ip.gif")
+$meta_refresh = 1;		// set to "1" tells the tag board to refresh, "0" turns feature off
+$meta_refresh_rate = 120;	// if you want to refresh the tag board, enter the time in seconds to wait per refresh
+
+// Bad Word Filter - Add as many as you like!
+
+$badword_array['fuck'] = 'f**k';
+$badword_array['bitch'] = 'female dog';
+$badword_array['shit'] = 'crap';
+$badword_array['nigger'] = 'black man';
+$badword_array['nigga'] = 'black man';
+$badword_array['slut'] = 'sl*t';
+$badword_array['whore'] = 'w**re';
+$badword_array['cock'] = 'c**k';
+$badword_array['pussy'] = 'p***y';
+
+
+?>
