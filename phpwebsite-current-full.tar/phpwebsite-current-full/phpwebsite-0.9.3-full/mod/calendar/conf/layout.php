e06db081012a06802471cd706882444abd16774e
me-suzy <me.suzana@gmail.com>
2025-08-11 00:48:49 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/calendar/conf/layout.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/calendar/conf/layout.php
@@ -0,0 +1,19 @@
+<?php
+$layout_info[] = array (
+			"content_var"=>"CNT_Calendar_Main", 
+			"theme_var"=>"body", 
+			"home_only"=>0
+			);
+
+$layout_info[] = array (
+			"content_var"=>"CNT_Calendar_Panel", 
+			"theme_var"=>"top", 
+			"home_only"=>0
+			);
+
+$layout_info[] = array (
+			"content_var"=>"CNT_Calendar_Box", 
+			"theme_var"=>"left_col_bottom", 
+			"home_only"=>0
+			);
+?>
