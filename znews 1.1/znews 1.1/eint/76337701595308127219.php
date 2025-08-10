335a17c47386db3f8df3070828069d9fe1f0f3c8
me-suzy <me.suzana@gmail.com>
2025-08-11 01:06:20 +0300

    Add znews 1.1/znews 1.1/eint/76337701595308127219.php

---
+++ znews 1.1/znews 1.1/eint/76337701595308127219.php
@@ -0,0 +1,10 @@
+<? $betr="Gästebuch und Newsscript";
+?><? $date="26.09.2002";
+?><? $name="Neo";
+?><? $mail="webmaster@nicis-page.de";
+?><? $text="Hi,
+<br>
+die beiden Scripte Gästebuch und Newscript sind online!";
+?><? $text2="Beide sind mit Adminbereich und man hat die möglichkeit einzelne Beiträge zu löschen.";
+?><? $mname="76337701595308127219";
+?>
