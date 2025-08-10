54d1c962b5e65a27f32ea46ce90bb34ce5cc0f25
me-suzy <me.suzana@gmail.com>
2025-08-11 00:12:08 +0300

    Add DivXManager/DivXManager/language/English.php

---
+++ DivXManager/DivXManager/language/English.php
@@ -0,0 +1,71 @@
+<?php
+/*
+DivX Manager languagefile
+Language = English
+
+If you like this DivX manager please translate the variables below in your own
+language and send them in a mail to lightman@lightman.nl --- THANX!
+*/
+
+// Mainpage
+$mainpage = "Mainpage";
+$overview = "Overview";
+$searchdivx = "Search DivX";
+$adddivx = "Add DivX";
+$editdivx = "Edit / Delete DivX";
+$contact = "Contact";
+$help = "Help";
+
+// Searchpage
+$searchheader = "Type here a couple of letters or a word from the DivX <br> you are looking for.";
+$searchbutton = " Search! ";
+$clearbutton = " Clear ";
+
+// Add
+$addingdivxfailed = "Adding DivX failed...";
+$addingdivxgood = "DivX is added to the database";
+$divxtitel = "Name";
+$divxquality = "Quality";
+$excelent = "Excelent";
+$good = "Good";
+$normal = "Normal";
+$acceptable = "Acceptable";
+$bad = "Bad";
+$unknown = "Unknown";
+$divxstatus = "Status";
+$home = "Home";
+$loaned = "Loaned";
+$lost = "Lost";
+$dammaged = "Dammaged";
+$incomplete = "Incomplete";
+$adddivxbutton = " Add DivX! ";
+
+// Remove
+$recorddelete = "Delete";
+$removedivxfailed = "Removing DivX failed...";
+$divxremoved = "DivX is removed from the database";
+
+// Update
+$recordedit = "Edit";
+$noidgiven = "No ID selected.";
+$updatebutton = " Update! ";
+$updatefailed = "Update failed...";
+$divxupdated = "DivX is updated...";
+
+// Help
+$helppage = "Sorry, no help yet in this version  :)";
+
+// Contact
+$contactname = "Name";
+$contactemail = "E-mail";
+$contactmessage = "Message";
+$contactsend = "The message is verstuurd!";
+$contacttitel = "has send a message";
+
+// Install
+$addingtablesfailed = "Adding tables failed...";
+$tablesadded = "Tabels are installed!";
+$deletefile = "Delete this file now!";
+$checkdata = "Check data below! If it's correct press the \"Install\" button. If you change anything you'll also have to change config.php";
+$install = " Install! ";
+?>
