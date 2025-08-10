a240e4f3afb63e9dd771e8e130764bd5f1c021cd
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:50 +0300

    Add Photopost_PHP/languages/hungarian/ecard.php

---
+++ Photopost_PHP/languages/hungarian/ecard.php
@@ -0,0 +1,23 @@
+<?
+$Globals['pp_lang']['disabled'] = "E-képeslap tiltva";
+$Globals['pp_lang']['larger'] = "Nagy kép megtekintése";
+$Globals['pp_lang']['smaller'] = "Kis kép megtekintése";
+$Globals['pp_lang']['send'] = "Fotó küldése E-képeslapként";
+$Globals['pp_lang']['view'] = "E-képeslap megtekintése";
+$Globals['pp_lang']['ecard'] = "E-képeslap";
+$Globals['pp_lang']['home'] = "Fõoldal";
+$Globals['pp_lang']['name'] = "Név";
+$Globals['pp_lang']['email'] = "E-mail";
+$Globals['pp_lang']['info'] = "Címzett információi";
+$Globals['pp_lang']['alter'] = "Ha nem tudja a címzett e-mail címét, helyette adja meg a felhasználónevét:<br /><br />Felhasználónév";
+$Globals['pp_lang']['subjgreet'] = "E-képeslap tárgya/üdvözlet";
+$Globals['pp_lang']['subject'] = "Tárgy";
+$Globals['pp_lang']['yourmess'] = "E-képeslap szövege";
+$Globals['pp_lang']['yourecard'] = "E-képeslapja";
+$Globals['pp_lang']['send'] = "E-képeslap küldése";
+$Globals['pp_lang']['noemail'] = "Nem adta meg a címzett e-mail címét. Kérjük, menjen vissza, és adja meg a címzett e-mail címét, vagy felhasználónevét!";
+$Globals['pp_lang']['blank'] = "Szövegmezõ üres.";
+$Globals['pp_lang']['badname'] = "Sajnáljuk, nem találtunk ilyen nevû felhasználót.";
+$Globals['pp_lang']['hello'] = "Üdvözlöm";
+$Globals['pp_lang']['cardsent'] = "Képeslapját elküldtük!";
+?>
