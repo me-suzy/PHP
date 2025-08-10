a3692a2758c70900ba11ebf6e8bc83018f6bf4b1
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:52 +0300

    Add Photopost_PHP/languages/spanish/useralbums.php

---
+++ Photopost_PHP/languages/spanish/useralbums.php
@@ -0,0 +1,28 @@
+<?
+$Globals['pp_lang']['closed'] = "Lo sentimos, pero nuestra galer&iacute;a se encuentra cerrada. Por favor intenta m&aacute;s tarde.";
+$Globals['pp_lang']['noperm'] = "Sorry, you don't have permission to create albums.";
+$Globals['pp_lang']['noperm'] = "Lo siento, no tienes permiso para crear &aacute;lbumes.";
+$Globals['pp_lang']['noverify'] = "Lo siento, pero no has verificado tu cuenta a&uacute;n.<p>Debes hacerlo antes de poder administrar tus &aacute;lbumes.";
+$Globals['pp_lang']['nopa'] = "Lo sentimos, los &aacute;lbumes personales no est&aacute;n permitidos.";
+$Globals['pp_lang']['useralbums'] = "&Aacute;lbumes de Usuarios";
+$Globals['pp_lang']['editor'] = "Editor de &Aacute;lbumes de PhotoPost";
+$Globals['pp_lang']['addalbum'] = "Agregar &Aacute;lbum";
+$Globals['pp_lang']['errordir'] = "Hubo un error creando el directorio $newdir. Por favor notif&iacute;cale al administrador de sistema.";
+$Globals['pp_lang']['complete'] = "Proceso completado!";
+$Globals['pp_lang']['adduser'] = "Agregar &Aacute;lbum de Usuario";
+$Globals['pp_lang']['addaalbum'] = "PhotoPost Agregar un &aacute;lbum";
+$Globals['pp_lang']['albumname'] = "Nombre del &Aacute;lbum";
+$Globals['pp_lang']['albumdesc'] = "Descripci&oacute;n del &Aacute;lbum";
+$Globals['pp_lang']['isprivate'] = "Es Privado";
+$Globals['pp_lang']['newalbum'] = "Nuevo &Aacute;lbum";
+$Globals['pp_lang']['complete'] = "Proceso completado!";
+$Globals['pp_lang']['editalbum'] = "Editar &Aacute;lbum de Usuario";
+$Globals['pp_lang']['savechanges'] = "Guardar Cambios";
+$Globals['pp_lang']['malformed'] = "Par&aacute;metro err&oacute;neo pasado!";
+$Globals['pp_lang']['invalidid'] = "ID de &aacute;lbum inv&aacute;lido";
+$Globals['pp_lang']['deleteuser'] = "Borrar &Aacute;lbum de Usuario ";
+$Globals['pp_lang']['delete1'] = "Estas a punto de borrar el &aacute;lbum llamado";
+$Globals['pp_lang']['delete2'] = "y <b>TODAS LAS FOTOS Y COMENTAIROS</b> dentro del &aacute;lbum.<p>";
+$Globals['pp_lang']['delete3'] = "Est&aacute;s seguro de esto?";
+$Globals['pp_lang']['imsure'] = "Estoy seguro, borrar el &aacute;lbum.";
+?>
