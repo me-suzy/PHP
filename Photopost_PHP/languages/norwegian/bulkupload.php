82b6304fdcaa7b4d7df3618cddcdcf5a751c8b6e
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:52 +0300

    Add Photopost_PHP/languages/norwegian/bulkupload.php

---
+++ Photopost_PHP/languages/norwegian/bulkupload.php
@@ -0,0 +1,25 @@
+<?
+$Globals['pp_lang']['closed'] = "Vi beklager, men våre sider er for øyeblikket nede grunnet vedlikehold. Vennligst prøv igjen senere.";
+$Globals['pp_lang']['noupload'] = "Bruker opplasting ikke tillatt.";
+$Globals['pp_lang']['noperm'] = "Beklager, du har ikke tillatelse til å laste opp bilder.";
+$Globals['pp_lang']['noverify'] = "Beklager men du har ikke aktivert din konto ennå.<p>Du må gjøre så før du kan laste opp bilder.";
+$Globals['pp_lang']['processing'] = "Behandler bildeliste";
+$Globals['pp_lang']['bulkuploads'] = "Mengde opplasting";
+$Globals['pp_lang']['photoadd'] = "PhotoPost Legg Til Bilder";
+$Globals['pp_lang']['addselect'] = "Legg Til Bilder - Velg Bilder / Forandre Egenskaper";
+$Globals['pp_lang']['image'] = "Bilde";
+$Globals['pp_lang']['add'] = "Legg til";
+$Globals['pp_lang']['category'] = "Kategori";
+$Globals['pp_lang']['optional'] = "Eventuell tekst";
+$Globals['pp_lang']['title'] = "Tittel";
+$Globals['pp_lang']['desc'] = "Beskrivelse";
+$Globals['pp_lang']['nomore'] = "Ingen fler bilder funnet.";
+$Globals['pp_lang']['clickhere'] = "Klikk her for å gå tilbake til hovedmenyen.";
+$Globals['pp_lang']['show'] = "Vise småbilder?";
+$Globals['pp_lang']['thumbs'] = "Antall småbilder å behandle?";
+$Globals['pp_lang']['proc'] = "Utfør";
+$Globals['pp_lang']['preppro'] = "Forbereder behandling";
+$Globals['pp_lang']['images'] = "bilder";
+$Globals['pp_lang']['click'] = "klikk her";
+$Globals['pp_lang']['notrefresh'] = "om siden ikke lastes på nytt.";
+?>
