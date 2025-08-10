655c158f789b3153b705c5253917c317eed191f5
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:10 +0300

    Add Photopost_PHP/languages/english/bulkupload.php

---
+++ Photopost_PHP/languages/english/bulkupload.php
@@ -0,0 +1,26 @@
+<?
+$Globals['pp_lang']['closed'] = "We're sorry, but our Photo Boards are currently down for maintainence. Please try again later.";
+$Globals['pp_lang']['noupload'] = "User uploads not allowed";
+$Globals['pp_lang']['noperm'] = "Sorry, you don't have permission to upload photos.";
+$Globals['pp_lang']['noverify'] = "Sorry, but you have not verified your account yet.<p>You must do so before being able to upload.";
+$Globals['pp_lang']['processing'] = "Processing image list";
+$Globals['pp_lang']['bulkuploads'] = "Bulk Uploads";
+$Globals['pp_lang']['photoadd'] = "PhotoPost Add Photos";
+$Globals['pp_lang']['addselect'] = "Add Photos - Select Photos / Configure Properties";
+$Globals['pp_lang']['image'] = "Image";
+$Globals['pp_lang']['add'] = "Add";
+$Globals['pp_lang']['category'] = "Category";
+$Globals['pp_lang']['optional'] = "Optional Text";
+$Globals['pp_lang']['title'] = "Title";
+$Globals['pp_lang']['desc'] = "Description";
+$Globals['pp_lang']['nomore'] = "No more images found.";
+$Globals['pp_lang']['clickhere'] = "Click here to return to main menu.";
+$Globals['pp_lang']['show'] = "Show thumbnails?";
+$Globals['pp_lang']['thumbs'] = "Number of thumbnails to process next?";
+$Globals['pp_lang']['proc'] = "Process";
+$Globals['pp_lang']['preppro'] = "Prepareing to process next";
+$Globals['pp_lang']['images'] = "images";
+$Globals['pp_lang']['click'] = "click here";
+$Globals['pp_lang']['process'] = "Process Files";
+$Globals['pp_lang']['notrefresh'] = "if page does not refresh.";
+?>
