c53634103696df7dd9215e283d3c8405ee07b68a
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:15 +0300

    Add Photopost_PHP/languages/norwegian/slideshow.php

---
+++ Photopost_PHP/languages/norwegian/slideshow.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['closed'] = "Vi beklager, men våre sider er for øyeblikket nede grunnet vedlikehold. Vennligst prøv igjen senere.";
+$Globals['pp_lang']['slideshow'] = "Lysbildevisning";
+$Globals['pp_lang']['delay'] = "Tidsforsinkelse for bilder?<br />(treg forbindelse vil kreve lengre forsinkelse)";
+$Globals['pp_lang']['twosec'] = "2 sekunder";
+$Globals['pp_lang']['foursec'] = "4 sekunder";
+$Globals['pp_lang']['eightsec'] = "8 sekunder";
+$Globals['pp_lang']['tensec'] = "10 sekunder";   
+$Globals['pp_lang']['startslide'] = "Start Lysbildevisning";
+?>
