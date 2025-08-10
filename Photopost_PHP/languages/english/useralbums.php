832f4439b112449a4999b8430fbb42caf4178a9d
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:35 +0300

    Add Photopost_PHP/languages/english/useralbums.php

---
+++ Photopost_PHP/languages/english/useralbums.php
@@ -0,0 +1,27 @@
+<?
+$Globals['pp_lang']['closed'] = "We're sorry, but our Photo Boards are currently down for maintainence. Please try again later.";
+$Globals['pp_lang']['noperm'] = "Sorry, you don't have permission to create albums.";
+$Globals['pp_lang']['noverify'] = "Sorry, but you have not verified your account yet.<p>You must do so before being able to manage your albums.";
+$Globals['pp_lang']['nopa'] = "Sorry, Personal Albums are not enabled.";
+$Globals['pp_lang']['useralbums'] = "User Albums";
+$Globals['pp_lang']['editor'] = "PhotoPost Album Editor";
+$Globals['pp_lang']['addalbum'] = "Add Album";
+$Globals['pp_lang']['errordir'] = "Error creating directory $newdir. Please notify the system administrator.";
+$Globals['pp_lang']['complete'] = "Processing complete!";
+$Globals['pp_lang']['adduser'] = "Add User Album";
+$Globals['pp_lang']['addaalbum'] = "PhotoPost Add an album";
+$Globals['pp_lang']['albumname'] = "Album Name";
+$Globals['pp_lang']['albumdesc'] = "Album Description";
+$Globals['pp_lang']['isprivate'] = "Is Album Private";
+$Globals['pp_lang']['newalbum'] = "New Album";
+$Globals['pp_lang']['complete'] = "Processing complete!";
+$Globals['pp_lang']['editalbum'] = "Edit User Album";
+$Globals['pp_lang']['savechanges'] = "Save Changes";
+$Globals['pp_lang']['malformed'] = "Malformed parameter passed!";
+$Globals['pp_lang']['invalidid'] = "Invalid album ID";
+$Globals['pp_lang']['deleteuser'] = "Delete User Album ";
+$Globals['pp_lang']['delete1'] = "You're about to delete the album called";
+$Globals['pp_lang']['delete2'] = "and <b>ALL PHOTOS AND COMMENTS</b> within the album.<p>";
+$Globals['pp_lang']['delete3'] = "Are you sure you want to do that?";
+$Globals['pp_lang']['imsure'] = "I'm sure, delete the album.";
+?>
