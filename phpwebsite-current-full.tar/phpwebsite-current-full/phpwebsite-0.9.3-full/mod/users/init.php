660acbc49f670713e483f903cd5e992d3695e6ec
me-suzy <me.suzana@gmail.com>
2025-08-11 00:58:26 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/users/init.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/users/init.php
@@ -0,0 +1,8 @@
+<?php
+
+require($GLOBALS["core"]->source_dir."mod/users/class/Cookie.php");
+require($GLOBALS["core"]->source_dir."mod/users/class/ModSetting.php");
+require($GLOBALS["core"]->source_dir."mod/users/class/Forms.php");
+require($GLOBALS["core"]->source_dir."mod/users/class/Groups.php");
+
+?>
