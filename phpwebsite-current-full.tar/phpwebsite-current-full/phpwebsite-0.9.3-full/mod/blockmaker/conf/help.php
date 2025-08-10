927f1615156e2c6fa9f3baf1cd56c35fa23a828f
me-suzy <me.suzana@gmail.com>
2025-08-11 00:47:39 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/blockmaker/conf/help.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/blockmaker/conf/help.php
@@ -0,0 +1,18 @@
+<?php
+
+$list_blocks = "List Blocks";
+$list_blocks_content = "The list blocks link will take you to a listing of the the blocks that are currently in the database.  From this list you can edit and delete your current blocks, you can also activate and deactivate your blocks which toggles whether or not they are publicly viewable";
+
+$create_block = "Create A New Block";
+$create_block_content = "The Create A New Block link will take you to a form to add a new block to the database.  Once saved the block will show up in your list of current blocks.  Don't forget to activate your new block after you save it";
+
+$main = "Block Maker Help";
+$main_content = "Blocks within phpWebSite are to be use for basic content display.  When created, these blocks are given a default box style.  The box style defines how the title, content, and footer will show up on the page. This box style can be changed at anytime via the layout module.";
+
+$theme_var = "Theme Variable";
+$theme_var_content = "The theme variable controls where on the page your block will be placed.  This is only for the creation process of the block.  It can be moved later by the layout module box move."; 
+
+$allow_view = "Allow View";
+$allow_view_content = "The allow view option allows you to choose when the block you are creating will show up on the page.  Choose which modules need to be active in order for the block to appear.";
+
+?>
