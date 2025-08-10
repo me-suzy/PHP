316d4901f10ade2af382a00098df31aa00cd905e
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:45 +0300

    Add Photopost_PHP/languages/italian/useralbums.php

---
+++ Photopost_PHP/languages/italian/useralbums.php
@@ -0,0 +1,27 @@
+<?
+$Globals['pp_lang']['closed'] = "Ci spiace, ma il servizio è temporaneamente sospeso per aggiornamenti. Riprova più tardi.";
+$Globals['pp_lang']['noperm'] = "Ci spiace informarti che non sei autorizzato a creare Albums.";
+$Globals['pp_lang']['noverify'] = "Il tuo account non è stato ancora attivato.<p>Segui le istruzioni che ti abbiamo inviato via Email per attivare il tuo account";
+$Globals['pp_lang']['nopa'] = "Spiacenti, Album Personali non abilitati.";
+$Globals['pp_lang']['useralbums'] = "Album Utenti";
+$Globals['pp_lang']['editor'] = "PhotoPost Album Editor";
+$Globals['pp_lang']['addalbum'] = "Aggiungi Album";
+$Globals['pp_lang']['errordir'] = "Errore nella creazione della cartella $newdir. Notifica al webmaster .";
+$Globals['pp_lang']['complete'] = "Elaborazione completata!";
+$Globals['pp_lang']['adduser'] = "Aggiungi Album utente";
+$Globals['pp_lang']['addaalbum'] = "PhotoPost Aggiungi un Album";
+$Globals['pp_lang']['albumname'] = "Nome Album";
+$Globals['pp_lang']['albumdesc'] = "Descrizione Album";
+$Globals['pp_lang']['isprivate'] = "Album Privato ?";
+$Globals['pp_lang']['newalbum'] = "Nuovo Album";
+$Globals['pp_lang']['complete'] = "Elaborazione completata!";
+$Globals['pp_lang']['editalbum'] = "Modifica Album Utente";
+$Globals['pp_lang']['savechanges'] = "Salva cambiamenti";
+$Globals['pp_lang']['malformed'] = "Trasmissione parametro incorretto!";
+$Globals['pp_lang']['invalidid'] = "ID Album non valido";
+$Globals['pp_lang']['deleteuser'] = "Cancella Album Utente ";
+$Globals['pp_lang']['delete1'] = "Stai per eliminare l'Album :";
+$Globals['pp_lang']['delete2'] = "e <b>TUTTE LE IMMAGINI E COMMENTI</b> in esso contenuti.<p>";
+$Globals['pp_lang']['delete3'] = "Sei sicuro di voler continuare?";
+$Globals['pp_lang']['imsure'] = "Sono sicuro. Cancella l'Album.";
+?>
