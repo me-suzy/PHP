a4d85e08834a23fbda5fa55e2d159c526fb7ebd2
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:32 +0300

    Add Photopost_PHP/languages/english/slideshow.php

---
+++ Photopost_PHP/languages/english/slideshow.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['closed'] = "We're sorry, but our Photo Boards are currently down for maintainence. Please try again later.";
+$Globals['pp_lang']['slideshow'] = "Slide Show";
+$Globals['pp_lang']['delay'] = "Time delay for photos?<br />(slower connections will want longer timeouts)";
+$Globals['pp_lang']['twosec'] = "2 seconds";
+$Globals['pp_lang']['foursec'] = "4 seconds";
+$Globals['pp_lang']['eightsec'] = "8 seconds";
+$Globals['pp_lang']['tensec'] = "10 seconds";   
+$Globals['pp_lang']['startslide'] = "Start Slideshow";
+?>
