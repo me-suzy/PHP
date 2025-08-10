d48017b5d13d5819dbb18bc0477e86d848a6fcf3
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:47 +0300

    Add Photopost_PHP/languages/hungarian/bulkupload.php

---
+++ Photopost_PHP/languages/hungarian/bulkupload.php
@@ -0,0 +1,25 @@
+<?
+$Globals['pp_lang']['closed'] = "Nagyon sajnáljuk, de a Képtár technikai okok miatt átmenetileg zárva tart. Kérjük, próbálkozzon késõbb!";
+$Globals['pp_lang']['noupload'] = "A felhasználók nem tölthetnek fel fotókat.";
+$Globals['pp_lang']['noperm'] = "Sajnáljuk, Önnek nincs jogosultsága fotók feltöltésére.";
+$Globals['pp_lang']['noverify'] = "Sajnáljuk, de Ön még nem erõsítette meg regisztrációját.<p>Erre szükség van, hogy fel tudjon tölteni fotókat.";
+$Globals['pp_lang']['processing'] = "Fotólista feldolgozása...";
+$Globals['pp_lang']['bulkuploads'] = "Tömeges feltöltés";
+$Globals['pp_lang']['photoadd'] = "FotóHáz Képtár feltölés";
+$Globals['pp_lang']['addselect'] = "Feltöltés - Fotók kiválasztása / Tulajdonságok beállítása";
+$Globals['pp_lang']['image'] = "Fotó";
+$Globals['pp_lang']['add'] = "Feltöltés";
+$Globals['pp_lang']['category'] = "Kategória";
+$Globals['pp_lang']['optional'] = "Szöveg (nem kötelezõ)";
+$Globals['pp_lang']['title'] = "Cím";
+$Globals['pp_lang']['desc'] = "Leírás";
+$Globals['pp_lang']['nomore'] = "A rendszer nem talált több fotót.";
+$Globals['pp_lang']['clickhere'] = "Vissza a fõmenübe";
+$Globals['pp_lang']['show'] = "Nézõképek mutatása?";
+$Globals['pp_lang']['thumbs'] = "Nézõképek száma a következõ feldolgozásnál?";
+$Globals['pp_lang']['proc'] = "Feldolgozás";
+$Globals['pp_lang']['preppro'] = "Felkészülés a következõ feldolgozásra";
+$Globals['pp_lang']['images'] = "fotók";
+$Globals['pp_lang']['click'] = "kattintson ide";
+$Globals['pp_lang']['notrefresh'] = "ha az oldal nem frissül automatikusan.";
+?>
