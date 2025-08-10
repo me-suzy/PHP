c054d0f3579ccf463d0a6e42f944314d1a2a5457
me-suzy <me.suzana@gmail.com>
2025-08-11 00:59:47 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/themes/mainsite/boxstyles/announcement_box1.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/themes/mainsite/boxstyles/announcement_box1.php
@@ -0,0 +1,22 @@
+<table border="0" cellspacing="0" cellpadding="0" width="100%">
+<tr>
+<td width="1%"><img src="<?php echo $box_dir; ?>mainsite_img/l_01.gif" alt="" height="17" width="16" border="0" /></td>
+<td style="background-image : url(<?php echo $source_http.$box_dir; ?>mainsite_img/l_02.gif); background-repeat : repeat-x"  width="98%"><img src="<?php echo $box_dir; ?>mainsite_img/spanner.gif" alt="" border="0" /></td>
+<td width="1%"><img src="<?php echo $box_dir; ?>mainsite_img/l_03.gif" width="19" height="17" alt="" border="0" /></td>
+</tr>
+<tr>
+<td style="background-image : url(<?php echo $source_http.$box_dir; ?>mainsite_img/l_04.gif); background-repeat : repeat-y" width="1%"><img src="<?php echo $box_dir; ?>mainsite_img/trans.gif" alt="" border="0" /></td>
+<td bgcolor="#618BB1"  width="98%"><table cellpadding="2"><tr><td><b><?php echo $box_title ?></b></td></tr></table></td>
+<td style="background-image : url(<?php echo $source_http.$box_dir; ?>mainsite_img/l_06.gif); background-repeat : repeat-y"  width="1%"><img src="<?php echo $box_dir; ?>mainsite_img/trans.gif" alt="" border="0" /></td>
+</tr>
+<tr>
+<td style="background-image : url(<?php echo $source_http.$box_dir; ?>mainsite_img/l_07.gif); background-repeat: repeat-y" width="1%"><img src="<?php echo $box_dir; ?>mainsite_img/trans.gif" alt="" border="0" /></td>
+<td bgcolor="#D0D1C9" width="98%"><p><?php echo $box_content ?></p></td>
+<td style="background-image : url(<?php echo $source_http.$box_dir; ?>mainsite_img/l_09.gif); background-repeat: repeat-y" width="1%"><img src="<?php echo $box_dir; ?>mainsite_img/trans.gif" alt="" border="0" /></td>
+</tr>
+<tr>
+<td valign="top" width="1%"><img src="<?php echo $box_dir; ?>mainsite_img/l_10.gif" alt="" width="16" height="20" border="0" /></td>
+<td style="background-image : url(<?php echo $source_http.$box_dir; ?>mainsite_img/l_11.gif); background-repeat : repeat-x" width="98%"><img src="<?php echo $box_dir; ?>mainsite_img/spanner.gif" alt="" border="0" /></td>
+<td valign="top" width="1%"><img src="<?php echo $box_dir; ?>mainsite_img/l_12.gif" width="19" height="20" alt="" border="0" /></td>
+</tr>
+</table>
