4f444797a01745c0b50a839ee763210d6d2344f7
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:15 +0300

    Add Photopost_PHP/languages/italian/addfav.php

---
+++ Photopost_PHP/languages/italian/addfav.php
@@ -0,0 +1,8 @@
+<?
+$Globals['pp_lang']['badcall'] = "Errore nello script.  Vai ad una immagine specifica, quindi clicca su Aggiungi a Preferiti.";
+$Globals['pp_lang']['closed'] = "Siamo spiacenti di informarti che il servizioè temporaneamente sospeso per aggiornamenti. Riprova più tardi.";
+$Globals['pp_lang']['noreg'] = "Sono gli utenti registrati possono avere un album Preferiti.";
+$Globals['pp_lang']['added'] = "L'immagine è stata inserita nel tuo Album Preferiti !";
+$Globals['pp_lang']['malform'] = "Chiamata non corretta a addfav.php";
+$Globals['pp_lang']['removed'] = "L'immagine è stata rimossa dal tuo Album Preferiti !";
+?>
