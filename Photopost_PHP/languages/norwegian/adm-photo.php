090318c4026e85967eee47855c35d4bc77b21355
me-suzy <me.suzana@gmail.com>
2025-08-11 00:22:51 +0300

    Add Photopost_PHP/languages/norwegian/adm-photo.php

---
+++ Photopost_PHP/languages/norwegian/adm-photo.php
@@ -0,0 +1,18 @@
+<?
+$Globals['pp_lang']['malform'] = "Ukorrekt parameter sendt!";
+$Globals['pp_lang']['mogrify'] = "Ikke-godkjent kall til å modifisere bilde.";
+$Globals['pp_lang']['erotate'] = "Det oppsto en feil ved rotering av bildet!";
+$Globals['pp_lang']['erotatemed'] = "Det oppsto en feil ved rotering av medium bilde!";
+$Globals['pp_lang']['rotateccw'] = "Bilde har blitt rotert mot klokken!";
+$Globals['pp_lang']['rotatecw'] = "Bildet har blitt rotert med klokken!";
+$Globals['pp_lang']['flipped'] = "Bildet har blitt flippet!";
+$Globals['pp_lang']['flopped'] = "Bildet har blitt floppet!";    
+$Globals['pp_lang']['nopic'] = "Feil: Ikke noe bilde å slette!";
+$Globals['pp_lang']['nophoto'] = "Bildet ble ikke funnet i din database!";
+$Globals['pp_lang']['delete'] = "Slettet bilde";
+$Globals['pp_lang']['errorcode'] = "Feilkode";
+$Globals['pp_lang']['noperm'] = "Du har ikke tillatelse til å gjøre dette!";
+$Globals['pp_lang']['badmove'] = "Du kan ikke flytte et bilde til den kategorien. Vennligst prøv igjen.";
+$Globals['pp_lang']['noneed'] = "Det er unødvendig å flytte bildet til samme kategori!";
+$Globals['pp_lang']['noaction'] = "Ingen handling spesifisert";
+?>
