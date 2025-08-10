7d2d27333b21d0c635b46344f9c21bfcdcfdd36d
me-suzy <me.suzana@gmail.com>
2025-08-11 00:20:38 +0300

    Add Photopost_PHP/languages/finnish/adm-cinc.php

---
+++ Photopost_PHP/languages/finnish/adm-cinc.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['delete'] = "Poista";
+$Globals['pp_lang']['edit'] = "Muuta";
+$Globals['pp_lang']['addsubcat'] = "Lisää alikategoria";
+$Globals['pp_lang']['empty'] = "Tyhjä";
+$Globals['pp_lang']['parenter1'] = "Yritit muuttaa päägatekorian alikategoriaan.";
+$Globals['pp_lang']['parenter2'] = "Yritit muuttaa pääkategorian itseensä.";
+$Globals['pp_lang']['prlink'] = "Yksityinen linkki albumiisi";
+$Globals['pp_lang']['order'] = "Tilaa";
+?>
