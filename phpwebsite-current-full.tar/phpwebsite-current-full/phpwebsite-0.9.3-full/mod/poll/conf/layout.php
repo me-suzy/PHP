5d6589cd565fdd841891e66d13538ac6ff3d4d1b
me-suzy <me.suzana@gmail.com>
2025-08-11 00:57:05 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/poll/conf/layout.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/poll/conf/layout.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$layout_info[] = array ("content_var" => "CNT_POLL", 
+			"transfer_var" => "body",
+			"home_only"=>0);
+
+$layout_info[] = array ("content_var" => "CNT_POLL_BOX", 
+			"transfer_var" => "left_col_top",
+			"home_only"=>0);
+?>
