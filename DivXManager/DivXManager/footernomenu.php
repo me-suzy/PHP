8a63a554aa914e29fb30f6f3c802c242dfcef371
me-suzy <me.suzana@gmail.com>
2025-08-11 00:11:48 +0300

    Add DivXManager/DivXManager/footernomenu.php

---
+++ DivXManager/DivXManager/footernomenu.php
@@ -0,0 +1,13 @@
+</td>
+    <td width="50" background="images/mr.gif">&nbsp;&nbsp;</td>
+</tr>
+<tr>
+    <td><img src="images/lo.gif" width="50" height="50" hspace="0" vspace="0" border="0" alt=""></td>
+	<td height="50" background="images/o.gif" width="100%">&nbsp;&nbsp;</td>
+    <td><img src="images/ro.gif" width="50" height="50" hspace="0" vspace="0" border="0" alt=""></td>
+</tr>
+</table>
+
+
+</body>
+</html>
