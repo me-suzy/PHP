358f5bae5be98caae389b4e55fd87b5f8b94ab80
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:56 +0300

    Add Photopost_PHP/languages/hungarian/login.php

---
+++ Photopost_PHP/languages/hungarian/login.php
@@ -0,0 +1,5 @@
+<?
+$Globals['pp_lang']['loginok'] = "Sikeres bejelentkezés!";
+$Globals['pp_lang']['loginbad'] = "Sikertelen bejelentkezés.  Kérem, ellenõrizze a felhasználónevét és jelszavát, vagy";
+$Globals['pp_lang']['loginbad2'] = "regisztráljon";
+?>
