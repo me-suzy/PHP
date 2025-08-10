4e47bf31a0fb80530d8e13b5fa4844d4ada62ef1
me-suzy <me.suzana@gmail.com>
2025-08-11 00:50:59 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/fatcat/conf/config.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/fatcat/conf/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define("FATCAT_MAX_IMAGE_WIDTH", 800);
+define("FATCAT_MAX_IMAGE_HEIGHT", 600);
+define("FATCAT_MAX_ICON_WIDTH", 50);
+define("FATCAT_MAX_ICON_HEIGHT", 50);
+
+// The cutoff title size before '...' are added to the end
+define("FATCAT_LINK_CUTOFF", 15);
+
+// The buffer size between the max length and the next space in a string
+define("FATCAT_LINK_BUFFER", 3);
+?>
