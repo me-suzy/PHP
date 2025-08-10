4779bb84aea89e6bf698171828f72c691ced85b7
me-suzy <me.suzana@gmail.com>
2025-08-11 00:18:56 +0300

    Add Photopost_PHP/email-inc.php

---
+++ Photopost_PHP/email-inc.php
@@ -0,0 +1,18 @@
+<?
+//////////////////////////// COPYRIGHT NOTICE //////////////////////////////
+// This script is part of PhotoPost PHP, a software application by        //
+// All Enthusiast, Inc.  Use of any kind of part or all of this           //
+// script or modification of this script requires a license from All      //
+// Enthusiast, Inc.  Use or modification of this script without a license //
+// constitutes Software Piracy and will result in legal action from All   //
+// Enthusiast, Inc.  All rights reserved.                                 //
+// http://www.photopost.com      legal@photopost.com                      //
+// Contributing Developer: Michael Pierce (danasoft.com)                  //
+//                                                                        //
+//            PhotoPost Copyright 2002, All Enthusiast, Inc.              //
+////////////////////////////////////////////////////////////////////////////
+
+// This file is no longer needed
+
+?>
+
