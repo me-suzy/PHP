7dfe1f8c2da9e788bc3d3965a59bdca51311fa61
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:00 +0300

    Add Photopost_PHP/languages/norwegian/login-inc.php

---
+++ Photopost_PHP/languages/norwegian/login-inc.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['login'] = "Logg inn";
+$Globals['pp_lang']['enterup'] = "Vennligst fyll ut med ditt brukernavn og passord.";
+$Globals['pp_lang']['username'] = "Brukernavn";
+$Globals['pp_lang']['register'] = "Registrer";
+$Globals['pp_lang']['password'] = "Passord";
+$Globals['pp_lang']['forgot'] = "Glemt Passord?";
+$Globals['pp_lang']['submit'] = "Send";
+$Globals['pp_lang']['unreg'] = "Uregistrert";
+?>
