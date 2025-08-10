d7be971505c6ff507c529a4cc335cee66a43f077
me-suzy <me.suzana@gmail.com>
2025-08-11 00:56:33 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/photoalbum/conf/controlpanel.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/photoalbum/conf/controlpanel.php
@@ -0,0 +1,14 @@
+<?php
+
+$image['name'] = "photo.png";
+$image['alt'] = "Photo Albums Author: Steven Levin";
+
+$link[0] = array ("label"=>"Photo Albums",
+		  "module"=>"photoalbum",
+		  "url"=>"index.php?module=photoalbum&amp;PHPWS_AlbumManager_op=list",
+		  "description"=>"The Photo Album allows you to manage sets of image galleries.",
+		  "image"=>$image,
+		  "admin"=>TRUE,
+		  "tab"=>"content");
+
+?>
