1e97ab91ac2d1653665110f846eb3803027f45bf
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:09 +0300

    Add Photopost_PHP/languages/french/addfav.php

---
+++ Photopost_PHP/languages/french/addfav.php
@@ -0,0 +1,8 @@
+<?
+$Globals['pp_lang']['badcall'] = "Script non appelé correctement. Choisissez la photo, puis cliquez sur le lien Ajouter aux Favoris.";
+$Globals['pp_lang']['closed'] = "Nous sommes désolés mais les albums photos sont actuellement indisponibles pour cause de maintenance. Veuillez revenir plus tard. Merci.";
+$Globals['pp_lang']['noreg'] = "Seuls les utilisateurs inscrits peuvent avoir les photos favorites.";
+$Globals['pp_lang']['added'] = "Cette image a été ajoutée à votre album favori";
+$Globals['pp_lang']['malform'] = "Appel mal formé du fichier addfav.php";
+$Globals['pp_lang']['removed'] = "Cette image a été retirée de votre album favori!";
+?>
