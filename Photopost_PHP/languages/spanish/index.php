4ced81f73201bc4e168898e14fb64ea724a31201
me-suzy <me.suzana@gmail.com>
2025-08-11 00:23:31 +0300

    Add Photopost_PHP/languages/spanish/index.php

---
+++ Photopost_PHP/languages/spanish/index.php
@@ -0,0 +1,24 @@
+<?
+$Globals['pp_lang']['closed'] = "Lo sentimos, pero nuestra galer&iacute;a se encuentra cerrada. Por favor intenta m&aacute;s tarde.";
+$Globals['pp_lang']['home'] = "Inicio";
+$Globals['pp_lang']['welcomeuser'] = "Bienvenido,";
+$Globals['pp_lang']['welcome'] = "Bienvenido a la ";
+$Globals['pp_lang']['tablehead'] = "&Iacute;ndice Principal";
+$Globals['pp_lang']['recent'] = "Mensajes recientes";
+$Globals['pp_lang']['lastday'] = "&uacute;lt. d&iacute;a";
+$Globals['pp_lang']['last7'] = "&uacute;lt. 7 d&iacute;as";
+$Globals['pp_lang']['last14'] = "&uacute;lt. 14 d&iacute;as";
+$Globals['pp_lang']['allimages'] = "todas las im&aacute;genes";
+$Globals['pp_lang']['category'] = "Categor&iacute;a";
+$Globals['pp_lang']['photos'] = "Im&aacute;genes";
+$Globals['pp_lang']['comments'] = "Comentarios";
+$Globals['pp_lang']['lastcomment'] = "&Uacute;lt. Comentario";
+$Globals['pp_lang']['lastphoto'] = "&Uacute;lt. imagen";
+$Globals['pp_lang']['toposter'] = "Top usuarios:";
+$Globals['pp_lang']['regusers'] = "usuarios registrados,";
+$Globals['pp_lang']['posted'] = "im&aacute;genes subidas y";
+$Globals['pp_lang']['postednoc'] = "im&aacute;genes subidas";
+$Globals['pp_lang']['posts'] = "comentarios";
+$Globals['pp_lang']['views'] = "im&aacute;genes vistas y";
+$Globals['pp_lang']['diskspace'] = "de espacio en disco.";
+?>
