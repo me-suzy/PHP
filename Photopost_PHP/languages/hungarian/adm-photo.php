cd00b9548a43cc75260bbe8893545eccaa0b7368
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:45 +0300

    Add Photopost_PHP/languages/hungarian/adm-photo.php

---
+++ Photopost_PHP/languages/hungarian/adm-photo.php
@@ -0,0 +1,18 @@
+<?
+$Globals['pp_lang']['malform'] = "Hibás paraméter elhagyva.";
+$Globals['pp_lang']['mogrify'] = "Nem támogatott kérés a kép módosítására.";
+$Globals['pp_lang']['erotate'] = "Hiba a kép forgatásakor.";
+$Globals['pp_lang']['erotatemed'] = "Hiba a közepes kép forgatásakor.";
+$Globals['pp_lang']['rotateccw'] = "A kép balra lett elforgatva.";
+$Globals['pp_lang']['rotatecw'] = "A kép az jobbra lett elforgatva.";
+$Globals['pp_lang']['flipped'] = "Fotó függõlegesen átfordítva!";
+$Globals['pp_lang']['flopped'] = "Fotó vízszintesen átfordítva!";    
+$Globals['pp_lang']['nopic'] = "Hiba: Nincs törölhetõ kép.";
+$Globals['pp_lang']['nophoto'] = "A fotó nincs az adatbázisban.";
+$Globals['pp_lang']['delete'] = "Törölt kép";
+$Globals['pp_lang']['errorcode'] = "Hiba kód";
+$Globals['pp_lang']['noperm'] = "Nincs jogosultsága a mûvelet végrehajtásához!";
+$Globals['pp_lang']['badmove'] = "Nem tehet képet ebbe a kategóriába, kérjük, próbálkozzon újra!";
+$Globals['pp_lang']['noneed'] = "A kép már szerepel ebben a kategóriában.";
+$Globals['pp_lang']['noaction'] = "Nem határozott meg mûveletet.";
+?>
