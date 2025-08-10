a09a52c59cf69b676fb3ec7f486d234c09cece51
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:16 +0300

    Add Photopost_PHP/languages/french/ecard.php

---
+++ Photopost_PHP/languages/french/ecard.php
@@ -0,0 +1,23 @@
+<?
+$Globals['pp_lang']['disabled'] = "Les cartes sont désactivées";
+$Globals['pp_lang']['larger'] = "Voir une image plus grande";
+$Globals['pp_lang']['smaller'] = "Voir une image plus petite";
+$Globals['pp_lang']['send'] = "Envoyer la photo comme carte électronique";
+$Globals['pp_lang']['view'] = "Voir votre carte électronique";
+$Globals['pp_lang']['ecard'] = "Carte électronique";
+$Globals['pp_lang']['home'] = "Index";
+$Globals['pp_lang']['name'] = "Nom";
+$Globals['pp_lang']['email'] = "Email";
+$Globals['pp_lang']['info'] = "Coordonnées du destinataire";
+$Globals['pp_lang']['alter'] = "Si vous ne connaissez pas l'adresse email du destinataire, veuillez entrer son nom d'utilisateur à la place:<br /><br />Nom d'utilisateur";
+$Globals['pp_lang']['subjgreet'] = "Sujet de la carte électronique";
+$Globals['pp_lang']['subject'] = "Sujet";
+$Globals['pp_lang']['yourmess'] = "Message de votre carte électronique";
+$Globals['pp_lang']['yourecard'] = "Votre carte électronique";
+$Globals['pp_lang']['send'] = "Expédier la carte électronique";
+$Globals['pp_lang']['noemail'] = "Vous n'avez pas entré l'adresse email du destinataire. Veuillez revenir à la page précédente et entrer soit l'email du destinaire soit son nom d'utilisateur.";
+$Globals['pp_lang']['blank'] = "Votre message est vide.";
+$Globals['pp_lang']['badname'] = "Désolé, le nom de cet utilisateur n'est pas reconnu.";
+$Globals['pp_lang']['hello'] = "Bonjour";
+$Globals['pp_lang']['cardsent'] = "Votre carte a été expédiée!";
+?>
