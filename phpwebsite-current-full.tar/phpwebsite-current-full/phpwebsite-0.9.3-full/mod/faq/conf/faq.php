ffccf2d1d4ee961bd9827a509b97f1f11f7a10a8
me-suzy <me.suzana@gmail.com>
2025-08-11 00:50:29 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/faq/conf/faq.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/faq/conf/faq.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Configuration file FAQ module.
+ */
+
+/* Default limit to use */
+define("PHPWS_FAQ_NUMLEGENDITEMS", "5.0");
+
+/* Layout Options */
+define("PHPWS_FAQ_NOCAT_CLICKQUES_VIEW", 0);
+define("PHPWS_FAQ_NOCAT_QA_VIEW", 1);
+define("PHPWS_FAQ_CAT_VIEW", 2);
+?>
