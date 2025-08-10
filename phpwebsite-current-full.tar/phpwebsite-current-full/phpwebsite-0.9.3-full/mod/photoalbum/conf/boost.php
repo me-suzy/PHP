354c33cefa85cb60da5d9f375a8b51cca3dbe0f3
me-suzy <me.suzana@gmail.com>
2025-08-11 00:56:30 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/photoalbum/conf/boost.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/photoalbum/conf/boost.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @version $Id: boost.php,v 1.13 2003/07/03 15:48:36 steven Exp $
+ */
+
+$mod_title = "photoalbum";
+$mod_pname = "Photo Albums";
+$mod_directory = "photoalbum";
+$mod_filename = "index.php";
+$mod_class_files = array("AlbumManager.php");
+$allow_view = "all";
+$admin_mod = 1;
+$priority = 50;
+$active = "on";
+$version = "0.62";
+
+?>
