c53d013e8c6ee7b5421627f14ee85ea7ea49febe
me-suzy <me.suzana@gmail.com>
2025-08-11 01:06:17 +0300

    Add znews 1.1/znews 1.1/eint/49852166489728068980.php

---
+++ znews 1.1/znews 1.1/eint/49852166489728068980.php
@@ -0,0 +1,14 @@
+<? $betr="Neues Design";
+?><? $date="21.09.2002";
+?><? $name="Neo";
+?><? $mail="webmaster@nicis-page.de";
+?><? $text="Hi,
+<br>
+meine neue Seite ist online!";
+?><? $text2="Ich hab ein neues Design, ein neues Newsscript und ein 
+
+Gästebuch. Natürlich wir es die beiden Scripts zum download
+
+geben.";
+?><? $mname="49852166489728068980";
+?>
