5840cbf582c733fb7e1e7833cf9c2561f191f7b3
me-suzy <me.suzana@gmail.com>
2025-08-11 00:59:49 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/themes/mainsite/boxstyles/comments_popbox.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/themes/mainsite/boxstyles/comments_popbox.php
@@ -0,0 +1,4 @@
+<br /><table border="0" cellspacing="0" cellpadding="0" width="100%">
+<tr><td class="bg_medium"><b><?php echo $box_title; ?></b></td></tr>
+<tr><td><p><?php echo $box_content ?></p></td></tr>
+</table>
