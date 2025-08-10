ff5ed816926884832acdece9df01332451bd722c
me-suzy <me.suzana@gmail.com>
2025-08-11 00:58:17 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/skeleton/conf/users.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/skeleton/conf/users.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This is a skeleton user configuration file. Edit it to be used
+ * with your module.
+ *
+ * $Id: users.php,v 1.2 2003/07/02 18:18:05 adam Exp $
+ */
+
+$default_admin_menu = "developer";
+
+?>
