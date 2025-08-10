184f284011006a9441210b31490cc29e3158a444
me-suzy <me.suzana@gmail.com>
2025-08-11 00:53:15 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/menuman/conf/config.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/menuman/conf/config.php
@@ -0,0 +1,32 @@
+<?php
+
+/* Menu Manager Module Configuration File */
+/* Use ascii to remain w3c compliant */
+$indent_options = array(1=>"&#160;",
+			2=>"&#35;",
+			3=>"&#43;",
+			4=>"&#45;",
+			5=>"&#62;",
+			6=>"&#187;",
+			7=>"&#215;"
+			);
+
+$color_options = array(""=>"&#160;",
+		       "#000000"=>"Black",
+		       "#ff0000"=>"Red",
+		       "#00ff00"=>"Green",
+		       "#0000ff"=>"Blue",
+		       "#ffffff"=>"White"
+		       );
+
+$upload_directory = "images/menuman/";
+
+$allowed_types = array("image/gif",
+		       "image/jpg",
+		       "image/jpeg",
+		       "image/png"
+		       );
+
+//$template_dir = "mod/menuman/templates/";
+
+?>
