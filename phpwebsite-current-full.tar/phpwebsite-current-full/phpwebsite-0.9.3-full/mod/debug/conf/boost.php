5f6f4468476ab096f31bb0f84105da5b2c1930d6
me-suzy <me.suzana@gmail.com>
2025-08-11 00:50:00 +0300

    Add phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/debug/conf/boost.php

---
+++ phpwebsite-current-full.tar/phpwebsite-current-full/phpwebsite-0.9.3-full/mod/debug/conf/boost.php
@@ -0,0 +1,15 @@
+<?php
+
+$mod_title = "debug";
+$mod_pname = "phpWebSite Debugger";
+$mod_directory = "debug";
+$mod_filename = "debug.php";
+$allow_view = "all";
+$admin_mod = 1;
+$priority = 98;
+$active = "on";
+$version = 1.00;
+$update_register = 0; 
+$deity_only = 1;
+
+?>
