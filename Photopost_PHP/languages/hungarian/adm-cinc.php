5f732362915c2613f110bac2b3f61fd8e99ee1f1
me-suzy <me.suzana@gmail.com>
2025-08-11 00:21:44 +0300

    Add Photopost_PHP/languages/hungarian/adm-cinc.php

---
+++ Photopost_PHP/languages/hungarian/adm-cinc.php
@@ -0,0 +1,10 @@
+<?
+$Globals['pp_lang']['delete'] = "Delete";
+$Globals['pp_lang']['edit'] = "Edit";
+$Globals['pp_lang']['addsubcat'] = "Add Subcat";
+$Globals['pp_lang']['empty'] = "Empty";
+$Globals['pp_lang']['parenter1'] = "You tried to parent a forum to one of its children.";
+$Globals['pp_lang']['parenter2'] = "You tried to parent a forum to itself.";
+$Globals['pp_lang']['prlink'] = "Private link to access album";
+$Globals['pp_lang']['order'] = "Order";
+?>
